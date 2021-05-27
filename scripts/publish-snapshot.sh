bd6145803b0c3ce0b58b2974f87e137dd213bf2a
Marc Handalian <handalm@amazon.com>
2021-05-27 14:36:23 -0700

    Add usage method and strip trailing / from any configured REPO_URL
    Signed-off-by: Marc Handalian <handalm@amazon.com>

--- scripts/publish-snapshot.sh
+++ scripts/publish-snapshot.sh
@@ -11,8 +11,7 @@
 # Language:      Shell
 #
 # About:         Deploy opensearch artifacts to a sonatype snapshot repository.
-#                The given directory is intended to be the root directory of a maven repository containing ./org/opensearch artifacts.
-#                This script will search POM files under ./org/opensearch.
+#                This script will search POM files under the passed in directory.
 #                If found, pom, jar, and signature files will be deployed to the org/opensearch namespace.
 #
 # Prerequisites: The given directory must be the parent directory of org/opensearch artifacts.
@@ -27,9 +26,29 @@
 set -e
 
 [ -z "${1:-}" ] && {
-  echo "Usage: ($basename $0) dir"
-  exit 1
+  usage
 }
+
+usage() {
+    echo "usage: $0 dir [-h]"
+    echo "  dir     parent directory of artifacts to be published to org/opensearch namespace."
+    echo "          example: dir = ~/.m2/repository/org/opensearch where dir contains artifacts of a path:"
+    echo "                   /plugin/reindex-client/1.0.0-SNAPSHOT/reindex-client-1.0.0-SNAPSHOT.jar"
+    echo "  -h      display help"
+    echo "Required environment variables:"
+    echo "SONATYPE_ID - username with publish rights to a sonatype repository"
+    echo "SONATYPE_ID - password for sonatype"
+    echo "REPO_URL    - repository URL without path, ex. https://aws.oss.sonatype.org/"
+    exit 1
+}
+
+while getopts ":h" option; do
+  case $option in
+    h)
+       usage
+       exit;
+  esac
+done
 
 [ -z "${SONATYPE_ID}" ] && {
   echo "SONATYPE_ID is required"
@@ -41,12 +60,12 @@ set -e
   exit 1
 }
 
-[ -z "${SNAPSHOT_HOST}" ] && {
-  echo "SNAPSHOT_HOST is required"
+[ -z "${REPO_URL}" ] && {
+  echo "REPO_URL is required"
   exit 1
 }
 
-snapshot_url="${SNAPSHOT_HOST}/nexus/content/repositories/snapshots/"
+snapshot_url="${REPO_URL%/}/nexus/content/repositories/snapshots/org/opensearch"
 
 # Import Opensearch GPG key
 curl -S https://artifacts.opensearch.org/publickeys/opensearch.pgp | gpg --import
@@ -56,7 +75,7 @@ i=0
 
 echo "searching for poms under $PWD"
 
-pomFiles="$(find "./org/opensearch" -name '*.pom')"
+pomFiles="$(find "." -name '*.pom')"
 if [ -z "${pomFiles}" ]; then
   echo "No artifacts found under $PWD"
   exit 1
