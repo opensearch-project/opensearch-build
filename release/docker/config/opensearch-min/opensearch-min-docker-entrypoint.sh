60b4c20185a718172b84898259b2b51bfaf73e88
Cameron Skinner <camerski@amazon.com>
2021-07-09 00:44:31 +0000

    Add opensearch-min Dockerfile and config
    Signed-off-by: Cameron Skinner <camerski@amazon.com>

---
+++ release/docker/config/opensearch-min/opensearch-min-docker-entrypoint.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+#
+# Modifications Copyright OpenSearch Contributors. See
+# GitHub history for details.
+
+
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+# This script specify the entrypoint startup actions for opensearch
+# It will start both opensearch and performance analyzer plugin cli
+# If either process failed, the entire docker container will be removed
+# in favor of a newly started container
+
+# Files created by OpenSearch should always be group writable too
+umask 0002
+
+if [[ "$(id -u)" == "0" ]]; then
+    echo "OpenSearch cannot run as root. Please start your container as another user."
+    exit 1
+fi
+
+# Parse Docker env vars to customize OpenSearch
+#
+# e.g. Setting the env var cluster.name=testcluster
+#
+# will cause OpenSearch to be invoked with -Ecluster.name=testcluster
+
+declare -a opensearch_opts
+
+while IFS='=' read -r envvar_key envvar_value
+do
+    # OpenSearch settings need to have at least two dot separated lowercase
+    # words, e.g. `cluster.name`, except for `processors` which we handle
+    # specially
+    if [[ "$envvar_key" =~ ^[a-z0-9_]+\.[a-z0-9_]+ || "$envvar_key" == "processors" ]]; then
+        if [[ ! -z $envvar_value ]]; then
+          opensearch_opt="-E${envvar_key}=${envvar_value}"
+          opensearch_opts+=("${opensearch_opt}")
+        fi
+    fi
+done < <(env)
+
+# The virtual file /proc/self/cgroup should list the current cgroup
+# membership. For each hierarchy, you can follow the cgroup path from
+# this file to the cgroup filesystem (usually /sys/fs/cgroup/) and
+# introspect the statistics for the cgroup for the given
+# hierarchy. Alas, Docker breaks this by mounting the container
+# statistics at the root while leaving the cgroup paths as the actual
+# paths. Therefore, OpenSearch provides a mechanism to override
+# reading the cgroup path from /proc/self/cgroup and instead uses the
+# cgroup path defined the JVM system property
+# opensearch.cgroups.hierarchy.override. Therefore, we set this value here so
+# that cgroup statistics are available for the container this process
+# will run in.
+export OPENSEARCH_JAVA_OPTS="-Dopensearch.cgroups.hierarchy.override=/ $OPENSEARCH_JAVA_OPTS"
+
+# Export OpenSearch Home
+export OPENSEARCH_HOME=/usr/share/opensearch
+
+# Start opensearch
+exec $OPENSEARCH_HOME/bin/opensearch "${opensearch_opts[@]}"
