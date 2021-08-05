fecdb6ddd3a457223abe2906ae313c4891abf5e1
dblock <dblock@amazon.com>
2021-08-05 23:28:46 +0000

    Fixup reporting distributions and remove broken performance analyzer for now.
    Signed-off-by: dblock <dblock@amazon.com>

--- scripts/bundle-build/components/dashboards-notebooks/build.sh
+++ scripts/bundle-build/components/dashboards-notebooks/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-outputDir=artifacts
-mkdir -p $outputDir
 cd opensearch-notebooks
 ./gradlew assemble --no-daemon --refresh-dependencies -Dbuild.snapshot=false -DskipTests=true -Dopensearch.version=$1
 
-distributions=$(find . -path \*build/distributions)
+outputDir=../artifacts
+mkdir -p $outputDir
+
+zipPath=$(find . -path \*build/distributions/*.zip)
+distributions="$(dirname "${zipPath}")"
+
+echo "COPY ${distributions}/*.zip"
 mkdir -p $outputDir/plugins
-cp ${distributions}/*.zip ../$outputDir/plugins
+cp ${distributions}/*.zip $outputDir/plugins
--- scripts/bundle-build/components/dashboards-reports/build.sh
+++ scripts/bundle-build/components/dashboards-reports/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-outputDir=artifacts
-mkdir -p $outputDir
 cd reports-scheduler
 ./gradlew assemble --no-daemon --refresh-dependencies -Dbuild.snapshot=false -DskipTests=true -Dopensearch.version=$1
 
-distributions=$(find . -path \*build/distributions)
-mkdir -p $outputDir/plugins
-cp ${distributions}/*.zip ../$outputDir/plugins
+outputDir=../artifacts
+mkdir -p $outputDir
 
+zipPath=$(find . -path \*build/distributions/*.zip)
+distributions="$(dirname "${zipPath}")"
+
+echo "COPY ${distributions}/*.zip"
+mkdir -p $outputDir/plugins
+cp ${distributions}/*.zip $outputDir/plugins
