7cde7325acf819181c8599ae98fe1912a9fa467e
Marc Handalian <handalm@amazon.com>
2021-08-05 23:08:19 -0700

    Add artifacts to output build manifest.
    This change updates the output manifest to include artifacts.
    artifacts will be in grouped into the following categories based on:
    where they are placed inside a component's build.sh script.
    
    bundles
    plugins
    libs
    maven
    
    This change also updates job-scheduler and OpenSearch core build scripts to correctly
    place their artifacts.
    
    Signed-off-by: Marc Handalian <handalm@amazon.com>

--- scripts/bundle-build/components/job-scheduler/build.sh
+++ scripts/bundle-build/components/job-scheduler/build.sh
@@ -6,12 +6,8 @@ outputDir=artifacts
 mkdir -p $outputDir
 ./gradlew assemble --no-daemon --refresh-dependencies -Dbuild.snapshot=false -DskipTests=true -Dopensearch.version=$1
 
-zipPath=$(find . -path \*build/distributions/*.zip)
-distributions="$(dirname "${zipPath}")"
-
-echo "COPY ${distributions}/*.zip"
 mkdir -p $outputDir/plugins
-cp ${distributions}/*.zip ./$outputDir/plugins
+cp ./build/distributions/*.zip ./$outputDir/plugins
 
 ./gradlew publishToMavenLocal -Dopensearch.version=$1 -Dbuild.snapshot=false
 mkdir -p $outputDir/maven
