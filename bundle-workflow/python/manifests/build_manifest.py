a90d6722d8e3f700d5be94c4f379f1aae7a55229
Cameron Skinner <camerski@amazon.com>
2021-08-09 21:21:23 +0000

    Refactor the build workflow and extract common components for future sharing with the bundle and test workflows
    Add 'manifests' package that contains immutable input and build manifest classes
    
    Add build_workflow library
    
    Add build workflow entry point
    
    Delete the old build workflow code
    
    Signed-off-by: Cameron Skinner <camerski@amazon.com>

---
+++ bundle-workflow/python/manifests/build_manifest.py
@@ -0,0 +1,84 @@
+# Copyright OpenSearch Contributors.
+# SPDX-License-Identifier: Apache-2.0
+
+import yaml
+
+'''
+A BuildManifest is an immutable view of the outputs from a build step
+The manifest contains information about the product that was built (in the `build` section),
+and the components that made up the build in the `components` section.
+
+The format for schema version 1.0 is:
+schema-version: 1.0
+build:
+  name: string
+  version: string
+  architecture: x64 or arm64
+components:
+  - name: string
+    repository: URL of git repository
+    ref: git ref that was built (sha, branch, or tag)
+    commit_id: The actual git commit ID that was built (i.e. the resolved "ref")
+    artifacts:
+      maven:
+        - maven/relative/path/to/artifact
+        - ...
+      plugins:
+        - plugins/relative/path/to/artifact
+        - ...
+      libs:
+        - libs/relative/path/to/artifact
+        - ...
+  - ...
+'''
+class BuildManifest:
+    @staticmethod
+    def from_file(path):
+        with open(path, 'r') as file:
+            return BuildManifest(yaml.safe_load(file))
+
+    def __init__(self, data):
+        self.version = str(data['schema-version'])
+        if self.version != '1.0':
+            raise ValueError(f'Unsupported schema version: {self.version}')
+        self.build = self.Build(data['build'])
+        self.components = list(map(lambda entry: self.Component(entry),
+                                   data['components']))
+
+    def to_dict(self):
+        return {
+            'schema-version': '1.0',
+            'build': self.build.to_dict(),
+            'components': list(map(lambda component: component.to_dict(),
+                                   self.components))
+        }
+
+    class Build:
+        def __init__(self, data):
+            self.name = data['name']
+            self.version = data['version']
+            self.architecture = data['architecture']
+
+        def to_dict(self):
+            return {
+                'name': self.name,
+                'version': self.version,
+                'architecture': self.architecture
+            }
+
+    class Component:
+        def __init__(self, data):
+            self.name = data['name']
+            self.repository = data['repository']
+            self.ref = data['ref']
+            self.commit_id = data['commit_id']
+            self.artifacts = data['artifacts']
+
+        def to_dict(self):
+            return {
+                'name': self.name,
+                'repository': self.repository,
+                'ref': self.ref,
+                'commit_id': self.commit_id,
+                'artifacts': self.artifacts
+            }
