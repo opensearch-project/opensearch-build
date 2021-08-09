37c1cee2e329a085bfcef09af1998c864c148d72
dblock <dblock@amazon.com>
2021-08-09 23:46:28 +0000

    Don't recommend sudo apt install.
    Signed-off-by: dblock <dblock@amazon.com>

--- bundle-workflow/run.sh
+++ bundle-workflow/run.sh
@@ -8,9 +8,9 @@ set -e
 DIR="$(dirname "$0")"
 
 if [ -z "$1" ]; then (echo "syntax: run.sh [workflow.py]"; exit -1); fi
-command -v python3 >/dev/null 2>&1 || (echo "missing python3, try 'sudo apt install python3'"; exit -1)
-command -v pip >/dev/null 2>&1 || (echo "missing pip, try 'sudo apt install python3-pip'"; exit -1)
-command -v pipenv >/dev/null 2>&1 || (echo "missing pipenv, try 'sudo apt install pipenv'"; exit -1)
+command -v python3 >/dev/null 2>&1 || (echo "missing python3"; exit -1)
+command -v pip >/dev/null 2>&1 || (echo "missing python3-pip"; exit -1)
+command -v pipenv >/dev/null 2>&1 || (echo "missing pipenv"; exit -1)
 
 python3 -m pipenv install
 python3 -m pipenv run "$1" ${@:2}
