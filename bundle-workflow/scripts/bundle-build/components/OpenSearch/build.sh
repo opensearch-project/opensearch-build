06c9070a09d3e92df7c34d795e45dc6d2504e1b8
Daniel Doubrovkine (dB.) <dblock@dblock.org>
2021-08-16 17:41:06 -0400

    Fix OpenSearch build output for snapshot builds. (#219)
    Signed-off-by: dblock <dblock@amazon.com>

--- bundle-workflow/scripts/bundle-build/components/OpenSearch/build.sh
+++ bundle-workflow/scripts/bundle-build/components/OpenSearch/build.sh
@@ -66,18 +66,28 @@ mkdir -p "${outputDir}/maven"
 # Copy maven publications to be promoted
 cp -r ./build/local-test-repo/org/opensearch "${outputDir}"/maven
 
-if [ "${ARCHITECTURE}" = "x64" ]; then
-  ./gradlew :distribution:archives:linux-tar:assemble -Dbuild.snapshot=false
-  cp -r distribution/archives/linux-tar/build/distributions/. "${outputDir}"/bundle
-elif [ "${ARCHITECTURE}" == "arm64" ]; then
-  ./gradlew :distribution:archives:linux-arm64-tar:assemble -Dbuild.snapshot=false
-  cp -r distribution/archives/linux-arm64-tar/build/distributions/ "${outputDir}"/bundle
-fi
+# Assemble distribution artifact
+# see https://github.com/opensearch-project/OpenSearch/blob/main/settings.gradle#L34 for other distribution targets
+case $ARCHITECTURE in
+    x86)
+        TARGET="linux-tar"
+        QUALIFIER="linux-x86"
+        ;;
+    x64)
+        TARGET="linux-arm64-tar"
+        QUALIFIER="linux-arm64"
+        ;;
+    *)
+        echo "Unsupported architecture: ${ARCHITECTURE}"
+        exit 1
+        ;;
+esac
 
-cd $outputDir/bundle
+./gradlew :distribution:archives:$TARGET:assemble -Dbuild.snapshot=$SNAPSHOT
 
-#rename included bundle to -min.
-ARTIFACT_FULL_NAME=`ls  | grep -E 'tar.gz$' | tail -n 1`
-ARTIFACT_BASE_NAME=`basename -s .tar.gz $ARTIFACT_FULL_NAME | sed "s/opensearch/opensearch-min/g"`
-ARTIFACT_NEW_NAME="${ARTIFACT_BASE_NAME}${IDENTIFIER}.tar.gz"
-mv $ARTIFACT_FULL_NAME $ARTIFACT_NEW_NAME || true
+# Copy artifact to bundle output with -min in the name
+[[ "$SNAPSHOT" == "true" ]] && IDENTIFIER="-SNAPSHOT"
+ARTIFACT_BUILD_NAME=opensearch-$VERSION$IDENTIFIER-$QUALIFIER.tar.gz
+ARTIFACT_TARGET_NAME=opensearch-min-$VERSION$IDENTIFIER-$QUALIFIER.tar.gz
+mkdir -p "${outputDir}/bundle"
+cp distribution/archives/$TARGET/build/distributions/$ARTIFACT_BUILD_NAME "${outputDir}"/bundle/$ARTIFACT_TARGET_NAME
