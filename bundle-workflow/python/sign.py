918eaa27dcc9e149c368e497f6fddd3f0e560efe
Poojita-Raj <poojiraj@amazon.com>
2021-08-19 21:33:11 +0000

    optional parameters added
    Signed-off-by: Poojita-Raj <poojiraj@amazon.com>

--- bundle-workflow/python/sign.py
+++ bundle-workflow/python/sign.py
@@ -11,20 +11,27 @@ from pathlib import Path
 from signing_workflow.signer import Signer
 
 parser = argparse.ArgumentParser(description = "Sign artifacts")
-parser.add_argument('manifest', type = argparse.FileType('r'), help="Manifest file.")
+parser.add_argument('manifest', type = argparse.FileType('r'), help="Absolute path of local manifest file.")
+parser.add_argument('--component',nargs='?',help="Component name")
+parser.add_argument('--type',nargs='?',help="Artifact type")
 args = parser.parse_args()
 
 manifest = BuildManifest.from_file(args.manifest)
-basepath = os.path.abspath(os.path.join(__file__ ,"../../..")) + '/artifacts/'
+basepath = os.path.dirname(args.manifest.name)
+print("hello",basepath)
+
 signer = Signer()
 for component in manifest.components:
+    if (not args.component) or ( args.component and component.name == args.component):
         print(f'signing {component.name}')
-        for key in component.artifacts:
-            artifact_list = component.artifacts[key]
-            for artifact in artifact_list:
-                location = basepath + artifact 
-                signer.sign(location)
-                signer.verify(location + ".asc")
+        for artifact_type in component.artifacts:
+            if (not args.type) or (args.type and artifact_type == args.type):
+                artifact_list = component.artifacts[artifact_type]
+                for artifact in artifact_list:
+                    location = os.path.join(basepath, artifact)
+                    print("LOCATION=", location)
+                    signer.sign(location)
+                    signer.verify(location + ".asc")
 signer.close()
 
 print('Done.')
