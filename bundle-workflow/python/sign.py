06a39e99c7ae40e35c671d2fd8fc76cb9bfaba55
Poojita-Raj <poojiraj@amazon.com>
2021-08-20 18:08:42 +0000

    Fixed import
    Signed-off-by: Poojita-Raj <poojiraj@amazon.com>

--- bundle-workflow/python/sign.py
+++ bundle-workflow/python/sign.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python
 
 import os
-import subprocess
-import tempfile
-import uuid
 import argparse
 from manifests.build_manifest import BuildManifest
-from pprint import pprint
-from pathlib import Path
 from signing_workflow.signer import Signer
 
 parser = argparse.ArgumentParser(description = "Sign artifacts")
-parser.add_argument('manifest', type = argparse.FileType('r'), help="Absolute path of local manifest file.")
-parser.add_argument('--component',nargs='?',help="Component name")
-parser.add_argument('--type',nargs='?',help="Artifact type")
+parser.add_argument('manifest', type = argparse.FileType('r'), help = "Path to local manifest file.")
+parser.add_argument('--component', nargs = '?', help = "Component name")
+parser.add_argument('--type', nargs = '?', help = "Artifact type")
 args = parser.parse_args()
 
 manifest = BuildManifest.from_file(args.manifest)
-basepath = os.path.dirname(args.manifest.name)
-print("hello",basepath)
+basepath = os.path.dirname(os.path.abspath(args.manifest.name))
+signer = Signer()
 
 signer = Signer()
 for component in manifest.components:
-    if (not args.component) or ( args.component and component.name == args.component):
-        print(f'signing {component.name}')
-        for artifact_type in component.artifacts:
-            if (not args.type) or (args.type and artifact_type == args.type):
-                artifact_list = component.artifacts[artifact_type]
-                for artifact in artifact_list:
-                    location = os.path.join(basepath, artifact)
-                    print("LOCATION=", location)
-                    signer.sign(location)
-                    signer.verify(location + ".asc")
-signer.close()
+    
+    if args.component and args.component != component.name:
+        print(f'\nSkipping {component.name}')
+        continue
+    
+    print(f'\nSigning {component.name}')
+    for artifact_type in component.artifacts:
+        
+        if args.type and args.type != artifact_type:
+            continue
+        
+        artifact_list = component.artifacts[artifact_type]
+        for artifact in artifact_list:
+            location = os.path.join(basepath, artifact)
+            signer.sign(location)
+            signer.verify(location + ".asc")
 
 print('Done.')
--- bundle-workflow/python/signing_workflow/signer.py
+++ bundle-workflow/python/signing_workflow/signer.py
@@ -2,12 +2,11 @@
 
 # Copyright OpenSearch Contributors.
 # SPDX-License-Identifier: Apache-2.0
+import sys 
 
-import os
-import subprocess
-import tempfile
-import uuid
-import argparse
+sys.path.insert(0,"../git")
+from git.git_repository import GitRepository
+
 
 '''
 This class is responsible for signing an artifact using the OpenSearch-signer-client and verifying its signature.
@@ -15,20 +14,15 @@ The signed artifacts will be found in the same location as the original artifact
 '''
 class Signer:
     def __init__(self):
-        subprocess.check_output(["git","clone","https://github.com/opensearch-project/opensearch-signer-client.git"])
-        subprocess.check_output(["./bootstrap"],cwd='opensearch-signer-client/src')
-        subprocess.check_output(["rm","config.cfg"],cwd='opensearch-signer-client/src')
+        self.git_repo = GitRepository("https://github.com/opensearch-project/opensearch-signer-client.git", "HEAD")
+        self.git_repo.execute("./bootstrap", "src")
+        self.git_repo.execute("rm config.cfg", "src")
 
     def sign(self, filename):
-        signatureFile = filename + ".asc"
-        basepath = os.path.basename(filename)
-        signing_cmd=['./opensearch-signer-client', '-i', filename, '-o', signatureFile, '-p', 'pgp']
-        subprocess.check_output(signing_cmd,cwd="opensearch-signer-client/src")
+        signature_file = filename + ".asc"
+        signing_cmd = ['./opensearch-signer-client', '-i', filename, '-o', signature_file, '-p', 'pgp']
+        self.git_repo.execute(" ".join(signing_cmd), "src")
 
     def verify(self, filename):
-        verify_cmd = ['gpg', '--verify-files',filename]
-        subprocess.check_output(verify_cmd, cwd="opensearch-signer-client/src")
-
-    def close(self):
-        subprocess.run(["rm","-rf","opensearch-signer-client"])
-
+        verify_cmd = ['gpg', '--verify-files', filename]
+        self.git_repo.execute(" ".join(verify_cmd))
