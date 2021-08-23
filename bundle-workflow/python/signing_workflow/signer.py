b8c852699d5ea0889a4e004ea50efce5a145463c
Marc Handalian <handalm@amazon.com>
2021-08-23 09:34:36 -0700

    Update signer repo url to require a github token. (#290)
    Signed-off-by: Marc Handalian <handalm@amazon.com>

--- bundle-workflow/python/signing_workflow/signer.py
+++ bundle-workflow/python/signing_workflow/signer.py
@@ -2,11 +2,11 @@
 
 # Copyright OpenSearch Contributors.
 # SPDX-License-Identifier: Apache-2.0
-import sys 
+import sys
+import os
 
 sys.path.insert(0,"../git")
 from git.git_repository import GitRepository
-
 
 '''
 This class is responsible for signing an artifact using the OpenSearch-signer-client and verifying its signature.
@@ -14,9 +14,14 @@ The signed artifacts will be found in the same location as the original artifact
 '''
 class Signer:
     def __init__(self):
-        self.git_repo = GitRepository("https://github.com/opensearch-project/opensearch-signer-client.git", "HEAD")
+        self.git_repo = GitRepository(self.get_repo_url(), "HEAD")
         self.git_repo.execute("./bootstrap", subdirname = "src")
         self.git_repo.execute("rm config.cfg", subdirname = "src")
+
+    def get_repo_url(self):
+        if "GITHUB_TOKEN" in os.environ:
+            return "https://${GITHUB_TOKEN}@github.com/opensearch-project/opensearch-signer-client.git"
+        return "https://github.com/opensearch-project/opensearch-signer-client.git"
 
     def sign(self, filename):
         signature_file = filename + ".asc"
