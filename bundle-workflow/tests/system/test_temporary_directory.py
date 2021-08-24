3ed043cb913d6b1718df8bbc0e3fd0d493c3269e
dblock <dblock@dblock.org>
2021-08-24 21:18:57 +0000

    Tests for TemporaryDirectory.
    Signed-off-by: dblock <dblock@dblock.org>

---
+++ bundle-workflow/tests/system/test_temporary_directory.py
@@ -0,0 +1,17 @@
+import unittest
+import os
+from unittest.mock import MagicMock
+
+from src.system.temporary_directory import TemporaryDirectory
+
+
+class TestTemporaryDirectory(unittest.TestCase):
+    def test_keep_true(self):
+        with TemporaryDirectory(keep=True) as work_dir:
+            self.assertTrue(os.path.exists(work_dir))
+        self.assertTrue(os.path.exists(work_dir))
+
+    def test_keep_faklse(self):
+        with TemporaryDirectory() as work_dir:
+            self.assertTrue(os.path.exists(work_dir))
+        self.assertFalse(os.path.exists(work_dir))
