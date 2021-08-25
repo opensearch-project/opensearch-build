60af2d27284996b9b87e6053d6c8803b7e7b742d
meghasaik <kavmegha@amazon.com>
2021-08-25 09:52:53 -0700

    Style changes and comments made

--- bundle-workflow/src/read_write_s3.py
+++ bundle-workflow/src/read_write_s3.py
@@ -9,14 +9,21 @@ import boto3
 
 class read_write_files:
 
-    #get_S3Objects:Used for getting the files from s3
+    # get_S3Objects:Used for getting the files from S3
+    # bucketname: Name of the bucket you want to get the file from
+    # RoleArn: Amazon Resource Name (ARN) of the role that you want to assume
+    # RoleSessionName: Name for the session
+    # bucket_bundle: Object path from where you want to get the bundle
+    # bucket_manifest: Object path from where you want to get the manifest
+    # path_tosave_Bundle: Path where you want to save the bundle at
+    # path_tosave_Manifest: Path where you want to save the manifest at
     def get_S3Objects(self, bucketname, RoleArn, RoleSessionName, bucket_bundle, bucket_manifest, path_tosave_Bundle, path_tosave_Manifest):
         sts_connection = boto3.client('sts')
         acct_b = sts_connection.assume_role(
             RoleArn=RoleArn,
             RoleSessionName=RoleSessionName,
             DurationSeconds=3600)['Credentials']
-        print (acct_b)
+
 
         client = boto3.client('s3',
                               aws_access_key_id=acct_b['AccessKeyId'],
@@ -24,19 +31,21 @@ class read_write_files:
                               aws_session_token=acct_b['SessionToken'])
 
         try:
-            result_bundle = client.download_file(bucketname, bucket_bundle, path_tosave_Bundle)
-            result_manifest = client.download_file(bucketname, bucket_manifest, path_tosave_Manifest)
+            client.download_file(bucketname, bucket_bundle, path_tosave_Bundle)
+            client.download_file(bucketname, bucket_manifest, path_tosave_Manifest)
         except:
             print("An exception occurred. No file is present in the bucket.")
 
-    #put_S3Objects:Used for putting the files into s3
+    # put_S3Objects:Used for putting the files into S3
+    # filename: The file that you want to put in the S3 bucket
+    # filePath: The Object path where you want to save the file
     def put_S3Objects(self, bucketname, RoleArn, RoleSessionName, filename, filePath):
         sts_connection = boto3.client('sts')
         acct_b = sts_connection.assume_role(
             RoleArn=RoleArn,
             RoleSessionName=RoleSessionName,
             DurationSeconds=3600)['Credentials']
-        print (acct_b)
+
 
         client = boto3.client('s3',
                               aws_access_key_id=acct_b['AccessKeyId'],
@@ -44,9 +53,6 @@ class read_write_files:
                               aws_session_token=acct_b['SessionToken'])
 
         try:
-            result = client.upload_file(filename, bucketname, filePath)
+            client.upload_file(filename, bucketname, filePath)
         except:
             print("An exception occurred. No file found for uploading.")
-
-
-
