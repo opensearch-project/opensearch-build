4cc45d988304cde44ee65017cd039214d7cb523e
Owais Kazi <owaiskazi19@gmail.com>
2021-08-24 19:06:41 -0700

    Jenkins test
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>

--- bundle-workflow/src/perf_test.py
+++ bundle-workflow/src/perf_test.py
@@ -5,7 +5,6 @@ import yaml
 
 from git.git_repository import GitRepository
 from manifests.bundle_manifest import BundleManifest
-from system.temporary_directory import TemporaryDirectory
 from test_workflow.perf_test_cluster import PerformanceTestCluster
 from test_workflow.perf_test_suite import PerformanceTestSuite
 
@@ -24,24 +23,22 @@ config = yaml.load(args.config, Loader=yaml.FullLoader)
 
 workspace = os.getcwd()
 
+os.chdir(workspace)
+current_workspace = os.path.join(workspace, 'infra66')
+cloned_repo = GitRepository(f'https://{args.token}:x-oauth-basic@github.com/opensearch-project/opensearch-infra', 'main', current_workspace)
+security = False
+for component in manifest.components:
+    if component.name == 'security':
+        security = True
+os.chdir(current_workspace)
+perf_cluster = PerformanceTestCluster(manifest, config, args.stack, security)
+try:
+    perf_cluster.create()
 
-with TemporaryDirectory(keep=args.keep) as work_dir:
-    os.chdir(work_dir)
-    current_workspace = os.path.join(workspace, 'infra56')
-    cloned_repo = GitRepository(f'https://{args.token}:x-oauth-basic@github.com/opensearch-project/opensearch-infra', 'main', current_workspace)
-    security = False
-    for component in manifest.components:
-        if component.name == 'security':
-            security = True
+    # IP of the cluster
+    endpoint = perf_cluster.endpoint()
     os.chdir(current_workspace)
-    perf_cluster = PerformanceTestCluster(manifest, config, args.stack, security)
-    try:
-        perf_cluster.create()
-
-        # IP of the cluster
-        endpoint = perf_cluster.endpoint()
-        os.chdir(current_workspace)
-        perf_test_suite = PerformanceTestSuite(manifest, endpoint, security)
-        perf_test_suite.execute()
-    except:
-        perf_cluster.destroy()
+    perf_test_suite = PerformanceTestSuite(manifest, endpoint, security)
+    perf_test_suite.execute()
+except:
+    perf_cluster.destroy()
--- bundle-workflow/src/test_workflow/perf_test_cluster.py
+++ bundle-workflow/src/test_workflow/perf_test_cluster.py
@@ -26,9 +26,9 @@ class PerformanceTestCluster(TestCluster):
         if self.security:
             security = 'enable'
 
-        command = f'cdk deploy --all -c url={self.manifest.build.location} -c security_group_id={self.security_id} -c vpc_id={self.vpc_id} '
-        f'-c account_id={self.account_id} -c region={self.region} -c stack_name={self.stack_name} -c security={security} '
-        f'-c architecture={self.manifest.build.architecture} --require-approval=never --profile infra --outputs-file {self.output_file}'
+        command = f'cdk deploy --all -c url={self.manifest.build.location} -c security_group_id={self.security_id} -c vpc_id={self.vpc_id} '\
+                  f'-c account_id={self.account_id} -c region={self.region} -c stack_name={self.stack_name} -c security={security} '\
+                  f'-c architecture={self.manifest.build.architecture} --require-approval=never --profile infra --outputs-file {self.output_file}'
         print(f'Executing "{command}" in {dir}')
         subprocess.check_call(command, cwd=dir, shell=True)
         with open(self.output_file, 'r') as read_file:
@@ -49,8 +49,8 @@ class PerformanceTestCluster(TestCluster):
         if self.security:
             security = 'enable'
 
-        command = f'cdk destroy --all -c url={self.manifest.build.location} -c security_group_id={self.security_id} -c vpc_id={self.vpc_id} '
-        f'-c account_id={self.account_id} -c region={self.region} -c stack_name={self.stack_name} -c security={security} '
-        f'-c architecture={self.manifest.build.architecture} --profile infra --require-approval=never'
+        command = f'cdk destroy --all -c url={self.manifest.build.location} -c security_group_id={self.security_id} -c vpc_id={self.vpc_id} '\
+                  f'-c account_id={self.account_id} -c region={self.region} -c stack_name={self.stack_name} -c security={security} '\
+                  f'-c architecture={self.manifest.build.architecture} --profile infra --require-approval=never'
         print(f'Executing "{command}" in {dir}')
         subprocess.check_call(command, cwd=dir, shell=True)
