3315ffe33d89c290461e2e2071f5fa940870f093
Owais Kazi <owaiskazi19@gmail.com>
2021-08-24 19:11:14 -0700

    Changes before PR
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>

--- bundle-workflow/src/perf_test.py
+++ bundle-workflow/src/perf_test.py
@@ -13,7 +13,6 @@ parser.add_argument('manifest', type=argparse.FileType('r'), help="Manifest file
 parser.add_argument('--keep', dest='keep', action='store_true', help="Do not delete the working temporary directory.")
 parser.add_argument('--stack', dest='stack', help='Stack name for performance test')
 parser.add_argument('config', type=argparse.FileType('r'), help="Config file.")
-parser.add_argument("-t", '--token', help="Github Token")
 
 args = parser.parse_args()
 
@@ -24,8 +23,8 @@ config = yaml.load(args.config, Loader=yaml.FullLoader)
 workspace = os.getcwd()
 
 os.chdir(workspace)
-current_workspace = os.path.join(workspace, 'infra66')
-cloned_repo = GitRepository(f'https://{args.token}:x-oauth-basic@github.com/opensearch-project/opensearch-infra', 'main', current_workspace)
+current_workspace = os.path.join(workspace, 'infra')
+cloned_repo = GitRepository(config['Constants']['Repository'], 'main', current_workspace)
 security = False
 for component in manifest.components:
     if component.name == 'security':
