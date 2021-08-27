c747ccae7773b27ef8a845ef4e95d7059654a092
Owais Kazi <owaiskazi19@gmail.com>
2021-08-27 15:58:45 -0700

    Addressed PR Comments
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>

--- bundle-workflow/src/perf_test.py
+++ bundle-workflow/src/perf_test.py
@@ -9,17 +9,17 @@ from test_workflow.perf_test_cluster import PerformanceTestCluster
 from test_workflow.perf_test_suite import PerformanceTestSuite
 
 parser = argparse.ArgumentParser(description="Test an OpenSearch Bundle")
-parser.add_argument('manifest', type=argparse.FileType('r'), help="Manifest file.")
+parser.add_argument('--bundle-manifest', type=argparse.FileType('r'), help="Bundle Manifest file.")
 parser.add_argument('--stack', dest='stack', help='Stack name for performance test')
-parser.add_argument('config', type=argparse.FileType('r'), help="Config file.")
+parser.add_argument('--config', type=argparse.FileType('r'), help="Config file.")
 args = parser.parse_args()
 
-manifest = BundleManifest.from_file(args.manifest)
+manifest = BundleManifest.from_file(args.bundle_manifest)
 
 config = yaml.load(args.config, Loader=yaml.FullLoader)
 
 
-def get_repo_url():
+def get_infra_repo_url():
     if "GITHUB_TOKEN" in os.environ:
         return "https://${GITHUB_TOKEN}@github.com/opensearch-project/opensearch-infra.git"
     return "https://github.com/opensearch-project/opensearch-infra.git"
@@ -27,7 +27,7 @@ def get_repo_url():
 
 os.chdir(os.getcwd())
 current_workspace = os.path.join(os.getcwd(), 'infra')
-cloned_repo = GitRepository(get_repo_url(), 'main', current_workspace)
+cloned_repo = GitRepository(get_infra_repo_url(), 'main', current_workspace)
 security = False
 for component in manifest.components:
     if component.name == 'security':
@@ -42,6 +42,6 @@ try:
     os.chdir(current_workspace)
     perf_test_suite = PerformanceTestSuite(manifest, endpoint, security)
     perf_test_suite.execute()
-except:
+finally:
     os.chdir(current_workspace)
     perf_cluster.destroy()
