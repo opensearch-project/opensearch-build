6314e5f54c36e0ae381ad8f3ddbad890b5516ca5
Cameron Skinner <68246816+camerski@users.noreply.github.com>
2021-08-27 10:51:17 -0700

    Implement TestRecorder stub and wire up IntegTestSuite, LocalTestCluster, and test.py (#284)
    * Implement TestRecorder stub and wire up IntegTestSuite, LocalTestCluster, and test.py
    
    Also add system.execute to make it easier to capture exit codes, stdout, and stderr from subprocesses.
    
    Signed-off-by: Cameron Skinner <camerski@amazon.com>
    Co-authored-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- bundle-workflow/src/test.py
+++ bundle-workflow/src/test.py
@@ -15,6 +15,7 @@ from paths.script_finder import ScriptFinder
 from system.temporary_directory import TemporaryDirectory
 from test_workflow.integ_test_suite import IntegTestSuite
 from test_workflow.local_test_cluster import LocalTestCluster
+from test_workflow.test_recorder import TestRecorder
 
 parser = argparse.ArgumentParser(description="Test an OpenSearch Bundle")
 parser.add_argument("manifest", type=argparse.FileType("r"), help="Manifest file.")
@@ -28,6 +29,7 @@ args = parser.parse_args()
 
 manifest = BundleManifest.from_file(args.manifest)
 script_finder = ScriptFinder()
+test_recorder = TestRecorder(os.path.dirname(args.manifest.name))
 
 with TemporaryDirectory(keep=args.keep) as work_dir:
     os.chdir(work_dir)
@@ -40,15 +42,15 @@ with TemporaryDirectory(keep=args.keep) as work_dir:
 
         # For each component, check out the git repo and run `integtest.sh`
         for component in manifest.components:
-            print(component.name)
+            print(f'Testing {component.name}')
             repo = GitRepository(
                 component.repository,
                 component.commit_id,
                 os.path.join(work_dir, component.name),
             )
-            test_suite = IntegTestSuite(component.name, repo, script_finder)
+            test_suite = IntegTestSuite(component.name, repo, script_finder, test_recorder)
             test_suite.execute(cluster, True)
     finally:
-        cluster.destroy()
+        cluster.destroy(test_recorder)
 
     # TODO: Store test results, send notification.
