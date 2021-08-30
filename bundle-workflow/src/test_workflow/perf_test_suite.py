136768bcbadb98354d69d8b0e92cd12facf74bbd
Owais Kazi <owaiskazi19@gmail.com>
2021-08-30 11:12:20 -0700

    PR Comments
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>

--- bundle-workflow/src/test_workflow/perf_test_suite.py
+++ bundle-workflow/src/test_workflow/perf_test_suite.py
@@ -12,7 +12,7 @@ class PerformanceTestSuite:
         self.work_dir = 'tools/cdk/mensor/mensor_tests'
         self.endpoint = endpoint
         self.security = security
-        self.command = f'python3 test_config.py -i {self.endpoint} -b {self.manifest.build.id}'\
+        self.command = f'python test_config.py -i {self.endpoint} -b {self.manifest.build.id}'\
                        f' -a {self.manifest.build.architecture} '
 
     def execute(self):
@@ -20,7 +20,7 @@ class PerformanceTestSuite:
         dir = os.getcwd()
         # Install the depedencies for the private repo
         # TODO Move the installation to the repo itself
-        subprocess.check_call('pip3 install boto3 requests setuptools retry dataclasses_json', cwd=dir, shell=True)
+        subprocess.check_call('pip install boto3 requests setuptools retry dataclasses_json', cwd=dir, shell=True)
 
         if self.security:
             subprocess.check_call(f'{self.command} -s', cwd=dir, shell=True)
