75ca38d7d6f3b1ce86cbf3b5423312428238e37b
Owais Kazi <owaiskazi19@gmail.com>
2021-08-30 14:42:11 -0700

    Moved dependency installation to private repo
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>

--- bundle-workflow/src/test_workflow/perf_test_suite.py
+++ bundle-workflow/src/test_workflow/perf_test_suite.py
@@ -12,16 +12,15 @@ class PerformanceTestSuite:
         self.work_dir = 'tools/cdk/mensor/mensor_tests'
         self.endpoint = endpoint
         self.security = security
-        self.command = f'python test_config.py -i {self.endpoint} -b {self.manifest.build.id}'\
+        self.command = f'pipenv run python test_config.py -i {self.endpoint} -b {self.manifest.build.id}'\
                        f' -a {self.manifest.build.architecture} '
 
     def execute(self):
         os.chdir(self.work_dir)
         dir = os.getcwd()
         # Install the depedencies for the private repo
-        # TODO Move the installation to the repo itself
-        subprocess.check_call('pip install boto3 requests setuptools retry dataclasses_json', cwd=dir, shell=True)
-
+        subprocess.check_call('python3 -m pipenv install', cwd=dir, shell=True)
+        subprocess.check_call('pipenv install', cwd=dir, shell=True)
         if self.security:
             subprocess.check_call(f'{self.command} -s', cwd=dir, shell=True)
         else:
