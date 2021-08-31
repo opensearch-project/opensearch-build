248d6bcba54cf5e6f77fac2653bc4f809b475007
Himanshu Setia <58999915+setiah@users.noreply.github.com>
2021-08-31 10:01:07 -0700

    Integ tests changes  (#338)
    This PR adds support for running integration tests for index-management plugin as part of the CICD framework. It also provides a template implementation for test_manifest, which is a configuration file that defines test support matrix for components of a bundle. This file is schema-version'ed and subject to change in future as the template evolves.  Currently, the dependency management for components is done in a legacy way by building dependencies for job-scheduler and notifications. This logic will be replaced soon with DependencyInstaller #283 . 
    
    * Integ tests changes - working integtests, code refactoring, dependency management, test manifest
    
    Signed-off-by: Himanshu Setia <setiah@amazon.com>
    
    * Addressing PR comments
    
    Signed-off-by: Himanshu Setia <setiah@amazon.com>
    
    * Adding licensing info to file
    
    Signed-off-by: Himanshu Setia <setiah@amazon.com>
    
    * correcting import order with pipenv isort
    
    Signed-off-by: Himanshu Setia <setiah@amazon.com>
    
    * resolving merge conflicts
    
    Signed-off-by: Himanshu Setia <setiah@amazon.com>
    
    * Removed placeholder bundle and build manifest files
    
    Signed-off-by: Himanshu Setia <setiah@amazon.com>
    
    * Removing unwanted integtest code from test.py
    
    Signed-off-by: Himanshu Setia <setiah@amazon.com>
    
    * pipenv isort fixes
    
    Signed-off-by: Himanshu Setia <setiah@amazon.com>
    
    * pipenv flake fixes
    
    Signed-off-by: Himanshu Setia <setiah@amazon.com>

--- bundle-workflow/src/test.py
+++ bundle-workflow/src/test.py
@@ -9,12 +9,8 @@
 import os
 
 from manifests.bundle_manifest import BundleManifest
-from system.temporary_directory import TemporaryDirectory
 from test_workflow.bwc_test_suite import BwcTestSuite
-from test_workflow.integ_test_suite import IntegTestSuite
-from test_workflow.local_test_cluster import LocalTestCluster
 from test_workflow.test_args import TestArgs
-from test_workflow.test_component import TestComponent
 from test_workflow.test_recorder import TestRecorder
 
 args = TestArgs()
@@ -22,34 +18,9 @@ manifest = BundleManifest.from_file(args.manifest)
 test_recorder = TestRecorder(os.path.dirname(manifest.name))
 
 
-def integ_test_suite():
-    with TemporaryDirectory(keep=args.keep) as work_dir:
-        os.chdir(work_dir)
-
-        # Spin up a test cluster
-        cluster = LocalTestCluster(manifest)
-        cluster.create()
-
-        # For each component, check out the git repo and run `integtest.sh`
-        try:
-            for component in manifest.components:
-                print(component.name)
-                test_component = TestComponent(
-                    component.repository, component.commit_id
-                )
-                repo = test_component.checkout(os.path.join(work_dir, component.name))
-                test_suite = IntegTestSuite(component.name, repo)
-                test_suite.execute(cluster)
-        finally:
-            cluster.destroy()
-
-        # TODO: Store test results, send notification.
-
-
 def bwc_test_suite():
     test_suite = BwcTestSuite(manifest, args.component, args.keep)
     test_suite.execute()
 
 
-integ_test_suite()
 bwc_test_suite()
