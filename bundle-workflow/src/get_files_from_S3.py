7637e665d4edef098f1dd355db6d2bf24e538f96
meghasaik <kavmegha@amazon.com>
2021-08-31 13:06:10 -0700

    Made changes to the both the files given.

--- bundle-workflow/src/get_files_from_S3.py
+++ bundle-workflow/src/get_files_from_S3.py
@@ -4,14 +4,23 @@
 # The OpenSearch Contributors require contributions made to
 # this file be licensed under the Apache-2.0 license or a
 # compatible open source license.
-
-
 import argparse
+from enum import Enum
 
 import read_write_s3
 
 
+class DocumentType(Enum):
+    FILE = "file"
+    FOLDER = "folder"
+
+    @classmethod
+    def has_value(cls, value):
+        return value in cls._value2member_map_
+
+
 class GetBundleManifest:
+
     def parse_arguments(self):
         parser = argparse.ArgumentParser()
         parser.add_argument("-a", "--assume_role", nargs="?", default="os.getenv('OPENSEARCH_TESTING_ROLE')", help="Assume role")
@@ -19,13 +28,17 @@ class GetBundleManifest:
         parser.add_argument("-b", "--bucket_name", help="Bucketname", required=True)
         parser.add_argument("-o", "--object_bundle", help="ObjectPath in Bucket for Bundle", required=True)
         parser.add_argument("-bp", "--bundle_path", help="Path for storing the bundle locally", required=True)
+        parser.add_argument("-f", "--file", help="Parameter for either file or folder", default="folder")
 
         args = parser.parse_args()
+
+        if not (DocumentType.has_value(args.file)):
+            raise ValueError("The document type given is not present. Document types to be given are file or folder")
         return args
 
     def download_files_from_S3(self, args):
-        objectname = read_write_s3.ReadWriteFiles(args.assume_role, args.session)
-        objectname.get_S3_objects(args.bucket_name, args.object_bundle, args.bundle_path)
+        objectname = read_write_s3.S3Bucket(args.assume_role, args.session, args.bucket_name)
+        objectname.download(args.object_bundle, args.bundle_path, args.file)
 
 
 getbm = GetBundleManifest()
--- bundle-workflow/src/read_write_s3.py
+++ bundle-workflow/src/read_write_s3.py
@@ -3,13 +3,12 @@
 # The OpenSearch Contributors require contributions made to
 # this file be licensed under the Apache-2.0 license or a
 # compatible open source license.
-
 import os
 
 import boto3
 
 
-class ReadWriteFiles:
+class S3Bucket:
     """
     get_S3Objects:Used for getting the files from S3
     bucket_name: Name of the bucket you want to get the file from
@@ -17,37 +16,40 @@ class ReadWriteFiles:
     Here, default value for role is taken. Can be changed through -a <role-name>
     role_session_name: Name for the session
     Here, default value for session is taken. Can be changed through -s <session-name>
-    bucket_bundle: Object path from where you want to get the bundle
-    path_to_save_bundle: Path where you want to save the bundle at
+    prefix: Object path from where you want to get the bundle
+    dest: Path where you want to save the bundle at
     """
-    def __init__(self, role_arn, role_session_name):
+    def __init__(self, role_arn, role_session_name, bucket_name):
         self.role_arn = role_arn
         self.role_session_name = role_session_name
-
-    def get_S3_objects(self, bucket_name, bucket_bundle, path_to_save_bundle=None):
+        self.bucket_name = bucket_name
         sts_connection = boto3.client('sts')
         assumed_role = sts_connection.assume_role(
             RoleArn=self.role_arn,
             RoleSessionName=self.role_session_name,
             DurationSeconds=3600)['Credentials']
+        self.client = boto3.client('s3',
+                                   aws_access_key_id=assumed_role['AccessKeyId'],
+                                   aws_secret_access_key=assumed_role['SecretAccessKey'],
+                                   aws_session_token=assumed_role['SessionToken'])
 
-        boto3.client('s3',
-                     aws_access_key_id=assumed_role['AccessKeyId'],
-                     aws_secret_access_key=assumed_role['SecretAccessKey'],
-                     aws_session_token=assumed_role['SessionToken'])
+    def download(self, prefix, dest, file):
 
         try:
-            s3_resource = boto3.resource('s3')
-            my_bucket = s3_resource.Bucket(bucket_name)
-            objects = my_bucket.objects.filter(Prefix=bucket_bundle)
-            for obj in objects:
-                target = obj.key if path_to_save_bundle is None \
-                    else os.path.join(path_to_save_bundle, os.path.relpath(obj.key, bucket_bundle))
-                if not os.path.exists(os.path.dirname(target)):
-                    os.makedirs(os.path.dirname(target))
-                if obj.key[-1] == '/':
-                    continue
-                my_bucket.download_file(obj.key, target)
+            if (file == "folder"):
+                s3_resource = boto3.resource('s3')
+                my_bucket = s3_resource.Bucket(self.bucket_name)
+                objects = my_bucket.objects.filter(Prefix=prefix)
+                for obj in objects:
+                    target = obj.key if dest is None \
+                        else os.path.join(dest, os.path.relpath(obj.key, prefix))
+                    if not os.path.exists(os.path.dirname(target)):
+                        os.makedirs(os.path.dirname(target))
+                    if obj.key[-1] == '/':
+                        continue
+                    my_bucket.download_file(obj.key, target)
+            else:
+                self.client.download_file(self.bucket_name, prefix, dest)
         except Exception as e:
             print(e)
     """
@@ -55,17 +57,8 @@ class ReadWriteFiles:
     file_name: The file that you want to put in the S3 bucket
     file_path: The Object path where you want to save the file
     """
-    def put_S3_objects(self, bucket_name, file_name, file_path):
-        sts_connection = boto3.client('sts')
-        assumed_role = sts_connection.assume_role(
-            RoleArn=self.role_arn,
-            RoleSessionName=self.role_session_name,
-            DurationSeconds=3600)['Credentials']
-
-        client = boto3.client('s3', aws_access_key_id=assumed_role['AccessKeyId'], aws_secret_access_key=assumed_role['SecretAccessKey'],
-                              aws_session_token=assumed_role['SessionToken'])
-
+    def upload(self, file_name, file_path):
         try:
-            client.upload_file(file_name, bucket_name, file_path)
+            self.client.upload_file(file_name, self.bucket_name, file_path)
         except Exception as e:
             print(e)
