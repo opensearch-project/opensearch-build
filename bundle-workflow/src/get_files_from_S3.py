e1b2f7e8978059b49e022be2c5af85e46e6e0e24
meghasaik <kavmegha@amazon.com>
2021-08-30 17:07:08 -0700

    Changes made by adding default values for the assume role and session name, added comments and also made changes to download the folder

--- bundle-workflow/src/get_files_from_S3.py
+++ bundle-workflow/src/get_files_from_S3.py
@@ -14,13 +14,12 @@ import read_write_s3
 class GetBundleManifest:
     def parse_arguments(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument("-a", "--assume_role", help="Assume role")
-        parser.add_argument("-s", "--session", help="Session name")
-        parser.add_argument("-b", "--bucket_name", help="Bucketname")
-        parser.add_argument("-o", "--object_bundle", help="ObjectPath in Bucket for Bundle")
-        parser.add_argument("-m", "--object_manifest", help="ObjectPath in Bucket for Manifest")
-        parser.add_argument("-bp", "--bundlepath", help="Filepath for the bundle")
-        parser.add_argument("-mp", "--manifestpath", help="Filepath for the manifest")
+        parser.add_argument("-a", "--assume_role", nargs="?", default="${OPENSEARCH_TESTING_ROLE}", help="Assume role")
+        parser.add_argument("-s", "--session", nargs="?", default="get-files-session", help="Session name")
+        parser.add_argument("-b", "--bucket_name", help="Bucketname", required=True)
+        parser.add_argument("-o", "--object_bundle", help="ObjectPath in Bucket for Bundle", required=True)
+        parser.add_argument("-bp", "--bundle_path", help="Path for storing the bundle locally", required=True)
+
 
         args = parser.parse_args()
 
@@ -28,7 +27,7 @@ class GetBundleManifest:
 
     def download_files_from_S3(self, args):
         objectname = read_write_s3.ReadWriteFiles(args.assume_role, args.session)
-        objectname.get_S3_objects(args.bucket_name, args.object_bundle, args.object_manifest, args.bundlepath, args.manifestpath)
+        objectname.get_S3_objects(args.bucket_name, args.object_bundle, args.bundle_path)
 
 
 getbm = GetBundleManifest()
