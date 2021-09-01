a357fd9d48c707589d0f1cbec4ed95d11dff8a6f
Vacha <vachshah@amazon.com>
2021-08-31 20:44:30 -0700

    Addressing comments for tests
    Signed-off-by: Vacha <vachshah@amazon.com>

--- bundle-workflow/tests/tests_test_workflow/test_dependency_installer.py
+++ bundle-workflow/tests/tests_test_workflow/test_dependency_installer.py
@@ -10,14 +10,12 @@ from test_workflow.dependency_installer import DependencyInstaller
 class DependencyInstallerTests(unittest.TestCase):
     def setUp(self):
         self.maxDiff = None
-        with open(
-            os.path.join(
-                os.path.dirname(__file__),
-                "../tests_assemble_workflow/data/opensearch-build-1.1.0.yml",
-            )
-        ) as f:
-            self.manifest = BuildManifest.from_file(f)
-            self.dependency_installer = DependencyInstaller(self.manifest.build)
+        self.manifest_filename = os.path.join(
+            os.path.dirname(__file__),
+            "../tests_assemble_workflow/data/opensearch-build-1.1.0.yml",
+        )
+        self.manifest = BuildManifest.from_path(self.manifest_filename)
+        self.dependency_installer = DependencyInstaller(self.manifest.build)
 
     def test_install(self):
         with patch(
@@ -38,30 +36,30 @@ class DependencyInstallerTests(unittest.TestCase):
                 ))
             mock_maven_copy.assert_has_calls(
                 [
-                    call(get_test_dependencies(), maven_local_paths[0]),
-                    call(get_test_dependencies(), maven_local_paths[1]),
+                    call(self.__get_test_dependencies(), maven_local_paths[0]),
+                    call(self.__get_test_dependencies(), maven_local_paths[1]),
                 ]
             )
 
+    @staticmethod
+    def __get_test_dependencies():
+        test_dir = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "../../src/test_workflow"
+        )
+        return [
+            file_name
+            for file_name in os.listdir(test_dir)
+            if os.path.isfile(os.path.join(test_dir, file_name))
+        ]
 
-class MavenLocalFileHandlerTests(unittest.TestCase):
-    def setUp(self):
-        self.maven_local_file_handler = DependencyInstaller.MavenLocalFileHandler()
+    class MavenLocalFileHandlerTests(unittest.TestCase):
+        def setUp(self):
+            self.maven_local_file_handler = DependencyInstaller.MavenLocalFileHandler()
 
-    def test_copy(self):
-        maven_local_path = tempfile.mkdtemp()
-        test_files = get_test_dependencies()
-        self.maven_local_file_handler.copy(test_files, maven_local_path)
-        self.assertCountEqual(test_files, os.listdir(maven_local_path))
-        self.assertListEqual(test_files, os.listdir(maven_local_path))
-
-
-def get_test_dependencies():
-    test_dir = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), "../../src/test_workflow"
-    )
-    return [
-        file_name
-        for file_name in os.listdir(test_dir)
-        if os.path.isfile(os.path.join(test_dir, file_name))
-    ]
+        def test_copy(self):
+            with tempfile.TemporaryFile() as maven_local_path:
+                test_files = DependencyInstallerTests.__get_test_dependencies()
+                self.assertTrue(len(test_files) > 0)
+                self.maven_local_file_handler.copy(test_files, maven_local_path)
+                self.assertCountEqual(test_files, os.listdir(maven_local_path))
+                self.assertListEqual(sorted(test_files), sorted(os.listdir(maven_local_path)))
