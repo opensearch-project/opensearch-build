a94592124dfbfd59f2572af1a71db807c3649e86
dblock <dblock@dblock.org>
2021-09-07 20:00:35 +0000

    Check output of gradle properties and dependencies.
    Signed-off-by: dblock <dblock@dblock.org>

--- bundle-workflow/tests/tests_paths/test_script_finder.py
+++ bundle-workflow/tests/tests_paths/test_script_finder.py
@@ -64,9 +64,9 @@ class TestScriptFinder(unittest.TestCase):
 
     @patch("os.path.exists", return_value=False)
     def test_find_build_script_does_not_exist(self, *mocks):
-        with self.assertRaisesRegex(
+        with self.assertRaises(
             ScriptFinder.ScriptNotFoundError,
-            "Could not find build.sh script. Looked in .*",
+            msg="Could not find build.sh script. Looked in .*",
         ):
             ScriptFinder.find_build_script("anything", self.component_without_scripts)
 
@@ -112,9 +112,9 @@ class TestScriptFinder(unittest.TestCase):
 
     @patch("os.path.exists", return_value=False)
     def test_find_integ_test_script_does_not_exist(self, *mocks):
-        with self.assertRaisesRegex(
+        with self.assertRaises(
             ScriptFinder.ScriptNotFoundError,
-            "Could not find integtest.sh script. Looked in .*",
+            msg="Could not find integtest.sh script. Looked in .*",
         ):
             ScriptFinder.find_integ_test_script(
                 "anything", self.component_without_scripts
@@ -138,8 +138,8 @@ class TestScriptFinder(unittest.TestCase):
 
     @patch("os.path.exists", return_value=False)
     def test_find_install_script_does_not_exist(self, *mocks):
-        with self.assertRaisesRegex(
+        with self.assertRaises(
             ScriptFinder.ScriptNotFoundError,
-            "Could not find install.sh script. Looked in .*",
+            msg="Could not find install.sh script. Looked in .*",
         ):
             ScriptFinder.find_install_script("anything")
