de4f5d239bbdd825c6ac63831030dc8c3005015a
Owais Kazi <owaiskazi19@gmail.com>
2021-09-08 15:00:19 -0700

    UT change
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>

--- bundle-workflow/tests/test_perf_workflow/test_perf_test_suite.py
+++ bundle-workflow/tests/test_perf_workflow/test_perf_test_suite.py
@@ -14,8 +14,13 @@ from test_workflow.perf_test_suite import PerfTestSuite
 
 class TestPerfTestSuite(unittest.TestCase):
     def setUp(self):
-        os.chdir(os.path.dirname(__file__))
-        self.manifest = BundleManifest.from_path("data/bundle_manifest.yaml")
+        self.data_path = os.path.realpath(
+            os.path.join(os.path.dirname(__file__), "data")
+        )
+        self.manifest_filename = os.path.join(
+            self.data_path, "bundle_manifest.yaml"
+        )
+        self.manifest = BundleManifest.from_path(self.manifest_filename)
         self.endpoint = None
         self.perf_test_suite = PerfTestSuite(
             bundle_manifest=self.manifest, endpoint=None, security=False, current_workspace='current_workspace'
