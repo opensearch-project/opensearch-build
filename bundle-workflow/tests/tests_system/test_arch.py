636d31851833579554a4d45f3b7cecc206047aca
Peter Zhu <zhujiaxi@amazon.com>
2021-09-08 21:09:39 -0400

    Add more test to cover arch.py
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- bundle-workflow/tests/tests_system/test_arch.py
+++ bundle-workflow/tests/tests_system/test_arch.py
@@ -16,6 +16,18 @@ class TestArch(unittest.TestCase):
         self.assertTrue(current_arch() in ["x64", "arm64"])
 
     @patch("subprocess.check_output")
+    def test_x64_arch(self, mock_subprocess):
+        mock_subprocess.return_value = "x86_64".encode()
+        self.assertTrue(current_arch() == "x64")
+
+    @patch("subprocess.check_output")
+    def test_arm64_arch(self, mock_subprocess):
+        mock_subprocess.return_value = "aarch64".encode()
+        self.assertTrue(current_arch() == "arm64")
+        mock_subprocess.return_value = "arm64".encode()
+        self.assertTrue(current_arch() == "arm64")
+
+    @patch("subprocess.check_output")
     def test_invalid_arch(self, mock_subprocess):
         mock_subprocess.return_value = "invalid".encode()
         with self.assertRaises(ValueError) as context:
