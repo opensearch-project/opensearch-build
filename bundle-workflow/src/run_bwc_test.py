dcca5200194ec2ab6494832ec40e34a38002a361
Sarat Vemulapalli <vemulapallisarat@gmail.com>
2021-09-21 14:54:14 -0700

    Fixing BWC Test suite runner (#537)
    * Fixing BWC test suite running in Jenkins
    
    Signed-off-by: Sarat Vemulapalli <vemulapallisarat@gmail.com>

--- bundle-workflow/src/run_bwc_test.py
+++ bundle-workflow/src/run_bwc_test.py
@@ -11,7 +11,6 @@ import sys
 from manifests.bundle_manifest import BundleManifest
 from system import console
 from system.temporary_directory import TemporaryDirectory
-from system.working_directory import WorkingDirectory
 from test_workflow.bwc_test.bwc_test_suite import BwcTestSuite
 from test_workflow.test_args import TestArgs
 
@@ -21,10 +20,9 @@ def main():
     console.configure(level=args.logging_level)
     with TemporaryDirectory(keep=args.keep) as work_dir:
         logging.info("Switching to temporary work_dir: " + work_dir)
-        with WorkingDirectory(work_dir) as cur_dir:
-            bundle_manifest = BundleManifest.from_s3(
-                args.s3_bucket, args.build_id, args.opensearch_version, args.architecture, cur_dir)
-            BwcTestSuite(bundle_manifest, cur_dir, args.component, args.keep).execute()
+        bundle_manifest = BundleManifest.from_s3(
+            args.s3_bucket, args.build_id, args.opensearch_version, args.architecture, work_dir)
+        BwcTestSuite(bundle_manifest, work_dir, args.component, args.keep).execute()
 
 
 if __name__ == "__main__":
