0a209a5e2deed01f49f2c4afcf563b1ef096a589
Daniel Doubrovkine (dB.) <dblock@dblock.org>
2021-09-24 19:23:40 -0400

    Refactored default build.sh scripts. (#594)
    Signed-off-by: dblock <dblock@dblock.org>

--- bundle-workflow/scripts/default/opensearch/build.sh
+++ bundle-workflow/scripts/default/opensearch/build.sh
@@ -20,7 +20,7 @@ function usage() {
     echo -e "-h help"
 }
 
-while getopts ":h:v:s:o:a:d:" arg; do
+while getopts ":h:v:s:o:a:" arg; do
     case $arg in
         h)
             usage
@@ -37,9 +37,6 @@ while getopts ":h:v:s:o:a:d:" arg; do
             ;;
         a)
             ARCHITECTURE=$OPTARG
-            ;;
-        d)
-            DASHBOARDS=$OPTARG
             ;;
         :)
             echo "Error: -${OPTARG} requires an argument"
@@ -59,32 +56,16 @@ if [ -z "$VERSION" ]; then
     exit 1
 fi
 
-[[ "$SNAPSHOT" == "true" && "$DASHBOARDS" == "false" ]] && VERSION=$VERSION-SNAPSHOT
+[[ "$SNAPSHOT" == "true" ]] && VERSION=$VERSION-SNAPSHOT
 [ -z "$OUTPUT" ] && OUTPUT=artifacts
 
 mkdir -p $OUTPUT
 
-if [[ "$DASHBOARDS" == "true" ]]; then
-    mkdir -p $OUTPUT/plugins
-    PLUGIN_NAME=$(basename "$PWD")
-    # TODO: [CLEANUP] Needed OpenSearch Dashboards git repo to build the required modules for plugins
-    # This makes it so there is a dependency on having Dashboards pulled already.
-    cp -r ../$PLUGIN_NAME/ ../OpenSearch-Dashboards/plugins
-    echo "BUILD MODULES FOR $PLUGIN_NAME"
-    (cd ../OpenSearch-Dashboards && yarn osd bootstrap)
-    echo "BUILD RELEASE ZIP FOR $PLUGIN_NAME"
-    (cd ../OpenSearch-Dashboards/plugins/$PLUGIN_NAME && yarn plugin-helpers build)
-    echo "COPY $PLUGIN_NAME.zip"
-    cp -r ../OpenSearch-Dashboards/plugins/$PLUGIN_NAME/build/$PLUGIN_NAME-$VERSION.zip $OUTPUT/plugins/
-else
-    ./gradlew assemble --no-daemon --refresh-dependencies -DskipTests=true -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT
+./gradlew assemble --no-daemon --refresh-dependencies -DskipTests=true -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT
 
-    zipPath=$(find . -path \*build/distributions/*.zip)
-    distributions="$(dirname "${zipPath}")"
+zipPath=$(find . -path \*build/distributions/*.zip)
+distributions="$(dirname "${zipPath}")"
 
-    echo "COPY ${distributions}/*.zip"
-    mkdir -p $OUTPUT/plugins
-    cp ${distributions}/*.zip ./$OUTPUT/plugins
-fi
-
-
+echo "COPY ${distributions}/*.zip"
+mkdir -p $OUTPUT/plugins
+cp ${distributions}/*.zip ./$OUTPUT/plugins
