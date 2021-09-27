c3bc5cef34a35223c050158c95124640f25ad4bc
Daniel Doubrovkine (dB.) <dblock@dblock.org>
2021-09-27 13:28:35 -0400

    Refactor bundle into OpenSearch and OpenSearch Dashboards. (#601)
    Signed-off-by: dblock <dblock@dblock.org>

---
+++ bundle-workflow/tests/tests_assemble_workflow/test_bundles.py
@@ -0,0 +1,53 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+
+import os
+import unittest
+from unittest.mock import MagicMock
+
+from assemble_workflow.bundle_opensearch import BundleOpenSearch
+from assemble_workflow.bundle_opensearch_dashboards import \
+    BundleOpenSearchDashboards
+from assemble_workflow.bundles import Bundles
+from manifests.build_manifest import BuildManifest
+
+
+class TestBundles(unittest.TestCase):
+    def test_bundle_opensearch(self):
+        manifest_path = os.path.join(
+            os.path.dirname(__file__), "data/opensearch-build-1.1.0.yml"
+        )
+        artifacts_path = os.path.join(os.path.dirname(__file__), "data/artifacts")
+        bundle = Bundles.create(
+            BuildManifest.from_path(manifest_path), artifacts_path, MagicMock()
+        )
+        self.assertIs(type(bundle), BundleOpenSearch)
+
+    def test_bundle_opensearch_dashboards(self):
+        manifest_path = os.path.join(
+            os.path.dirname(__file__), "data/opensearch-dashboards-build-1.1.0.yml"
+        )
+        artifacts_path = os.path.join(os.path.dirname(__file__), "data/artifacts")
+        bundle = Bundles.create(
+            BuildManifest.from_path(manifest_path), artifacts_path, MagicMock()
+        )
+        self.assertIs(type(bundle), BundleOpenSearchDashboards)
+
+    def test_bundle_opensearch_invalid(self):
+        manifest = BuildManifest(
+            {
+                "schema-version": "1.0",
+                "build": {
+                    "name": "invalid",
+                    "architecture": "x86",
+                    "id": "id",
+                    "version": "1.0.0",
+                },
+            }
+        )
+        with self.assertRaises(ValueError) as ctx:
+            Bundles.create(manifest, "path", MagicMock())
+        self.assertEqual(str(ctx.exception), "Unsupported bundle: invalid")
