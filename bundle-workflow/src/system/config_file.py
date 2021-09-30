1de226ed0eeaa66b3d0179b93b98193e6836c88b
dblock <dblock@dblock.org>
2021-09-30 23:03:36 +0000

    Check values in opensearch-dashboards/pliugin/opensearch_dashboards.json.
    Signed-off-by: dblock <dblock@dblock.org>

---
+++ bundle-workflow/src/system/config_file.py
@@ -0,0 +1,65 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+
+import json
+
+
+class ConfigFile:
+    class CheckError(Exception):
+        pass
+
+    class UnexpectedKeyValueError(CheckError):
+        def __init__(self, key, expected, current=None):
+            super().__init__(
+                f"Expected to have {key}='{expected}', but was '{current}'."
+                if current
+                else f"Expected to have {key}='{expected}', but none was found."
+            )
+
+    class UnexpectedKeyValuesError(CheckError):
+        def __init__(self, key, expected, current=None):
+            super().__init__(
+                f"Expected to have {key}=any of {expected}, but was '{current}'."
+                if current
+                else f"Expected to have {key}=any of {expected}, but none was found."
+            )
+
+    @property
+    def data(self):
+        return self.__data
+
+    def __init__(self, data=None):
+        if type(data) is str:
+            self.__data = json.loads(data)
+        elif type(data) is dict:
+            self.__data = data
+        elif data is not None:
+            raise TypeError()
+        else:
+            self.__data = {}
+
+    def get_value(self, key, default_value=None):
+        try:
+            return self.__data[key]
+        except KeyError:
+            return default_value
+
+    def check_value(self, key, expected):
+        try:
+            value = self.__data[key]
+            if value != expected:
+                raise ConfigFile.UnexpectedKeyValueError(key, expected, value)
+        except KeyError:
+            raise ConfigFile.UnexpectedKeyValueError(key, expected)
+
+    def check_value_in(self, key, expected):
+        try:
+            value = self.__data[key]
+            if value not in expected:
+                raise ConfigFile.UnexpectedKeyValuesError(key, expected, value)
+        except KeyError:
+            if None not in expected:
+                raise ConfigFile.UnexpectedKeyValuesError(key, expected)
