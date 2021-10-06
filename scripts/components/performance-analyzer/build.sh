69471a6cb287e9af4d46f77905f96385520878b1
dblock <dblock@dblock.org>
2021-10-06 17:29:46 +0000

    Removed PA and PA RCA from Maven publication.
    Signed-off-by: dblock <dblock@dblock.org>

--- scripts/components/performance-analyzer/build.sh
+++ scripts/components/performance-analyzer/build.sh
@@ -61,7 +61,3 @@ fi
 ./gradlew build -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT -x test
 mkdir -p $OUTPUT/plugins
 cp ./build/distributions/*.zip $OUTPUT/plugins
-
-mkdir -p $OUTPUT/maven/org/opensearch
-./gradlew publishToMavenLocal -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT
-cp -r ~/.m2/repository/org/opensearch/opensearch-performance-analyzer $OUTPUT/maven/org/opensearch
