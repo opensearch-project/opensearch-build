d4740b61087da5ebf1dfdd0511bf0d658f3e6032
Sayali Gaikawad <gaiksaya@amazon.com>
2021-10-25 19:21:18 -0700

    Fix S3 permissions and jenkins module names
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- deployment/lib/buckets.ts
+++ deployment/lib/buckets.ts
@@ -1,12 +1,12 @@
 import { AnyPrincipal, Role } from '@aws-cdk/aws-iam';
 import { Bucket, IBucket } from '@aws-cdk/aws-s3';
 
-import { JenkinsArtifactStack } from './jenkins-artifact-stack';
+import { BuildArtifactStack } from './build-artifact-stack';
 
 export class Buckets {
   readonly BuildBucket: IBucket;
 
-  constructor(stack: JenkinsArtifactStack, buildBucketArn?: string) {
+  constructor(stack: BuildArtifactStack, buildBucketArn?: string) {
     this.BuildBucket = buildBucketArn
       ? Bucket.fromBucketArn(stack, 'BuildBucket', buildBucketArn)
       : new Bucket(stack, 'BuildBucket', {});
--- deployment/lib/identities.ts
+++ deployment/lib/identities.ts
@@ -37,13 +37,14 @@ export class Identities {
         assumedBy: props.buildAgentPrinciple,
       });
 
-    props.buildBucket.grantPut(buildRole, 'builds/*');
-    props.buildBucket.grantRead(bundleRole, 'builds/*');
+    props.buildBucket.grantPut(buildRole, '*/builds/*');
 
-    props.buildBucket.grantPut(bundleRole, 'dist/*');
-    props.buildBucket.grantRead(testRole, 'dist/*');
+    props.buildBucket.grantRead(bundleRole, '*/builds/*');
+    props.buildBucket.grantPut(bundleRole, '*/builds/*');
+    props.buildBucket.grantPut(bundleRole, '*/dist/*');
 
-    props.buildBucket.grantPut(testRole, 'dist/*/tests/*');
+    props.buildBucket.grantRead(testRole, '*/dist/*');
+    props.buildBucket.grantPut(testRole, '*/dist/*/tests/*');
   }
 
   private static roleFromName(stack: Stack, roleName: string): IRole {
