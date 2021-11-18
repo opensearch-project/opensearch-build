d3a29fc64448428d92772828b077e81a2e399280
Daniel Doubrovkine (dB.) <dblock@dblock.org>
2021-11-18 23:56:53 +0000

    Always cleanup, load a library with @date and fix dashboards. (#1104)
    * Clear the environment in a post-step.
    
    Signed-off-by: dblock <dblock@amazon.com>
    
    * Get rid of extra steps.
    
    Signed-off-by: dblock <dblock@amazon.com>

--- vars/assembleManifest.groovy
+++ vars/assembleManifest.groovy
@@ -1,5 +1,5 @@
 void call(Map args = [:]) {
-    lib = library(identifier: "jenkins@current", retriever: legacySCM(scm))
+    lib = library(identifier: "jenkins@20211118", retriever: legacySCM(scm))
     def manifestFilename = args.manifest ?: 'builds/manifest.yml'
     def buildManifest = lib.jenkins.BuildManifest.new(readYaml(file: manifestFilename))
     def baseUrl = buildManifest.getArtifactRootUrl("${PUBLIC_ARTIFACT_URL}", "${JOB_NAME}", "${BUILD_NUMBER}")
