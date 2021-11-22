5a79d6814eb8b4f7189f3e2a71e62b2521db732b
Daniel Doubrovkine (dB.) <dblock@dblock.org>
2021-11-22 22:33:21 +0000

    Build and assemble manifest with lock. (#1111)
    Consolidate dry_run arguments.
    
    Signed-off-by: dblock <dblock@amazon.com>

--- tests/jenkins/TestVars.groovy
+++ tests/jenkins/TestVars.groovy
@@ -14,7 +14,7 @@ import java.util.*
 class TestVars extends BuildPipelineTest {
     @Test
     public void testVars() {
-        binding.setVariable('INPUT_MANIFEST', 'manifests/2.0.0/opensearch-2.0.0.yml')
+        binding.setVariable('INPUT_MANIFEST', '2.0.0/opensearch-2.0.0.yml')
         binding.setVariable('BUILD_URL', 'http://jenkins.us-east-1.elb.amazonaws.com/job/vars/42')
         binding.setVariable('BUILD_NUMBER', '33')
         binding.setVariable('PUBLIC_ARTIFACT_URL', 'https://ci.opensearch.org/dbc/')
@@ -29,6 +29,10 @@ class TestVars extends BuildPipelineTest {
         })
 
         binding.setVariable('WEBHOOK_URL', 'https://web/hook/url')
+
+        helper.registerAllowedMethod("sha1", [String], { filename ->
+            return 'sha1'
+        })
 
         helper.registerAllowedMethod("s3Upload", [Map])
         helper.registerAllowedMethod("withAWS", [Map, Closure], { args, closure ->
--- vars/assembleManifest.groovy
+++ vars/assembleManifest.groovy
@@ -1,10 +1,10 @@
 void call(Map args = [:]) {
-    lib = library(identifier: "jenkins@20211118", retriever: legacySCM(scm))
-    def manifestFilename = args.manifest ?: 'builds/manifest.yml'
+    lib = library(identifier: 'jenkins@20211122', retriever: legacySCM(scm))
+    String manifestFilename = args.manifest ?: 'builds/manifest.yml'
     def buildManifest = lib.jenkins.BuildManifest.new(readYaml(file: manifestFilename))
     def baseUrl = buildManifest.getArtifactRootUrl("${PUBLIC_ARTIFACT_URL}", "${JOB_NAME}", "${BUILD_NUMBER}")
     sh ([
-        args.script ?: './assemble.sh',
+        args.dryRun ? 'echo ./assemble.sh' : './assemble.sh',
         "\"${manifestFilename}\"",
         "--base-url ${baseUrl}"
     ].join(' '))
