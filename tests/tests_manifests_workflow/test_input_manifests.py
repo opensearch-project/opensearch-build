385091ff6118355e4683f4125f8ee10c1cc81f06
dblock <dblock@amazon.com>
2021-12-01 10:07:34 -0500

    Replace CI runner by 20211028.
    Signed-off-by: dblock <dblock@amazon.com>

--- tests/tests_manifests_workflow/test_input_manifests.py
+++ tests/tests_manifests_workflow/test_input_manifests.py
@@ -23,7 +23,7 @@ class TestInputManifests(unittest.TestCase):
             {
                 "schema-version": "1.0",
                 "build": {"name": "test", "version": "1.2.3"},
-                "ci": {"image": {"name": "opensearchstaging/ci-runner:centos7-x64-arm64-jdkmulti-node10.24.1-cypress6.9.1-20211019"}},
+                "ci": {"image": {"name": "opensearchstaging/ci-runner:centos7-x64-arm64-jdkmulti-node10.24.1-cypress6.9.1-20211028"}},
             },
         )
 
