d14456dc1c207bf21f1a7b058e972b9aaea4c8f8
Tianle Huang <tianleh@amazon.com>
2021-12-03 07:32:01 +0000

    refactor runner
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

--- src/test_workflow/integ_test/integ_test_runner.py
+++ src/test_workflow/integ_test/integ_test_runner.py
@@ -5,6 +5,12 @@
 # compatible open source license.
 
 import abc
+import logging
+import os
+
+from system.temporary_directory import TemporaryDirectory
+from test_workflow.test_recorder.test_recorder import TestRecorder
+from test_workflow.test_result.test_suite_results import TestSuiteResults
 
 
 class IntegTestRunner(abc.ABC):
@@ -12,6 +18,28 @@ class IntegTestRunner(abc.ABC):
         self.args = args
         self.test_manifest = test_manifest
 
-    @abc.abstractmethod
+        self.tests_dir = os.path.join(os.getcwd(), "test-results", "opensearch")
+        os.makedirs(self.tests_dir, exist_ok=True)
+        self.test_recorder = TestRecorder(self.args.test_run_id, "integ-test", self.tests_dir)
+
     def run(self):
+        with TemporaryDirectory(keep=self.args.keep, chdir=True) as work_dir:
+
+            all_results = TestSuiteResults()
+            for component in self.components.select(focus=self.args.component):
+                if component.name in self.test_manifest.components:
+                    test_config = self.test_manifest.components[component.name]
+                    if test_config.integ_test:
+                        test_suite = self.create_test_suite(component, test_config, work_dir)
+                        test_results = test_suite.execute_tests()
+                        all_results.append(component.name, test_results)
+                    else:
+                        logging.info(f"Skipping integ-tests for {component.name}, as it is currently not supported")
+                else:
+                    logging.info(f"Skipping integ-tests for {component.name}, as it is currently not declared in the test manifest")
+
+        return all_results
+
+    @abc.abstractmethod
+    def create_test_suite(self, component, test_config, work_dir):
         pass
--- src/test_workflow/integ_test/integ_test_runner_opensearch.py
+++ src/test_workflow/integ_test/integ_test_runner_opensearch.py
@@ -5,14 +5,10 @@
 # compatible open source license.
 
 import logging
-import os
 
-from system.temporary_directory import TemporaryDirectory
 from test_workflow.integ_test.integ_test_runner import IntegTestRunner
 from test_workflow.integ_test.integ_test_start_properties_opensearch import IntegTestStartPropertiesOpenSearch
 from test_workflow.integ_test.integ_test_suite_opensearch import IntegTestSuiteOpenSearch
-from test_workflow.test_recorder.test_recorder import TestRecorder
-from test_workflow.test_result.test_suite_results import TestSuiteResults
 
 
 class IntegTestRunnerOpenSearch(IntegTestRunner):
@@ -21,33 +17,19 @@ class IntegTestRunnerOpenSearch(IntegTestRunner):
         super().__init__(args, test_manifest)
         self.properties = IntegTestStartPropertiesOpenSearch(args.path)
 
-    def run(self):
-        logging.info("Running integ test for OpenSearch")
+        self.properties.dependency_installer.install_maven_dependencies()
 
-        tests_dir = os.path.join(os.getcwd(), "test-results", "opensearch")
-        os.makedirs(tests_dir, exist_ok=True)
-        with TemporaryDirectory(keep=self.args.keep, chdir=True) as work_dir:
-            test_recorder = TestRecorder(self.args.test_run_id, "integ-test", tests_dir)
-            self.properties.dependency_installer.install_maven_dependencies()
-            all_results = TestSuiteResults()
-            for component in self.properties.bundle_manifest.components.select(focus=self.args.component):
-                if component.name in self.test_manifest.components:
-                    test_config = self.test_manifest.components[component.name]
-                    if test_config.integ_test:
-                        test_suite = IntegTestSuiteOpenSearch(
-                            self.properties.dependency_installer,
-                            component,
-                            test_config,
-                            self.properties.bundle_manifest,
-                            self.properties.build_manifest,
-                            work_dir.name,
-                            test_recorder
-                        )
-                        test_results = test_suite.execute_tests()
-                        all_results.append(component.name, test_results)
-                    else:
-                        logging.info(f"Skipping integ-tests for {component.name}, as it is currently not supported")
-                else:
-                    logging.info(f"Skipping integ-tests for {component.name}, as it is currently not declared in the test manifest")
+        self.components = self.properties.bundle_manifest.components
 
-        return all_results
+        logging.info("Entering integ test for OpenSearch")
+
+    def create_test_suite(self, component, test_config, work_dir):
+        return IntegTestSuiteOpenSearch(
+            self.properties.dependency_installer,
+            component,
+            test_config,
+            self.properties.bundle_manifest,
+            self.properties.build_manifest,
+            work_dir.name,
+            self.test_recorder
+        )
