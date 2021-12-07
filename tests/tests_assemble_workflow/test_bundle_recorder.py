fdb434e402bfcf3372790b697f3d3bcadc66b025
Tianle Huang <tianleh@amazon.com>
2021-12-07 02:36:46 +0000

    fix long line
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

--- tests/tests_assemble_workflow/test_bundle_recorder.py
+++ tests/tests_assemble_workflow/test_bundle_recorder.py
@@ -115,7 +115,8 @@ class TestBundleRecorder(unittest.TestCase):
                 "components": [
                     {
                         "commit_id": "3913d7097934cbfe1fdcf919347f22a597d00b76",
-                        "location": "https://ci.opensearch.org/ci/ci-env-prod/job-name-opensearch/1.2.0/build-123/platform-mac/arch-amd64/builds/opensearch/plugins",
+                        "location":
+                            "https://ci.opensearch.org/ci/ci-env-prod/job-name-opensearch/1.2.0/build-123/platform-mac/arch-amd64/builds/opensearch/plugins",
                         "name": component.name,
                         "ref": "main",
                         "repository": "https://github.com/opensearch-project/job_scheduler",
@@ -249,7 +250,9 @@ class TestBundleRecorderDashboards(unittest.TestCase):
                 "components": [
                     {
                         "commit_id": "ae789280740d7000d1f13245019414abeedfc286",
-                        "location": "https://ci.opensearch.org/ci/ci-env-prod/job-name-dashboards/1.2.0/build-123/platform-mac/arch-amd64/builds/opensearch-dashboards/plugins",
+                        "location":
+                            "https://ci.opensearch.org/ci/ci-env-prod/job-name-dashboards/1.2.0/build-123/platform-mac/arch-amd64/" +
+                            "builds/opensearch-dashboards/plugins",
                         "name": component.name,
                         "ref": "main",
                         "repository": "https://github.com/opensearch-project/alerting-dashboards-plugin",
