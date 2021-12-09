dce07ef2276f5bd538bff5c13e44f9d0249e850f
Tianle Huang <60111637+tianleh@users.noreply.github.com>
2021-12-08 16:43:00 -0800

    Update public url to include filename (#1265)
    * append product name to CI endpoint
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * remove usused
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * update from source
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * Revert "update from source"
    
    This reverts commit 031afcfa1cdee5bb4bb33892034d0c54c5738c52.
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * update unit test
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * fix unit test
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * remove extra line
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * fix long line
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * fix long line
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * refactor the location logic for file and url
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * remove
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * clean up
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * add unit tests
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * style check
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

---
+++ tests/tests_assemble_workflow/test_bundle_locations.py
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+
+import unittest
+
+from assemble_workflow.bundle_file_location import BundleFileLocation
+from assemble_workflow.bundle_locations import BundleLocations
+from assemble_workflow.bundle_url_location import BundleUrlLocation
+
+
+class TestBundleLocations(unittest.TestCase):
+
+    def test(self) -> None:
+        self.assertIsInstance(
+            BundleLocations.from_path("", "file", "opensearch"),
+            BundleFileLocation
+        )
+
+        self.assertIsInstance(
+            BundleLocations.from_path("https://ci.opensearch.org/ci/ci-env-prod/job-name-opensearch/", "file", "opensearch"),
+            BundleUrlLocation
+        )
