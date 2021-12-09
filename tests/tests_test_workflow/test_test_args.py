a10ada29299566df548d117dfa098006d1ec7785
Tianle Huang <tianleh@amazon.com>
2021-12-09 15:36:48 +0000

    extra space
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

--- tests/tests_test_workflow/test_test_args.py
+++ tests/tests_test_workflow/test_test_args.py
@@ -70,33 +70,33 @@ class TestTestArgs(unittest.TestCase):
         self.assertEqual(test_args.logging_level, logging.INFO)
         self.assertEqual(test_args.test_manifest_path, self.TEST_MANIFEST_PATH)
 
-    @ patch("argparse._sys.argv", [ARGS_PY, TEST_MANIFEST_PATH, "--paths", "opensearch=" + PATH, "--test-run-id", "6"])
+    @patch("argparse._sys.argv", [ARGS_PY, TEST_MANIFEST_PATH, "--paths", "opensearch=" + PATH, "--test-run-id", "6"])
     def test_run_id(self):
         test_args = TestArgs()
         self.assertEqual(test_args.test_run_id, 6)
         self.assertEqual(test_args.test_manifest_path, self.TEST_MANIFEST_PATH)
 
-    @ patch("argparse._sys.argv", [ARGS_PY, TEST_MANIFEST_PATH, "--paths", "opensearch=" + PATH, "--verbose"])
+    @patch("argparse._sys.argv", [ARGS_PY, TEST_MANIFEST_PATH, "--paths", "opensearch=" + PATH, "--verbose"])
     def test_verbose(self):
         test_args = TestArgs()
         self.assertEqual(test_args.logging_level, logging.DEBUG)
         self.assertEqual(test_args.test_manifest_path, self.TEST_MANIFEST_PATH)
 
-    @ patch("argparse._sys.argv", [ARGS_PY, TEST_MANIFEST_PATH, "--paths", "opensearch=https://ci.opensearch.org/x/y", "--verbose"])
+    @patch("argparse._sys.argv", [ARGS_PY, TEST_MANIFEST_PATH, "--paths", "opensearch=https://ci.opensearch.org/x/y", "--verbose"])
     def test_opensearch_url_opensearch_dashboards_default(self):
         test_args = TestArgs()
         self.assertEqual(test_args.opensearch_path, "https://ci.opensearch.org/x/y")
         self.assertEqual(test_args.opensearch_dashboards_path, os.getcwd())
         self.assertEqual(test_args.test_manifest_path, self.TEST_MANIFEST_PATH)
 
-    @ patch("argparse._sys.argv", [ARGS_PY, TEST_MANIFEST_PATH, "--paths", "opensearch-dashboards=https://ci.opensearch.org/x/y", "--verbose"])
+    @patch("argparse._sys.argv", [ARGS_PY, TEST_MANIFEST_PATH, "--paths", "opensearch-dashboards=https://ci.opensearch.org/x/y", "--verbose"])
     def test_opensearch_default_opensearch_dashboards_url(self):
         test_args = TestArgs()
         self.assertEqual(test_args.opensearch_dashboards_path, "https://ci.opensearch.org/x/y")
         self.assertEqual(test_args.opensearch_path, os.getcwd())
         self.assertEqual(test_args.test_manifest_path, self.TEST_MANIFEST_PATH)
 
-    @ patch(
+    @patch(
         "argparse._sys.argv",
         [
             ARGS_PY,
