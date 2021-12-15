0fae3c4655858c8eab7e6d0ad4b8c6c94e29b535
Peter Nied <petern@amazon.com>
2021-12-15 18:06:33 +0000

    Code review feedback
    Signed-off-by: Peter Nied <petern@amazon.com>

--- vars/getRepositoryName.groovy
+++
@@ -1,9 +0,0 @@
-String call(String repository) {
-    if (!repository) {
-        throw new IllegalArgumentException("repository property was not set, try again with getRepositoryName('fooRepository')")
-    }
-    afterLastSlash = repository.tokenize("/").pop()
-    onlyRepoName = afterLastSlash.tokenize(".")[0]
-
-    return onlyRepoName
-}
