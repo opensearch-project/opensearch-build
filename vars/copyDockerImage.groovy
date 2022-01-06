74ca62a158a0cdab7a58e68cea1c3470cf2cc01d
Abhinav Gupta <abhng@amazon.com>
2022-01-05 18:09:45 -0800

    added copy function and changed policy
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>

---
+++ vars/copyDockerImage.groovy
@@ -0,0 +1,22 @@
+void call(Map args = [:]){
+
+    if(args.destinationType == 'docker'){
+        withCredentials([usernamePassword(credentialsId: args.destinationCredentialIdentifier, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+            sh """
+                set -ex
+                echo Login to $CREDENTIAL_ID
+                docker logout && docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+                gcrane cp ${args.sourceImagePath} ${args.destinationImagePath}
+            """
+        }
+    } else if(args.destinationType == 'ecr'){
+        withAWS(role: 'Upload_ECR_Image', roleAccount: "${AWS_ACCOUNT_PUBLIC}", duration: 900, roleSessionName: 'jenkins-session') {
+            sh """
+                docker logout
+                aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${args.destinationCredentialIdentifier}
+                gcrane cp ${args.sourceImagePath} ${args.destinationImagePath}
+            """
+        }
+    }
+
+}
