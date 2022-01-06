6865e8bc253d26ba07cc8d2d534cf85dbc7ade6d
Tyler Ohlsen <ohltyler@amazon.com>
2022-01-06 15:28:48 -0800

    Simplify params in runIntegTests() and integ-test workflow
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

--- vars/runIntegTestScript.groovy
+++ vars/runIntegTestScript.groovy
@@ -1,9 +1,15 @@
 void call(Map args = [:]) {
+    String jobName = args.jobName ?: 'distribution-build-opensearch'
+    lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
+    def buildManifest = lib.jenkins.BuildManifest.new(readYaml(file: args.buildManifest))
+    String artifactRootUrl = buildManifest.getArtifactRootUrl("${jobName}", "${env.BUILD_ID}")
+    echo "Artifact root URL: ${artifactRootUrl}"
+    
     sh([
         './test.sh',
         'integ-test',
         "${args.testManifest}",
-        "--paths opensearch=${args.artifactBasePath}",
+        "--paths opensearch=${artifactRootUrl}",
         "--test-run-id ${env.BUILD_NUMBER}"
     ].join(' '))
 }
--- vars/runIntegTests.groovy
+++ vars/runIntegTests.groovy
@@ -5,15 +5,10 @@ void call(Map args = [:]) {
     String manifest = args.manifest ?: "manifests/${INPUT_MANIFEST}"
     def inputManifest = lib.jenkins.InputManifest.new(readYaml(file: manifest))
 
-    String version = "${inputManifest.build.version}"
     String tarball = args.architecture == "x64" ? env.ARTIFACT_URL_linux_x64 : env.ARTIFACT_URL_linux_arm64
-    String basePath = args.architecture == "x64" ? env.ARTIFACT_BASE_PATH_linux_x64 : env.ARTIFACT_BASE_PATH_linux_arm64
-    String buildManifest = "${basePath}/dist/opensearch/manifest.yml"
-    String testManifest = manifest.replaceAll(".yml", "-test.yml")
-    
-    echo "version: ${version}"
-    echo "tarball: ${tarball}"
-    echo "basePath: ${basePath}"
+    String artifactRootUrl = args.architecture == "x64" ? env.ARTIFACT_ROOT_URL_linux_x64 : env.ARTIFACT_ROOT_URL_linux_arm64
+    String buildManifest = "${artifactRootUrl}/dist/opensearch/manifest.yml"
+    String testManifest = manifest.replaceAll(".yml", "-test.yml")    
     echo "buildManifest: ${buildManifest}"
     echo "testManifest: ${testManifest}"
 
@@ -24,12 +19,8 @@ void call(Map args = [:]) {
             // TODO: will need to override integ-test in Jenkins with the new workflow in jenkins/opensearch/integ-test.jenkinsfile
             build job: 'Playground/ohltyler-integ-test',
             parameters: [
-                string(name: 'OPENSEARCH_VERSION', value: "${inputManifest.build.version}"),
-                string(name: 'ARCHITECTURE', value: "${args.architecture}"),
-                string(name: 'BUILD_ID', value: "${BUILD_NUMBER}"),
                 string(name: 'TEST_MANIFEST', value: "${testManifest}"),
                 string(name: 'BUILD_MANIFEST', value: "${buildManifest}"),
-                string(name: 'ARTIFACT_BASE_PATH', value: "${basePath}")
             ]
         }
     }
