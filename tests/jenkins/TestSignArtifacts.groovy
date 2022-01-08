19c0bd73c6b41ee6e4c27b17e92014893ee7f589
Abhinav Gupta <abhng@amazon.com>
2022-01-08 02:33:08 -0800

    moved variables for job in the job test case
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>

--- tests/jenkins/TestSignArtifacts.groovy
+++ tests/jenkins/TestSignArtifacts.groovy
@@ -18,19 +18,10 @@ class TestSignArtifacts extends BuildPipelineTest {
     void setUp() {
         super.setUp()
 
-        binding.setVariable('URLs', 'https://www.dummy.com/dummy_1_artifact.tar.gz,' +
-                ' https://www.dummy.com/dummy_2_artifact.tar.gz')
-        binding.setVariable('S3_FILE_UPLOAD_PATH', '/dummy/upload/path/')
-        binding.setVariable('JOB_NAME', 'sign_artifacts_job')
-        binding.setVariable('BUILD_NUMBER', '20')
-        binding.setVariable('ARTIFACT_DOWNLOAD_ROLE_NAME', 'Dummy_Download_Role')
-        binding.setVariable('ARTIFACT_UPLOAD_ROLE_NAME', 'Dummy_Upload_Role')
-        binding.setVariable('AWS_ACCOUNT_PUBLIC', 'dummy_account')
-        binding.setVariable('ARTIFACT_BUCKET_NAME', 'dummy_bucket_name')
-        binding.setVariable('DISTRIBUTION_PLATFORM', 'linux')
-        binding.setVariable('SIGNATURE_TYPE', '.sig')
         binding.setVariable('GITHUB_BOT_TOKEN_NAME', 'github_bot_token_name')
         binding.setVariable('WORKSPACE', 'workspace')
+        binding.setVariable('DISTRIBUTION_PLATFORM', 'linux')
+        binding.setVariable('SIGNATURE_TYPE', '.sig')
 
         binding.setVariable('SIGNER_CLIENT_ROLE', 'dummy_signer_client_role')
         binding.setVariable('SIGNER_CLIENT_EXTERNAL_ID', 'signer_client_external_id')
@@ -38,12 +29,8 @@ class TestSignArtifacts extends BuildPipelineTest {
         binding.setVariable('SIGNER_CLIENT_SIGNED_BUCKET', 'signer_client_signed_bucket')
 
         helper.registerAllowedMethod("git", [Map])
-        helper.registerAllowedMethod("s3Upload", [Map])
-        helper.registerAllowedMethod("cleanWs", [Map])
-        helper.registerAllowedMethod("withAWS", [Map, Closure], { args, closure ->
-            closure.delegate = delegate
-            return helper.callClosure(closure)
-        })
+        helper.registerAllowedMethod("withCredentials", [Map])
+
     }
 
     @Test
@@ -59,6 +46,23 @@ class TestSignArtifacts extends BuildPipelineTest {
 
     @Test
     void testSignArtifactsJob() {
+        binding.setVariable('URLs', 'https://www.dummy.com/dummy_1_artifact.tar.gz,' +
+                ' https://www.dummy.com/dummy_2_artifact.tar.gz')
+        binding.setVariable('S3_FILE_UPLOAD_PATH', '/dummy/upload/path/')
+        binding.setVariable('JOB_NAME', 'sign_artifacts_job')
+        binding.setVariable('BUILD_NUMBER', '20')
+        binding.setVariable('ARTIFACT_DOWNLOAD_ROLE_NAME', 'Dummy_Download_Role')
+        binding.setVariable('ARTIFACT_UPLOAD_ROLE_NAME', 'Dummy_Upload_Role')
+        binding.setVariable('AWS_ACCOUNT_PUBLIC', 'dummy_account')
+        binding.setVariable('ARTIFACT_BUCKET_NAME', 'dummy_bucket_name')
+
+        helper.registerAllowedMethod("cleanWs", [Map])
+        helper.registerAllowedMethod("s3Upload", [Map])
+        helper.registerAllowedMethod("withAWS", [Map, Closure], { args, closure ->
+            closure.delegate = delegate
+            return helper.callClosure(closure)
+        })
+
         super.testPipeline("jenkins/sign-artifacts/sign-standalone-artifacts.jenkinsfile")
     }
 }
