d21faa5aee71958e843e3a751cc7e0e9e805cebf
Abhinav Gupta <abhng@amazon.com>
2022-01-08 14:29:29 -0800

    added test case for printArtifactUrls
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>

--- vars/printArtifactDownloadUrlsForStaging.groovy
+++ vars/printArtifactDownloadUrlsForStaging.groovy
@@ -1,7 +1,8 @@
 void call(Map args = [:]){
 
     for(filename in args.artifactFileNames){
-        println("File ${filename} can be accessed using the url - https://ci.opensearch.org/ci/dbc/${args.uploadPath}/${filename}" )
+        url = "https://ci.opensearch.org/ci/dbc/${args.uploadPath}/${filename}"
+        println("File ${filename} can be accessed using the url - ${url}" )
     }
 
 }
