12955b1d2bf28000f16f24a6bfec36d55bb7c5b5
Peter Zhu <zhujiaxi@amazon.com>
2022-01-11 00:10:28 +0000

    Add readme information
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

---
+++ mvn.sh
@@ -0,0 +1,7 @@
+export MAVEN_DIR=/usr/local/apache-maven
+export MAVEN_URL=`curl -s https://maven.apache.org/download.cgi | grep -Eo '["\047].*.bin.tar.gz["\047]' | tr -d '"'`  && \
+    mkdir -p $MAVEN_DIR && (curl -s $MAVEN_URL | tar xzf - --strip-components=1 -C $MAVEN_DIR) && \
+    echo "export M2_HOME=$MAVEN_DIR" > /etc/profile.d/maven_path.sh && \
+    echo "export M2=\$M2_HOME/bin" >> /etc/profile.d/maven_path.sh && \
+    echo "export PATH=\$M2:\$PATH" >> /etc/profile.d/maven_path.sh && \
+    ln -sfn $MAVEN_DIR/bin/mvn /usr/local/bin/mvn
