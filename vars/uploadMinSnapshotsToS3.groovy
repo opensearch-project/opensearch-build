541992ac2c3618a35fae6a8aad849c0b08b2de90
Sayali Gaikawad <gaiksaya@amazon.com>
2022-01-12 13:45:19 -0800

    Support snapshot builds
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

---
+++ vars/uploadMinSnapshotsToS3.groovy
@@ -0,0 +1,15 @@
+void call(Map args = [:]) {
+    def lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
+    String manifest = args.manifest ?: "manifests/${INPUT_MANIFEST}"
+    
+    def inputManifest = lib.jenkins.InputManifest.new(readYaml(file: manifest))
+    version = inputManifest.build.version
+    architecture = inputManifest.build.architecture
+    platform = inputManifest.build.platform
+    minArtifactName = opensearch-min-${version}-SNAPSHOT-${platform}-${architecture}-latest.tar.gz
+
+
+    withAWS(role: "${ARTIFACT_PROMOTION_ROLE_NAME}", roleAccount: "${AWS_ACCOUNT_ARTIFACT}", duration: 900, roleSessionName: 'jenkins-session') {
+        s3Upload(file: "$WORKSPACE/builds/opensearch/dist/", bucket: "${ARTIFACT_PRODUCTION_BUCKET_NAME}", path: "snapshots/core/opensearch/${version}-SNAPSHOT/${minArtifactName}")
+        }
+}
