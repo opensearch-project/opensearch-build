d0ac21177a2081e068bf825ae34c14b285702851
Yilin Zhang <37334547+zylzulu@users.noreply.github.com>
2022-01-11 16:23:01 -0800

    Promote min and bundle artifacts to release-candidate after every bundle build (#1378)
    * Promote to release candidate after every bundle build
    
    Signed-off-by: Yilin Zhang <yilizhan@amazon.com>
    
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>
    
    Co-authored-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- src/jenkins/BuildManifest.groovy
+++ src/jenkins/BuildManifest.groovy
@@ -25,30 +25,72 @@ class BuildManifest implements Serializable {
         String getFilename() {
             return this.name.toLowerCase().replaceAll(' ', '-')
         }
+
+        String getPackageName() {
+            return [
+                    this.getFilename(),
+                    this.version,
+                    this.platform,
+                    this.architecture,
+            ].join('-') + '.tar.gz'
+        }
+    }
+
+    class Components extends HashMap<String, Component> {
+
+        Components(ArrayList data) {
+            data.each { item ->
+                Component component = new Component(item)
+                this[component.name] = component
+            }
+        }
+    }
+
+    class Component implements Serializable {
+        String name
+        String version
+        String ref
+        String commit_id
+        String repository
+        Map<String, ArrayList> artifacts
+
+        Component(Map data) {
+            this.name = data.name
+            this.version = data.version
+            this.ref = data.ref
+            this.commit_id = data.commit_id
+            this.repository = data.repository
+            this.artifacts = new HashMap<>(data.artifacts)
+        }
+
     }
 
     Build build
+    Components components
 
     BuildManifest(Map data) {
         this.build = new BuildManifest.Build(data.build)
+        this.components = new BuildManifest.Components(data.components)
     }
 
     public String getArtifactRoot(String jobName, String buildNumber) {
         return [
-            jobName,
-            this.build.version,
-            buildNumber,
-            this.build.platform,
-            this.build.architecture
+                jobName,
+                this.build.version,
+                buildNumber,
+                this.build.platform,
+                this.build.architecture
         ].join("/")
     }
 
     public String getArtifactRootUrl(String publicArtifactUrl = 'https://ci.opensearch.org/ci/dbc', String jobName, String buildNumber) {
         return [
-            publicArtifactUrl,
-            this.getArtifactRoot(jobName, buildNumber)
+                publicArtifactUrl,
+                this.getArtifactRoot(jobName, buildNumber)
         ].join('/')
     }
+
+    public String getMinArtifact() {
+        components.get(build.name.replace(' ','-'))?.artifacts?.get("dist")?.first()
+    }
 }
-
-
