cfeb3c1df313edf43ce77fbe311dd8612b24ffa8
Tyler Ohlsen <ohltyler@amazon.com>
2022-01-12 12:30:35 -0800

    Add build vars in stage env to use in post block
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

--- vars/getBuildManifestInfo.groovy
+++ vars/getBuildManifestInfo.groovy
@@ -2,13 +2,10 @@ Map call(Map args = [:]) {
     def lib = library(identifier: "jenkins@20211123", retriever: legacySCM(scm))
     String buildManifestUrl = args.buildManifestUrl ?: BUILD_MANIFEST_URL
 
-    String BUILD_MANIFEST = 'manifest.yml'
-    sh "wget ${buildManifestUrl} -O ${BUILD_MANIFEST}"
-    def buildManifest = lib.jenkins.BuildManifest.new(readYaml(file: BUILD_MANIFEST))
-    String BUILD_ID = buildManifest.getArtifactBuildId()
+    String buildManifestPath = 'manifest.yml'
+    sh "wget ${buildManifestUrl} -O ${buildManifestPath}"
+    def buildManifest = lib.jenkins.BuildManifest.new(readYaml(file: buildManifestPath))
+    String buildId = buildManifest.getArtifactBuildId()
 
-    echo "BUILD_MANIFEST: ${BUILD_MANIFEST}"
-    echo "BUILD_ID: ${BUILD_ID}"
-
-    return [BUILD_MANIFEST, BUILD_ID]
+    return [buildManifestPath, buildId]
 }
