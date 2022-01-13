c515d0537e13538384d7c5360c9d3321e51c46ec
Sayali Gaikawad <gaiksaya@amazon.com>
2022-01-13 13:32:04 -0800

    Rename
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- vars/uploadMinSnapshotsToS3.groovy
+++ vars/uploadMinSnapshotsToS3.groovy
@@ -7,10 +7,10 @@ void call(Map args = [:]) {
     version = buildManifest.build.version
     architecture = buildManifest.build.architecture
     platform = buildManifest.build.platform
-    minArtifactName = "opensearch-min-${version}-SNAPSHOT-${platform}-${architecture}-latest.tar.gz"
+    minArtifactName = "opensearch-min-${version}-${platform}-${architecture}-latest.tar.gz"
 
 
     withAWS(role: "${ARTIFACT_PROMOTION_ROLE_NAME}", roleAccount: "${AWS_ACCOUNT_ARTIFACT}", duration: 900, roleSessionName: 'jenkins-session') {
-        s3Upload(file: "$WORKSPACE/builds/opensearch/dist/", bucket: "${ARTIFACT_PRODUCTION_BUCKET_NAME}", path: "builds/test-release-candidates/snapshots/core/opensearch/${version}-SNAPSHOT/${minArtifactName}")
+        s3Upload(file: "$WORKSPACE/builds/opensearch/dist/", bucket: "${ARTIFACT_PRODUCTION_BUCKET_NAME}", path: "builds/test-release-candidates/snapshots/core/opensearch/${version}/${minArtifactName}")
         }
 }
