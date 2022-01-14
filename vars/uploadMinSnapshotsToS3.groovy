783c826da792a4b7a278ea86ff57fc3495f9daf2
Sayali Gaikawad <gaiksaya@amazon.com>
2022-01-13 16:34:52 -0800

    Remove blank spaces
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- vars/uploadMinSnapshotsToS3.groovy
+++ vars/uploadMinSnapshotsToS3.groovy
@@ -1,7 +1,6 @@
 void call(Map args = [:]) {
     def lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
     String manifest = args.manifest ?: "manifests/${INPUT_MANIFEST}"
-
     def inputManifest = lib.jenkins.InputManifest.new(readYaml(file: manifest))
     echo("Retreving build manifest from: $WORKSPACE/builds/${inputManifest.build.getFilename()}/manifest.yml")
     def buildManifest = lib.jenkins.BuildManifest.new(readYaml(file: "$WORKSPACE/builds/${inputManifest.build.getFilename()}/manifest.yml"))
@@ -9,7 +8,6 @@ void call(Map args = [:]) {
     architecture = buildManifest.build.architecture
     platform = buildManifest.build.platform
     minArtifactName = "opensearch-min-${version}-${platform}-${architecture}-latest.tar.gz"
-
     withAWS(role: "${ARTIFACT_PROMOTION_ROLE_NAME}", roleAccount: "${AWS_ACCOUNT_ARTIFACT}", duration: 900, roleSessionName: 'jenkins-session') {
         s3Upload(file: "$WORKSPACE/builds/opensearch/dist/", bucket: "${ARTIFACT_PRODUCTION_BUCKET_NAME}", path: "snapshots/core/opensearch/${version}/${minArtifactName}")
     }
