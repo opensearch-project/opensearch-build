cf82927c140c0e4d2240f439e061f35db85b23b3
Abhinav Gupta <abhng@amazon.com>
2022-01-15 00:57:05 -0800

    added more comprehensive tests and refactered variables
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>

--- tests/jenkins/TestCopyDockerImage.groovy
+++ tests/jenkins/TestCopyDockerImage.groovy
@@ -9,8 +9,11 @@
 
 import jenkins.tests.BuildPipelineTest
 import org.junit.*
-import java.util.*
-import java.nio.file.*
+
+import static org.hamcrest.CoreMatchers.anyOf
+import static org.hamcrest.CoreMatchers.equalTo
+import static org.hamcrest.CoreMatchers.notNullValue
+import static org.hamcrest.MatcherAssert.assertThat
 
 class TestCopyDockerImage extends BuildPipelineTest {
 
@@ -18,7 +21,6 @@ class TestCopyDockerImage extends BuildPipelineTest {
         binding.setVariable('CREDENTIAL_ID', 'dummy_credentials_id')
         binding.setVariable('DOCKER_USERNAME', 'dummy_docker_username')
         binding.setVariable('DOCKER_PASSWORD', 'dummy_docker_password')
-        binding.setVariable('AWS_ACCOUNT_PUBLIC', 'account')
 
         helper.registerAllowedMethod("withAWS", [Map, Closure], { args, closure ->
             closure.delegate = delegate
@@ -33,13 +35,68 @@ class TestCopyDockerImage extends BuildPipelineTest {
         setUpVariables(binding, helper)
     }
 
+    def sourceImagePath = 'opensearchstaging/ci-runner:latest'
+    def destinationImagePath = 'opensearchproject/ci-runner:latest'
+
     @Test
     public void testForDockerhub() {
+        def destinationCredentialIdentifier = 'jenkins-staging-docker-prod-token'
+        def destinationType = 'docker'
+
+        binding.setVariable('sourceImagePath', sourceImagePath)
+        binding.setVariable('destinationImagePath', destinationImagePath)
+        binding.setVariable('destinationType', destinationType)
+        binding.setVariable('destinationCredentialIdentifier', destinationCredentialIdentifier)
+
         super.testPipeline("tests/jenkins/jobs/CopyDockerImage_docker_Jenkinsfile")
+        verifyCopyDockerImageParams(helper, sourceImagePath, destinationImagePath,
+                destinationType, destinationCredentialIdentifier, null)
     }
 
     @Test
     public void testForEcr() {
+        def destinationCredentialIdentifier = 'jenkins-staging-docker-prod-token'
+        def accountName = 'DUMMY_NAME'
+        def destinationType = 'ecr'
+
+        binding.setVariable('sourceImagePath', sourceImagePath)
+        binding.setVariable('destinationImagePath', destinationImagePath)
+        binding.setVariable('destinationType', destinationType)
+        binding.setVariable('destinationCredentialIdentifier', destinationCredentialIdentifier)
+        binding.setVariable('accountName', accountName)
+
         super.testPipeline("tests/jenkins/jobs/CopyDockerImage_ecr_Jenkinsfile")
+
+        verifyCopyDockerImageParams(helper, sourceImagePath, destinationImagePath,
+                destinationType, destinationCredentialIdentifier, accountName)
+    }
+
+    static void verifyCopyDockerImageParams(helper, sourceImagePath, destinationImagePath,
+                                            destinationType, destinationCredentialIdentifier, accountName) {
+        assert helper.callStack.findAll { call ->
+            call.methodName == 'copyDockerImage'
+        }.size() > 0
+
+        helper.callStack.findAll { call ->
+            call.methodName == 'copyDockerImage'
+        }.each { call ->
+            assertThat(call.args.sourceImagePath.first(), notNullValue())
+            assertThat(call.args.destinationImagePath.first(), notNullValue())
+            assertThat(call.args.destinationType.first(), notNullValue())
+            assertThat(call.args.destinationType.first(), anyOf(equalTo('ecr'), equalTo('docker')))
+            assertThat(call.args.destinationCredentialIdentifier.first(), notNullValue())
+            if(call.args.destinationType.first() == "docker") {
+                assertThat(call.args.destinationCredentialIdentifier.first(), anyOf(equalTo('jenkins-staging-docker-prod-token'),
+                        equalTo('jenkins-staging-docker-staging-credential')))
+            }
+            if(call.args.destinationType.first() == "ecr"){
+                assertThat(call.args.accountName.first(), notNullValue())
+                assert call.args.accountName.first() == accountName
+            }
+            assert call.args.sourceImagePath.first() == sourceImagePath
+            assert call.args.destinationImagePath.first() == destinationImagePath
+            assert call.args.destinationCredentialIdentifier.first() == destinationCredentialIdentifier
+            assert call.args.destinationType.first() == destinationType
+        }
     }
 }
