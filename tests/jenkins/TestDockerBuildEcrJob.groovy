3377d5c1f1d6bb6b2ab3e1aa6f7aa404008f72c3
Abhinav Gupta <abhng@amazon.com>
2022-01-19 16:18:56 -0800

    created separate job for promote docker job
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>

--- tests/jenkins/TestDockerBuildEcrJob.groovy
+++ tests/jenkins/TestDockerBuildEcrJob.groovy
@@ -3,8 +3,9 @@ import org.junit.*
 
 class TestDockerBuildEcrJob extends BuildPipelineTest {
 
-    def imageInStaging = 'ci-runner-staging:latest'
-    def sourceImagePath = "opensearchstaging/${imageInStaging}"
+    def imageRepository = 'ci-runner-staging'
+    def imageTag = 'latest'
+    def sourceImagePath = "opensearchstaging/${imageRepository}:${imageTag}"
 
     @Before
     void setUp() {
@@ -20,43 +21,19 @@ class TestDockerBuildEcrJob extends BuildPipelineTest {
         binding.setVariable('DOCKER_BUILD_SCRIPT_WITH_COMMANDS', 'dummy_command')
         binding.setVariable('DOCKER_USERNAME ', 'docker_username')
         binding.setVariable('DOCKER_PASSWORD', 'docker_password')
-        binding.setVariable('IMAGE_IN_STAGING', imageInStaging)
+        binding.setVariable('IMAGE_REPOSITORY', imageRepository)
+        binding.setVariable('IMAGE_TAG', imageTag)
         binding.setVariable('AWS_ACCOUNT_PUBLIC', accountName)
-
-        binding.setVariable('ENVIRONMENT', 'staging')
 
         helper.registerAllowedMethod("git", [Map])
         helper.registerAllowedMethod("cleanWs", [Map])
 
         super.testPipeline("jenkins/docker-ecr/docker-build-ecr.jenkinsfile",
-                "tests/jenkins/jenkinsjob-regression-files/docker-ecr/docker-build-ecr-staging.jenkinsfile")
+                "tests/jenkins/jenkinsjob-regression-files/docker-build-ecr/docker-build-ecr.jenkinsfile")
 
-        TestCopyDockerImage.verifyCopyDockerImageParams(helper, sourceImagePath, "public.ecr.aws/m0o1u6w1/${imageInStaging}",
+        TestCopyDockerImage.verifyCopyDockerImageParams(helper, sourceImagePath, "public.ecr.aws/m0o1u6w1/${imageRepository}:${imageTag}",
                 'ecr', 'public.ecr.aws/m0o1u6w1', accountName)
 
         }
-
-    @Test
-    public void testDockerForEcrJobProduction(){
-
-        def imageInProd = 'ci-runner-prod:latest'
-        def accountName = 'aws_account_artifact'
-
-        binding.setVariable('IMAGE_IN_STAGING', imageInStaging)
-        binding.setVariable('IMAGE_IN_PROD', imageInProd)
-        binding.setVariable('AWS_ACCOUNT_ARTIFACT', accountName)
-        binding.setVariable('ENVIRONMENT', 'production')
-
-        helper.registerAllowedMethod("cleanWs", [Map])
-
-        super.testPipeline("jenkins/docker-ecr/docker-build-ecr.jenkinsfile",
-                "tests/jenkins/jenkinsjob-regression-files/docker-ecr/docker-build-ecr-production.jenkinsfile")
-
-        TestCopyDockerImage.verifyCopyDockerImageParams(helper, sourceImagePath, "public.ecr.aws/p5f6l6i3/${imageInProd}",
-                'ecr', 'public.ecr.aws/p5f6l6i3', accountName)
-
-        TestCopyDockerImage.verifyCopyDockerImageParams(helper, sourceImagePath, "opensearchproject/${imageInProd}",
-                'docker', 'jenkins-staging-docker-prod-token', accountName)
-    }
 
 }
