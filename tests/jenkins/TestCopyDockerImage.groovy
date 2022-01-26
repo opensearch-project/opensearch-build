9f422bf666ac5b89a09fbf135f8fce3c27a17b74
Abhinav Gupta <abhng@amazon.com>
2022-01-25 18:45:18 -0800

    updated test cases with updated jenkins testing framework
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>

--- tests/jenkins/TestCopyDockerImage.groovy
+++ tests/jenkins/TestCopyDockerImage.groovy
@@ -10,106 +10,28 @@
 import jenkins.tests.BuildPipelineTest
 import org.junit.*
 
-import static org.hamcrest.CoreMatchers.anyOf
-import static org.hamcrest.CoreMatchers.equalTo
-import static org.hamcrest.CoreMatchers.notNullValue
-import static org.hamcrest.MatcherAssert.assertThat
 
 class TestCopyDockerImage extends BuildPipelineTest {
 
-    static void setUpVariables(binding, helper){
-        binding.setVariable('DOCKER_USERNAME', 'dummy_docker_username')
-        binding.setVariable('DOCKER_PASSWORD', 'dummy_docker_password')
-
-        helper.registerAllowedMethod("withAWS", [Map, Closure], { args, closure ->
-            closure.delegate = delegate
-            return helper.callClosure(closure)
-        })
-    }
-
     @Before
     void setUp() {
-        super.setUp()
-        setUpVariables(binding, helper)
-    }
 
-    def sourceImagePath = 'opensearchstaging/ci-runner:latest'
-    def destinationImagePath = 'opensearchproject/ci-runner:latest'
+        this.registerLibTester(new CopyDockerImageLibTester('opensearchstaging/ci-runner:latest',
+                'opensearchproject/ci-runner:latest',
+                'ecr',
+                'public.ecr.aws/p5f6l6i3',
+                'DUMMY_ACCOUNT_NAME'))
+
+        this.registerLibTester(new CopyDockerImageLibTester('opensearchstaging/ci-runner:latest',
+                'opensearchproject/ci-runner:latest',
+                'docker',
+                'jenkins-staging-docker-prod-token'))
+
+        super.setUp()
+    }
 
     @Test
     public void testForDockerhub() {
-        def destinationCredentialIdentifier = 'jenkins-staging-docker-prod-token'
-        def destinationType = 'docker'
-
-        binding.setVariable('sourceImagePath', sourceImagePath)
-        binding.setVariable('destinationImagePath', destinationImagePath)
-        binding.setVariable('destinationType', destinationType)
-        binding.setVariable('destinationCredentialIdentifier', destinationCredentialIdentifier)
-
         super.testPipeline("tests/jenkins/jobs/CopyDockerImage_docker_Jenkinsfile")
-        verifyCopyDockerImageParams(helper, sourceImagePath, destinationImagePath,
-                destinationType, destinationCredentialIdentifier, null)
-    }
-
-    @Test
-    public void testForEcr() {
-        def destinationCredentialIdentifier = 'public.ecr.aws/p5f6l6i3'
-        def accountName = 'DUMMY_NAME'
-        def destinationType = 'ecr'
-
-        binding.setVariable('sourceImagePath', sourceImagePath)
-        binding.setVariable('destinationImagePath', destinationImagePath)
-        binding.setVariable('destinationType', destinationType)
-        binding.setVariable('destinationCredentialIdentifier', destinationCredentialIdentifier)
-        binding.setVariable('accountName', accountName)
-
-        super.testPipeline("tests/jenkins/jobs/CopyDockerImage_ecr_Jenkinsfile")
-
-        verifyCopyDockerImageParams(helper, sourceImagePath, destinationImagePath,
-                destinationType, destinationCredentialIdentifier, accountName)
-    }
-
-    static void verifyCopyDockerImageParams(helper, sourceImagePath, destinationImagePath,
-                                            destinationType, destinationCredentialIdentifier, accountName) {
-        assert helper.callStack.findAll { call ->
-            call.methodName == 'copyDockerImage'
-        }.size() > 0
-
-        helper.callStack.findAll { call ->
-            call.methodName == 'copyDockerImage'
-        }.each { call ->
-            assertThat(call.args.sourceImagePath.first(), notNullValue())
-            assertThat(call.args.destinationImagePath.first(), notNullValue())
-            assertThat(call.args.destinationType.first(), notNullValue())
-            assertThat(call.args.destinationType.first(), anyOf(equalTo('ecr'), equalTo('docker')))
-            assertThat(call.args.destinationCredentialIdentifier.first(), notNullValue())
-            if(call.args.destinationType.first() == "docker") {
-                assertThat(call.args.destinationCredentialIdentifier.first(), anyOf(equalTo('jenkins-staging-docker-prod-token'),
-                        equalTo('jenkins-staging-docker-staging-credential')))
-            }
-            if(call.args.destinationType.first() == "ecr"){
-                assertThat(call.args.accountName.first(), notNullValue())
-                assert call.args.accountName.first() == accountName
-                assertThat(call.args.destinationCredentialIdentifier.first(), anyOf(equalTo('public.ecr.aws/p5f6l6i3'),
-                        equalTo('public.ecr.aws/m0o1u6w1')))
-            }
-        }
-
-        def callList = helper.callStack.findAll { call ->
-            call.methodName == 'copyDockerImage'
-        }
-
-        def callFound = false
-
-        for(call in callList){
-            if( call.args.sourceImagePath.first().equals(sourceImagePath)
-                && call.args.destinationImagePath.first().equals(destinationImagePath)
-                && call.args.destinationCredentialIdentifier.first().equals(destinationCredentialIdentifier)
-                && call.args.destinationType.first().equals(destinationType)){
-                callFound = true
-            }
-        }
-
-        assert callFound
     }
 }
--- tests/jenkins/TestDockerPromoteJob.groovy
+++ tests/jenkins/TestDockerPromoteJob.groovy
@@ -4,35 +4,36 @@ import org.junit.Test
 
 class TestDockerPromoteJob extends BuildPipelineTest {
 
-    def imageRepository = 'ci-runner-staging'
-    def imageTag = 'latest'
-    def sourceImagePath = "opensearchstaging/${imageRepository}:${imageTag}"
-
     @Before
     void setUp() {
+        String imageRepository = 'ci-runner-staging'
+        String imageTag = 'latest'
+        String accountName = 'aws_account_artifact'
+        String sourceImagePath = "opensearchstaging/${imageRepository}:${imageTag}"
+
+        this.registerLibTester(new CopyDockerImageLibTester(sourceImagePath,
+                "public.ecr.aws/p5f6l6i3/${imageRepository}:${imageTag}",
+                'ecr',
+                'public.ecr.aws/p5f6l6i3',
+                accountName))
+
+        this.registerLibTester(new CopyDockerImageLibTester(sourceImagePath,
+                "opensearchproject/${imageRepository}:${imageTag}",
+                'docker',
+                'jenkins-staging-docker-prod-token'))
+        
         super.setUp()
-        TestCopyDockerImage.setUpVariables(binding, helper)
-    }
-
-    @Test
-    public void testDockerForEcrJobProduction(){
-
-        def accountName = 'aws_account_artifact'
 
         binding.setVariable('IMAGE_REPOSITORY', imageRepository)
         binding.setVariable('IMAGE_TAG', imageTag)
         binding.setVariable('AWS_ACCOUNT_ARTIFACT', accountName)
 
-        helper.registerAllowedMethod("cleanWs", [Map])
+    }
 
-        super.testPipeline("jenkins/docker-ecr/docker-promote.jenkinsfile",
-                "tests/jenkins/jenkinsjob-regression-files/docker-promote/docker-promote.jenkinsfile")
-
-        TestCopyDockerImage.verifyCopyDockerImageParams(helper, sourceImagePath, "public.ecr.aws/p5f6l6i3/${imageRepository}:${imageTag}",
-                'ecr', 'public.ecr.aws/p5f6l6i3', accountName)
-
-        TestCopyDockerImage.verifyCopyDockerImageParams(helper, sourceImagePath, "opensearchproject/${imageRepository}:${imageTag}",
-                'docker', 'jenkins-staging-docker-prod-token', accountName)
+    @Test
+    public void testDockerForEcrJobProduction(){
+        super.testPipeline("jenkins/docker-ecr/docker-ecr-promote.jenkinsfile",
+                "tests/jenkins/jenkinsjob-regression-files/docker-ecr/docker-ecr-promote.jenkinsfile")
     }
 
 }
