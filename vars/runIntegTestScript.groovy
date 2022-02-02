debd67b25682560e50dd3f00ce7444e80dc65109
Tyler Ohlsen <ohltyler@amazon.com>
2022-02-02 10:35:25 -0800

    Change references back to prod jenkins jobs
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

--- vars/runIntegTestScript.groovy
+++ vars/runIntegTestScript.groovy
@@ -1,6 +1,5 @@
 void call(Map args = [:]) {
-    // TODO: put this back to 'distribution-build-opensearch' once 
-    String jobName = args.jobName ?: 'Playground/ohltyler-distribution-build-opensearch'
+    String jobName = args.jobName ?: 'distribution-build-opensearch'
     lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
     def buildManifest = lib.jenkins.BuildManifest.new(readYaml(file: args.buildManifest))
     String artifactRootUrl = buildManifest.getArtifactRootUrl(jobName, args.buildId)
