0e7fb4ad88c64346e70c9a0dc3d74d13ac10b6c4
Zelin Hao <zelinhao@amazon.com>
2022-02-03 18:24:46 -0800

    Add python type checking for manifest workflow
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- tests/tests_manifests_workflow/test_input_manifests.py
+++ tests/tests_manifests_workflow/test_input_manifests.py
@@ -6,17 +6,17 @@
 
 import os
 import unittest
-from unittest.mock import call, mock_open, patch
+from unittest.mock import MagicMock, call, mock_open, patch
 
 from manifests_workflow.input_manifests import InputManifests
 
 
 class TestInputManifests(unittest.TestCase):
-    def test_manifests_path(self):
+    def test_manifests_path(self) -> None:
         path = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "..", "manifests"))
         self.assertEqual(path, InputManifests.manifests_path())
 
-    def test_create_manifest(self):
+    def test_create_manifest(self) -> None:
         input_manifests = InputManifests("test")
         input_manifest = input_manifests.create_manifest("1.2.3", [])
         self.assertEqual(
@@ -30,7 +30,7 @@ class TestInputManifests(unittest.TestCase):
 
     @patch("os.makedirs")
     @patch("manifests_workflow.input_manifests.InputManifests.create_manifest")
-    def test_write_manifest(self, mock_create_manifest, mock_makedirs):
+    def test_write_manifest(self, mock_create_manifest: MagicMock, mock_makedirs: MagicMock) -> None:
         input_manifests = InputManifests("test")
         input_manifests.write_manifest('0.1.2', [])
         mock_create_manifest.assert_called_with('0.1.2', [])
@@ -39,7 +39,7 @@ class TestInputManifests(unittest.TestCase):
             os.path.join(InputManifests.manifests_path(), '0.1.2', 'test-0.1.2.yml')
         )
 
-    def test_jenkins_path(self):
+    def test_jenkins_path(self) -> None:
         self.assertEqual(
             InputManifests.jenkins_path(),
             os.path.realpath(
@@ -49,14 +49,14 @@ class TestInputManifests(unittest.TestCase):
             )
         )
 
-    def test_cron_jenkinsfile(self):
+    def test_cron_jenkinsfile(self) -> None:
         self.assertEqual(
             InputManifests.cron_jenkinsfile(),
             os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "..", "jenkins", "check-for-build.jenkinsfile"))
         )
 
     @patch("builtins.open", new_callable=mock_open)
-    def test_add_to_cron(self, mock_open):
+    def test_add_to_cron(self, mock_open: MagicMock) -> None:
         mock_open().read.return_value = "parameterizedCron '''\n"
         input_manifests = InputManifests("test")
         input_manifests.add_to_cron('0.1.2')
@@ -66,5 +66,5 @@ class TestInputManifests(unittest.TestCase):
             f"parameterizedCron '''\n{' ' * 12}H/10 * * * * %INPUT_MANIFEST=0.1.2/test-0.1.2.yml;TARGET_JOB_NAME=distribution-build-test\n"
         )
 
-    def test_create_manifest_with_components(self):
+    def test_create_manifest_with_components(self) -> None:
         pass
--- tests/tests_manifests_workflow/test_input_manifests_opensearch_dashboards.py
+++ tests/tests_manifests_workflow/test_input_manifests_opensearch_dashboards.py
@@ -12,7 +12,7 @@ from manifests_workflow.input_manifests_opensearch_dashboards import InputManife
 
 
 class TestInputManifestsOpenSearchDashboards(unittest.TestCase):
-    def test_files(self):
+    def test_files(self) -> None:
         files = InputManifestsOpenSearchDashboards.files()
         self.assertTrue(len(files) >= 1)
         manifest = os.path.realpath(
@@ -33,7 +33,8 @@ class TestInputManifestsOpenSearchDashboards(unittest.TestCase):
     @patch("manifests_workflow.input_manifests.InputManifest.from_path")
     @patch("manifests_workflow.input_manifests_opensearch_dashboards.ComponentOpenSearchDashboardsMin")
     @patch("manifests_workflow.input_manifests.InputManifest")
-    def test_update(self, mock_input_manifest, mock_component_opensearch_min, mock_input_manifest_from_path, mock_add_to_cron, *mocks):
+    def test_update(self, mock_input_manifest: MagicMock, mock_component_opensearch_min: MagicMock,
+                    mock_input_manifest_from_path: MagicMock, mock_add_to_cron: MagicMock, *mocks: MagicMock) -> None:
         mock_component_opensearch_min.return_value = MagicMock(name="OpenSearch-Dashboards")
         mock_component_opensearch_min.branches.return_value = ["main", "0.9.0"]
         mock_component_opensearch_min.checkout.return_value = MagicMock(version="0.9.0")
