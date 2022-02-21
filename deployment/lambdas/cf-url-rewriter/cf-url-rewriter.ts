e3252036227e09e0762398f9b429a3d937e97ca9
Tianle Huang <tianleh@amazon.com>
2022-02-21 02:15:00 +0000

    add unit tests
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

--- deployment/lambdas/cf-url-rewriter/cf-url-rewriter.ts
+++ deployment/lambdas/cf-url-rewriter/cf-url-rewriter.ts
@@ -1,12 +1,14 @@
-import { CloudFrontRequestCallback, CloudFrontRequestEvent, Context } from 'aws-lambda';
+import { CloudFrontRequest, CloudFrontRequestCallback, CloudFrontRequestEvent, Context } from 'aws-lambda';
+
+export const handle = (request: CloudFrontRequest) => {
+    // Incoming URLs from ci.opensearch.org will have a '/ci/123/' prefix, remove the prefix path from requests into S3.
+    request.uri = request.uri.replace(/^\/ci\/...\//, '\/');
+}
 
 export async function handler(event: CloudFrontRequestEvent, context: Context, callback: CloudFrontRequestCallback) {
     const request = event.Records[0].cf.request;
 
-    // Incoming URLs from ci.opensearch.org will have a '/ci/123/' prefix, remove the prefix path from requests into S3.
-    request.uri = request.uri.replace(/^\/ci\/...\//, '\/');
-
-    console.log('the new request is ', request);
+    handle(request);
 
     callback(null, request);
 }
