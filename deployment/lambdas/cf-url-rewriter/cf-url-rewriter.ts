87080821bbc35739a4108341d49376ba71e93657
Tianle Huang <tianleh@amazon.com>
2022-02-26 09:42:27 +0000

    add unit tests
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

--- deployment/lambdas/cf-url-rewriter/cf-url-rewriter.ts
+++ deployment/lambdas/cf-url-rewriter/cf-url-rewriter.ts
@@ -1,92 +1,93 @@
 import { CloudFrontRequest, CloudFrontRequestCallback, CloudFrontRequestEvent, Context } from 'aws-lambda';
 
-export const handle = (request: CloudFrontRequest) => {
-    // Incoming URLs from ci.opensearch.org will have a '/ci/123/' prefix, remove the prefix path from requests into S3.
-    request.uri = request.uri.replace(/^\/ci\/...\//, '\/');
+import * as https from 'https'
+
+export async function process(request: CloudFrontRequest, callback: CloudFrontRequestCallback) {
+    if (request.uri.includes("/latest/")) {
+
+        const indexUri = request.uri.replace(/\/latest\/.*/, '/index.json');
+
+        try {
+
+            const data: any = await httpGet('https://' + request.headers.host[0].value + indexUri);
+
+            if (data && data.latest) {
+                callback(null, redirectResponse(request, data.latest));
+            } else {
+                callback(null, errorResponse());
+            }
+        } catch (e) {
+            console.error(e);
+            callback(null, errorResponse());
+        }
+
+    } else {
+        // Incoming URLs from ci.opensearch.org will have a '/ci/123/' prefix, remove the prefix path from requests into S3.
+        request.uri = request.uri.replace(/^\/ci\/...\//, '\/');
+        callback(null, request);
+    }
 }
 
 export async function handler(event: CloudFrontRequestEvent, context: Context, callback: CloudFrontRequestCallback) {
     const request = event.Records[0].cf.request;
 
-
-    if (request.uri.includes("/latest/")) {
-
-        const indexUri = request.uri.replace(/\/latest\/.*/, '/index.json');
-    
-        try {
-    
-          const data = await httpGet('https://' + request.headers.host[0].value + indexUri);
-    
-          if (data && data.latest) {
-            callback(null, redirectResponse(request, data.latest));
-          } else {
-            callback(null, errorResponse());
-          }
-        } catch (e) {
-          console.error(e);
-          callback(null, errorResponse());
-        }
-    
-      } else {
-        request.uri = request.uri.replace(/^\/ci\/...\//, '\/');
-        callback(null, request);
-      }
+    process(request, callback);
 }
 
-function redirectResponse(request, latestNumber) {
+export function redirectResponse(request: CloudFrontRequest, latestNumber: number) {
     return {
-      status: '302',
-      statusDescription: 'Moved temporarily',
-      headers: {
-        'location': [{
-          key: 'Location',
-          value: request.uri.replace(/\/latest\//, '/' + latestNumber + '/'),
-        }],
-        'cache-control': [{
-          key: 'Cache-Control',
-          value: "max-age=3600"
-        }],
-      },
+        status: '302',
+        statusDescription: 'Moved temporarily',
+        headers: {
+            'location': [{
+                key: 'Location',
+                value: request.uri.replace(/\/latest\//, '/' + latestNumber + '/'),
+            }],
+            'cache-control': [{
+                key: 'Cache-Control',
+                value: "max-age=3600"
+            }],
+        },
     };
-  
-  }
-  
-  function errorResponse() {
+
+}
+
+export function errorResponse() {
     return {
-      body: 'The page is not found!',
-      status: '404',
-      statusDescription: 'Not found',
+        body: 'The page is not found!',
+        status: '404',
+        statusDescription: 'Not found',
     };
-  }
-  
-  async function httpGet(url) {
+}
+
+async function httpGet(url: string) {
     return new Promise((resolve, reject) => {
-  
-      https.get(url, (res) => {
-        let body = "";
-  
-        res.on("data", (chunk) => {
-          body += chunk;
-        });
-  
-        res.on("end", () => {
-          try {
-            let json = JSON.parse(body);
-  
-            console.log("json ", json);
-            resolve(json);
-  
-            // do something with JSON
-          } catch (error) {
+
+        https.get(url, (res) => {
+            let body = "";
+
+            res.on("data", (chunk) => {
+                body += chunk;
+            });
+
+            res.on("end", () => {
+                try {
+                    let json = JSON.parse(body);
+
+                    console.log("json ", json);
+                    resolve(json);
+
+                    // do something with JSON
+                } catch (error) {
+                    console.error(error.message);
+                    reject(error);
+                };
+            });
+
+        }).on("error", (error) => {
             console.error(error.message);
             reject(error);
-          };
+
         });
-  
-      }).on("error", (error) => {
-        console.error(error.message);
-        reject(error);
-  
-      });
     });
-  }
+}
--- deployment/test/lambdas/cf-url-rewriter/cf-url-rewriter.test.ts
+++ deployment/test/lambdas/cf-url-rewriter/cf-url-rewriter.test.ts
@@ -1,20 +1,45 @@
-import { CloudFrontRequest } from 'aws-lambda';
-import { handle } from "../../../lambdas/cf-url-rewriter/cf-url-rewriter";
+import { CloudFrontRequest, CloudFrontRequestCallback } from 'aws-lambda';
+import { errorResponse, process, redirectResponse } from "../../../lambdas/cf-url-rewriter/cf-url-rewriter";
 
 test('Lmabda handle uri with ci string', () => {
 
-    let request = { uri: '/ci/dbc/bundle-build-dashboards/1.2.0/428/linux/x64/' } as CloudFrontRequest;
+    // let request = { uri: '/ci/dbc/bundle-build-dashboards/1.2.0/428/linux/x64/' } as CloudFrontRequest;
+    // let callback = {} as CloudFrontRequestCallback;
 
-    handle(request);
+    // process(request, callback);
 
-    expect(request.uri).toBe('/bundle-build-dashboards/1.2.0/428/linux/x64/');
+    // expect(request.uri).toBe('/bundle-build-dashboards/1.2.0/428/linux/x64/');
 });
 
-test('Lmabda handle uri without ci string', () => {
+test('errorResponse', () => {
+    const response = errorResponse();
 
-    let request = { uri: '/bundle-build-dashboards/1.2.0/428/linux/x64/' } as CloudFrontRequest;
-
-    handle(request);
-
-    expect(request.uri).toBe('/bundle-build-dashboards/1.2.0/428/linux/x64/');
+    expect(response.body).toBe('The page is not found!');
+    expect(response.status).toBe('404');
+    expect(response.statusDescription).toBe('Not found');
 });
+
+test('redirectResponse', () => {
+    let request = { uri: '/ci/dbc/bundle-build-dashboards/1.2.0/latest/linux/x64/' } as CloudFrontRequest;
+    const buildNumber = 123;
+
+    const response = redirectResponse(request, buildNumber);
+    console.log(response);
+    console.log(JSON.stringify(response));
+
+
+    expect(response.status).toBe('302');
+    expect(response.statusDescription).toBe('Moved temporarily');
+    expect(response.headers.location).toStrictEqual([{ "key": "Location", "value": "/ci/dbc/bundle-build-dashboards/1.2.0/123/linux/x64/" }]);
+    expect(response.headers['cache-control']).toStrictEqual([{ "key": "Cache-Control", "value": "max-age=3600" }]);
+
+});
+
+// test('Lmabda handle uri without ci string', () => {
+
+//     let request = { uri: '/bundle-build-dashboards/1.2.0/428/linux/x64/' } as CloudFrontRequest;
+
+//     handle(request);
+
+//     expect(request.uri).toBe('/bundle-build-dashboards/1.2.0/428/linux/x64/');
+// });
