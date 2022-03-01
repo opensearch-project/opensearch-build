9cb1ffbbda144b2aa43012b0bc30dad826637f96
Zelin Hao <87548827+zelinh@users.noreply.github.com>
2022-03-01 10:27:14 -0800

    Fix the type checking for Ci (#1678)
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- src/ci_workflow/ci_manifests.py
+++ src/ci_workflow/ci_manifests.py
@@ -7,7 +7,7 @@
 
 import re
 from io import TextIOWrapper
-from typing import Any
+from typing import Type, Union
 
 from ci_workflow.ci_args import CiArgs
 from ci_workflow.ci_input_manifest import CiInputManifest
@@ -16,12 +16,12 @@ from ci_workflow.ci_test_manifest import CiTestManifest
 
 class CiManifests:
     @staticmethod
-    def __klass(filename: str) -> Any:
+    def __klass(filename: str) -> Union[Type[CiTestManifest], Type[CiInputManifest]]:
         if re.search("-test.yml$", filename):
             return CiTestManifest
         else:
             return CiInputManifest
 
     @classmethod
-    def from_file(cls, file: TextIOWrapper, args: CiArgs) -> Any:
+    def from_file(cls, file: TextIOWrapper, args: CiArgs) -> Union[CiTestManifest, CiInputManifest]:
         return cls.__klass(file.name)(file, args)
