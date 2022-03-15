947ab8690b8599d4c01f38f4d68bcdc48125e140
Tianle Huang <60111637+tianleh@users.noreply.github.com>
2022-03-15 12:55:51 -0700

    Use latest image for OSD integ test (#1748)
    * use the osd test image
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * use image
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * fix schema
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * fix flake8
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * add unit test
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * use test manifest
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * use build job name
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * test
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * remove
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * update syntax
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * simplify
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * add test for the ci image
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>
    
    * fix style
    
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

---
+++ tests/tests_manifests/test_test_manifest_with_ci_image.py
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+
+import os
+import unittest
+
+from manifests.test_manifest import TestManifest
+
+
+class TestTestManifest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.maxDiff = None
+        self.data_path = os.path.realpath(os.path.join(os.path.dirname(__file__), "data"))
+        self.manifest_filename = os.path.join(self.data_path, "opensearch-dashboards-test-1.3.0.yml")
+        self.manifest = TestManifest.from_path(self.manifest_filename)
+
+    def test_component(self) -> None:
+        self.assertEqual(self.manifest.name, "OpenSearch Dashboards")
+        self.assertEqual(self.manifest.ci.image.name, "opensearchstaging/ci-runner:ci-runner-rockylinux8-opensearch-dashboards-integtest-v1")
