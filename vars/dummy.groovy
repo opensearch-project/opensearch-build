0e6f890af3bae7ebbee80e0ce4d81eac2a75a4ca
Tianle Huang <tianleh@amazon.com>
2022-03-15 00:45:03 +0000

    dummy
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

---
+++ vars/dummy.groovy
@@ -0,0 +1,40 @@
+void call(Map args = [:]) {
+    git url: 'https://github.com/opensearch-project/opensearch-build.git', branch: 'main'
+
+    def lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
+    def inputManifest = lib.jenkins.InputManifest.new(readYaml(file: args.inputManifest))
+
+    String filename = inputManifest.build.getFilename()
+
+    if (args.artifactUrlX64 == null || args.artifactUrlArm64 ==  null) {
+        echo 'Skipping docker build, one of x64 or arm64 artifacts was not built.'
+        
+    } else {
+        dockerBuild: {
+            build job: 'docker-build',
+            parameters: [
+                string(name: 'DOCKER_BUILD_GIT_REPOSITORY', value: 'https://github.com/opensearch-project/opensearch-build'),
+                string(name: 'DOCKER_BUILD_GIT_REPOSITORY_REFERENCE', value: 'main'),
+                string(name: 'DOCKER_BUILD_SCRIPT_WITH_COMMANDS', value: [
+                        'id',
+                        'pwd',
+                        'cd docker/release',
+                        "curl -sSL ${args.artifactUrlX64} -o ${filename}-x64.tgz",
+                        "curl -sSL ${args.artifactUrlArm64} -o ${filename}-arm64.tgz",
+                        [
+                            'bash',
+                            'build-image-multi-arch.sh',
+                            "-v ${inputManifest.build.version}",
+                            "-f ./dockerfiles/${filename}.al2.dockerfile",
+                            "-p ${filename}",
+                            "-a 'x64,arm64'",
+                            "-r opensearchstaging/${filename}",
+                            "-t '${filename}-x64.tgz,${filename}-arm64.tgz'",
+                            "-n ${BUILD_NUMBER}"
+                        ].join(' ')
+                    ].join(' && ')),
+                booleanParam(name: 'IS_STAGING', value: true)
+            ]
+        }
+    }
+}
