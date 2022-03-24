053b3fa50d80db68ca84d95c18718caa7d8d6f7b
pgodithi <pgodithi@amazon.com>
2022-03-24 12:54:54 -0700

    Added data types
    Signed-off-by: pgodithi <pgodithi@amazon.com>

--- vars/buildFailureMessage.groovy
+++ vars/buildFailureMessage.groovy
@@ -1,10 +1,14 @@
 @NonCPS
+import java.io.Reader;
 def call(){
-    def ERROR_STRING = "ERROR"
-    def message = ""
-    String delimiter = ","
-    def performance_log = currentBuild.rawBuild.getLog(Integer.MAX_VALUE).join('\n')
-    performance_log.eachLine() { line ->
+    def String ERROR_STRING = "ERROR"
+    def String message = ""
+    def String delimiter = ","
+    Reader performance_log = currentBuild.getRawBuild().getLogReader().join('\n')
+    String logContent = IOUtils.toString(performance_log)
+    performance_log.close();
+    performance_log = null
+    logContent.eachLine() { line ->
         if (line.matches(".*$ERROR_STRING.*")) {
             message+=(line+delimiter)
         }
