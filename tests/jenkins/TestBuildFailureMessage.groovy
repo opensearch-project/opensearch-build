b546c8656b31aed5dc2e0d5980eacf9f0038cfe0
pgodithi <pgodithi@amazon.com>
2022-03-24 07:23:00 -0700

    Added Unit tests
    Signed-off-by: pgodithi <pgodithi@amazon.com>

--- tests/jenkins/TestBuildFailureMessage.groovy
+++ tests/jenkins/TestBuildFailureMessage.groovy
@@ -1,10 +1,18 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
 package jenkins.tests
 
 import org.junit.*
 import com.lesfurets.jenkins.unit.*
 import static groovy.test.GroovyAssert.*
 
-class TestBuildFailureMessage extends BasePipelineTest {
+class TestBuildFailureMessage extends BasePipelineTestCPS {
 
     def buildFailureMessage
 
