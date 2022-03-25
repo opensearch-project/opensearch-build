b2dd3c68fb9107e5bad19c788be248a44db8e4fa
pgodithi <pgodithi@amazon.com>
2022-03-25 08:30:34 -0700

    Buffer Reader to store the jenkins log and read from this stream
    Signed-off-by: pgodithi <pgodithi@amazon.com>

--- vars/buildFailureMessage.groovy
+++ vars/buildFailureMessage.groovy
@@ -1,17 +1,21 @@
+import com.cloudbees.groovy.cps.NonCPS
+import org.apache.commons.io.IOUtils
 @NonCPS
 def call(){
-    def String ERROR_STRING = "ERROR"
-    def String message = ""
-    def String delimiter = ","
-    def performance_log = currentBuild.rawBuild.getLog(Integer.MAX_VALUE).join('\n')
-    performance_log.eachLine() { line ->
+    String ERROR_STRING = "ERROR"
+    List<String> message = []
+    String delimiter = ","
+    Reader performance_log = currentBuild.getRawBuild().getLogReader()
+    String logContent = IOUtils.toString(performance_log)
+    performance_log.close()
+    performance_log = null
+    logContent.eachLine() { line ->
         if (line.matches(".*$ERROR_STRING.*")) {
-            message+=(line+delimiter)
+            message.add(line)
         }
     }
-    message=message.replaceAll(",", "\n")
     if(message.isEmpty()){
-        message="Build failed"
+        message=["Build failed"]
     }
     return message
 }
