2b57883c64e09306489ce7a9f1d114a3d4bfcc95
Tianle Huang <tianleh@amazon.com>
2022-03-26 01:39:26 +0000

    fix missing types
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

--- src/paths/assemble_output_dir.py
+++ src/paths/assemble_output_dir.py
@@ -4,9 +4,10 @@
 # this file be licensed under the Apache-2.0 license or a
 # compatible open source license.
 
+from turtle import st
 from paths.output_dir import OutputDir
 
 
 class AssembleOutputDir(OutputDir):
-    def __init__(cls, filename: str, distribution, cwd: str = None, makedirs: bool = True) -> None:
+    def __init__(cls, filename: str, distribution: str, cwd: str = None, makedirs: bool = True) -> None:
         super().__init__("dist", filename, distribution, cwd, makedirs)
