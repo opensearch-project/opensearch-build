c1d6f1136bcd125a5acdf208c7de27f6cb13b24a
Kawika Avilla <kavilla414@gmail.com>
2022-03-27 11:34:51 +0000

    trailing space
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>

--- src/build_workflow/build_target.py
+++ src/build_workflow/build_target.py
@@ -81,7 +81,7 @@ class BuildTarget:
 
     @property
     def compatible_opensearch_dashboards_component_versions(self) -> List[str]:
-        versions = self.compatible_component_versions 
+        versions = self.compatible_component_versions
         versions.extend([self.version + ".0"] if self.version + ".0" not in self.compatible_component_versions else [])
         return versions
 
