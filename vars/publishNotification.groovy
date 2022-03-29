aa6b8294daf1818bdd70e935c222bddb5121e107
Prudhvi Godithi <pgodithi@amazon.com>
2022-03-29 15:59:32 -0400

    Added enhancement to slack publish with build failure cause (#1819)
    * Added enhancement to slack publish with build failure cause
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added Unit tests
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added Unit tests
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added Unit tests
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added Unit tests
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added data types
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added data types
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added data types
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added data types
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added data types
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added data types
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added data types
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added data types
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Buffer Reader to store the jenkins log and read from this stream
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Removed delimiter
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Removed delimiter
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added test files
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added jenkins BuildFailureMessage shared lib test cases
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Fixing all failed test cases
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Fixing all failed test cases: testing
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Fixing all failed test cases: testing
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Fixing double-quotes bug in buildFailureMessage.groovy file
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Fixing double-quotes bug in buildFailureMessage.groovy file
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added buildFailureMessage() pipelines
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Added buildFailureMessage() pipelines
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * removing buildFailureMessage() for dashboard jenkinsfile
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>

--- vars/publishNotification.groovy
+++ vars/publishNotification.groovy
@@ -1,11 +1,13 @@
 void call(Map args = [:]) {
     text = ([
-        "${args.icon} ${JOB_NAME} [${BUILD_NUMBER}] ${args.message}",
-        "Build: ${BUILD_URL}",
-        args.manifest,
+        "${args.icon}", 
+        "JOB_NAME=${JOB_NAME}",
+        "BUILD_NUMBER=[${BUILD_NUMBER}]",
+        "MESSAGE=${args.message}",
+        "BUILD_URL: ${BUILD_URL}",
+        "MANIFEST: ${args.manifest}",
         args.extra
     ] - null).join("\n")
-
     withCredentials([string(credentialsId: args.credentialsId, variable: 'WEBHOOK_URL')]) {
         sh ([
             'curl',
