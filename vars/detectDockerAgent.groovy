a1fddbd33cd2105874c966048b6ffdb3050f0413
Tianle Huang <tianleh@amazon.com>
2022-04-04 21:06:19 +0000

    fix
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

--- vars/detectDockerAgent.groovy
+++ vars/detectDockerAgent.groovy
@@ -1,5 +1,5 @@
 Map call(Map args = [:]) {
-    git url: 'https://github.com/tianleh/opensearch-build.git', branch: 'add-new-folder'
+    git url: 'https://github.com/opensearch-project/opensearch-build.git', branch: 'main'
     def lib = library(identifier: "jenkins@20211123", retriever: legacySCM(scm))
     String manifest = args.manifest ?: "manifests/${INPUT_MANIFEST}"
     def inputManifest = lib.jenkins.InputManifest.new(readYaml(file: manifest))
