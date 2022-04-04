0f02c6fc295c14b56b516d0f5fcfdb85e1f9c24e
Prudhvi Godithi <pgodithi@amazon.com>
2022-04-04 15:01:30 -0700

    Fixed buildFailureMessage.groovy (#1902)
    Signed-off-by: pgodithi <pgodithi@amazon.com>

--- vars/buildFailureMessage.groovy
+++ vars/buildFailureMessage.groovy
@@ -2,18 +2,22 @@ import com.cloudbees.groovy.cps.NonCPS
 import org.apache.commons.io.IOUtils
 @NonCPS
 def call(){
-    String ERROR_STRING = "ERROR"
+    String ERROR_STRING = "Error building"
     List<String> message = []
     Reader performance_log = currentBuild.getRawBuild().getLogReader()
     String logContent = IOUtils.toString(performance_log)
     performance_log.close()
     performance_log = null
     logContent.eachLine() { line ->
-        if (line.matches(".*?$ERROR_STRING(.*?)")) {
-            line=line.replace("\"", "")
+        line=line.replace("\"", "")
+        //Gets the exact match for Error building
+        def java.util.regex.Matcher match = (line =~ /$ERROR_STRING.*/) 
+        if (match.find()) {
+            line=match[0]
             message.add(line)
         }
     }
+    //if no match returns as Build failed
     if(message.isEmpty()){
         message=["Build failed"]
     }
