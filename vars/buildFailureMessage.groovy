2f83e2c4639f28142a4c82819785bfbdf897716a
pgodithi <pgodithi@amazon.com>
2022-04-04 13:40:52 -0700

    Fix ERROR string bug added Error Building
    Signed-off-by: pgodithi <pgodithi@amazon.com>

--- vars/buildFailureMessage.groovy
+++ vars/buildFailureMessage.groovy
@@ -2,7 +2,7 @@ import com.cloudbees.groovy.cps.NonCPS
 import org.apache.commons.io.IOUtils
 @NonCPS
 def call(){
-    String ERROR_STRING = "Error"
+    String ERROR_STRING = "Error building"
     List<String> message = []
     Reader performance_log = currentBuild.getRawBuild().getLogReader()
     String logContent = IOUtils.toString(performance_log)
