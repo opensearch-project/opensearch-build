39446d400697cf9959a843c21c7af75c02bc3bb3
Abhinav Gupta <abhng@amazon.com>
2022-04-05 16:23:55 -0700

    Update new docker jobs to make ECR optional (#1859)
    * update docker jobs with boolean ECR option
    
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>
    
    * change expression
    
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>
    
    * update when condition
    
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>
    
    * update when condition
    
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>
    
    * update description
    
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>
    
    * updated job to have dropdown for ecr and dockerhub
    
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>
    
    * update default
    
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>
    
    * update default
    
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>
    
    * updated the job timeout to be 1 hour
    
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>
    
    * update regression files
    
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>

--- tests/jenkins/TestDockerBuildWithEcrJob.groovy
+++ tests/jenkins/TestDockerBuildWithEcrJob.groovy
@@ -22,19 +22,31 @@ class TestDockerBuildWithEcrJob extends BuildPipelineTest {
         binding.setVariable('IMAGE_TAG', imageTag)
         binding.setVariable('DOCKER_BUILD_SCRIPT_WITH_COMMANDS', 'dummy_command')
         binding.setVariable('AWS_ACCOUNT_PUBLIC', accountName)
+
     }
 
     @Test
-    public void testDockerForEcrJobStaging(){
+    public void testDockerForEcrOnlyJobStaging() {
 
+        binding.setVariable('PLATFORM', 'ECR')
+
+        helper.registerAllowedMethod("git", [Map])
+
+        super.testPipeline("jenkins/docker-ecr/docker-build-with-ecr.jenkinsfile",
+                "tests/jenkins/jenkinsjob-regression-files/docker-ecr/docker-build-with-only-ecr.jenkinsfile")
+    }
+
+    @Test
+    public void testDockerForEcrAndDockerhubJobStaging() {
+
+        binding.setVariable('PLATFORM', 'docker-hub + ECR')
         binding.setVariable('DOCKER_USERNAME ', 'docker_username')
         binding.setVariable('DOCKER_PASSWORD', 'docker_password')
 
         helper.registerAllowedMethod("git", [Map])
 
         super.testPipeline("jenkins/docker-ecr/docker-build-with-ecr.jenkinsfile",
-                "tests/jenkins/jenkinsjob-regression-files/docker-ecr/docker-build-with-ecr.jenkinsfile")
-
-        }
+                "tests/jenkins/jenkinsjob-regression-files/docker-ecr/docker-build-with-ecr-dockerhub.jenkinsfile")
+    }
 
 }
---
+++ tests/jenkins/TestDockerPromoteWithEcrOnlyJob.groovy
@@ -0,0 +1,35 @@
+import jenkins.tests.BuildPipelineTest
+import org.junit.Before
+import org.junit.Test
+
+class TestDockerPromoteWithEcrOnlyJob extends BuildPipelineTest {
+
+    @Before
+    void setUp() {
+        String imageRepository = 'ci-runner-staging'
+        String imageTag = 'latest'
+        String accountName = 'aws_account_artifact'
+        String sourceImagePath = "opensearchstaging/${imageRepository}:${imageTag}"
+
+        this.registerLibTester(new CopyContainerLibTester(sourceImagePath,
+                "public.ecr.aws/p5f6l6i3/${imageRepository}:${imageTag}",
+                'ecr',
+                'public.ecr.aws/p5f6l6i3',
+                accountName))
+
+        super.setUp()
+
+        binding.setVariable('IMAGE_REPOSITORY', imageRepository)
+        binding.setVariable('IMAGE_TAG', imageTag)
+        binding.setVariable('AWS_ACCOUNT_ARTIFACT', accountName)
+        binding.setVariable('PLATFORM', 'ECR')
+
+    }
+
+    @Test
+    public void testDockerForEcrAndDockerhubJobProductionWithECR(){
+        super.testPipeline("jenkins/docker-ecr/docker-ecr-promote.jenkinsfile",
+                "tests/jenkins/jenkinsjob-regression-files/docker-ecr/docker-ecr-promote-with-ECR-only.jenkinsfile")
+    }
+
+}
