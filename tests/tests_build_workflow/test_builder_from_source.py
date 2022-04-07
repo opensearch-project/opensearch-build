f4d142524891764d797b101fd2107046e84501d6
Peter Zhu <zhujiaxi@amazon.com>
2022-04-07 17:46:09 +0000

    Add not found component tests to ensure it is calling opensearch default script
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- tests/tests_build_workflow/test_builder_from_source.py
+++ tests/tests_build_workflow/test_builder_from_source.py
@@ -52,6 +52,18 @@ class TestBuilderFromSource(unittest.TestCase):
             ),
         )
 
+        self.builder_distribution_support_component_not_found = BuilderFromSource(
+            InputComponentFromSource({"name": "not_found_component", "repository": "url", "ref": "ref"}),
+            BuildTarget(
+                name="OpenSearch",
+                version="1.3.0",
+                platform="linux",
+                architecture="x64",
+                distribution="rpm",
+                snapshot=False,
+            ),
+        )
+
     def test_builder(self) -> None:
         self.assertEqual(self.builder.component.name, "sample_component")
 
@@ -142,7 +154,7 @@ class TestBuilderFromSource(unittest.TestCase):
         build_recorder.record_component.assert_called_with("sample_component", self.builder.git_repo)
 
     @patch("build_workflow.builder_from_source.GitRepository")
-    def test_build_snapshot_qualiier(self, mock_git_repo: Mock) -> None:
+    def test_build_snapshot_qualifier(self, mock_git_repo: Mock) -> None:
         self.builder.target.snapshot = True
         self.builder.target.qualifier = "alpha1"
         mock_git_repo.return_value = MagicMock(working_directory="dir")
@@ -164,6 +176,30 @@ class TestBuilderFromSource(unittest.TestCase):
             )
         )
         build_recorder.record_component.assert_called_with("sample_component", self.builder.git_repo)
+
+    @patch("build_workflow.builder_from_source.GitRepository")
+    def test_build_snapshot_qualifier_component_not_found(self, mock_git_repo: Mock) -> None:
+        self.builder_distribution_support_component_not_found.target.snapshot = True
+        self.builder_distribution_support_component_not_found.target.qualifier = "alpha1"
+        mock_git_repo.return_value = MagicMock(working_directory="dir")
+        build_recorder = MagicMock()
+        self.builder_distribution_support_component_not_found.checkout("dir")
+        self.builder_distribution_support_component_not_found.build(build_recorder)
+        mock_git_repo.return_value.execute.assert_called_with(
+            " ".join(
+                [
+                    "bash",
+                    os.path.realpath(os.path.join(ScriptFinder.default_scripts_path, "opensearch", "build.sh")),
+                    "-v 1.3.0",
+                    "-q alpha1",
+                    "-p linux",
+                    "-a x64",
+                    "-s true",
+                    "-o builds",
+                ]
+            )
+        )
+        build_recorder.record_component.assert_called_with("not_found_component", mock_git_repo.return_value)
 
     def mock_os_walk(self, artifact_path: str) -> List[Any]:
         if artifact_path.endswith(os.path.join("dir", "builds", "core-plugins")):
