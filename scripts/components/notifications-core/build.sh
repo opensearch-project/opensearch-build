c9b3a51edc3c5a6a3f53acf917a6642b17874de6
Daniel Doubrovkine (dB.) <dblock@dblock.org>
2022-04-12 21:58:17 -0400

    Added notifications-core and notifications to 2.0 and add integTest manifest (#1957)
    * Added notifications-core and notifications to 2.0.
    
    Signed-off-by: dblock <dblock@dblock.org>
    
    * Remove scripts from opensearch-build.
    
    Signed-off-by: dblock <dblock@dblock.org>
    
    * Remove dashboards notifications scripts to use the default one
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Remove CI checks for lack of gradlew.
    
    Signed-off-by: dblock <dblock@dblock.org>
    
    * Fix the second subfolder location after core
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Add notifications related build scripts to build repo
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Tweak build scripts to correctly move the zips
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    Co-authored-by: Peter Zhu <zhujiaxi@amazon.com>

--- scripts/components/notifications-core/build.sh
+++ scripts/components/notifications-core/build.sh
@@ -13,7 +13,7 @@ function usage() {
     echo ""
     echo "Arguments:"
     echo -e "-v VERSION\t[Required] OpenSearch version."
-    echo -e "-q QUALIFIER\t[Optional] Version qualifier."
+    echo -e "-q QUALIFIER\t[Optional] Build qualifier."
     echo -e "-s SNAPSHOT\t[Optional] Build a snapshot, default is 'false'."
     echo -e "-p PLATFORM\t[Optional] Platform, ignored."
     echo -e "-a ARCHITECTURE\t[Optional] Build architecture, ignored."
@@ -58,25 +58,21 @@ while getopts ":h:v:q:s:o:p:a:" arg; do
 done
 
 if [ -z "$VERSION" ]; then
-    echo "Error: You must specify the OpenSearch Dashboards version"
+    echo "Error: You must specify the OpenSearch version"
     usage
     exit 1
 fi
 
+[[ ! -z "$QUALIFIER" ]] && VERSION=$VERSION-$QUALIFIER
+[[ "$SNAPSHOT" == "true" ]] && VERSION=$VERSION-SNAPSHOT
 [ -z "$OUTPUT" ] && OUTPUT=artifacts
-[ ! -z "$QUALIFIER" ] && QUALIFIER_IDENTIFIER="-$QUALIFIER"
 
-mkdir -p $OUTPUT/plugins
-# For hybrid plugin it actually resides in 'notificationsDashboards/dashboards-notifications'
-PLUGIN_FOLDER=$(basename "$PWD")
-PLUGIN_NAME=$(basename $(dirname "$PWD"))
-# TODO: [CLEANUP] Needed OpenSearch Dashboards git repo to build the required modules for plugins
-# This makes it so there is a dependency on having Dashboards pulled already.
-cp -r ../$PLUGIN_FOLDER/ ../../OpenSearch-Dashboards/plugins
-echo "BUILD MODULES FOR $PLUGIN_NAME"
-(cd ../../OpenSearch-Dashboards && source $NVM_DIR/nvm.sh && nvm use && yarn osd bootstrap)
-echo "BUILD RELEASE ZIP FOR $PLUGIN_NAME"
-(cd ../../OpenSearch-Dashboards && source $NVM_DIR/nvm.sh && nvm use && cd plugins/$PLUGIN_FOLDER && yarn plugin_helpers build --opensearch-dashboards-version=$VERSION$QUALIFIER_IDENTIFIER)
-echo "COPY $PLUGIN_NAME.zip"
-cp -r ../../OpenSearch-Dashboards/plugins/$PLUGIN_FOLDER/build/$PLUGIN_NAME-$VERSION$QUALIFIER_IDENTIFIER.zip $OUTPUT/plugins/
-rm -rf ../../OpenSearch-Dashboards/plugins/$PLUGIN_FOLDER
+
+# Go to the first notifications folder, which holds the core folder and the second notifications folder
+cd ../
+./gradlew publishToMavenLocal -PexcludeTests="**/SesChannelIT*" -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT -Dbuild.version_qualifier=$QUALIFIER
+./gradlew assemble --no-daemon --refresh-dependencies -DskipTests=true -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT -Dbuild.version_qualifier=$QUALIFIER
+
+mkdir -p core/$OUTPUT/plugins
+notifCoreZipPath=$(ls core/build/distributions/ | grep .zip)
+cp -v core/build/distributions/$notifCoreZipPath core/$OUTPUT/plugins
