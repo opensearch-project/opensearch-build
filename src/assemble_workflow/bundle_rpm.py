061a757e53a6c58425b97ef06573769e7d3ad03d
Peter Zhu <zhujiaxi@amazon.com>
2022-04-14 17:52:44 -0400

    Remove opensearch-env backup as core has fixed the source in rpm (#1974)
    * Remove opensearch-env backup as core has fixed the source
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Remove unused lines
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- src/assemble_workflow/bundle_rpm.py
+++ src/assemble_workflow/bundle_rpm.py
@@ -26,7 +26,6 @@ class BundleRpm:
         min_source_path = os.path.join(dest, 'usr', 'share', self.filename)
         min_dest_path = os.path.join(dest, self.min_path)
         min_config_path = os.path.join(dest, 'etc', self.filename)
-        min_bin_env_path = os.path.join(min_dest_path, 'bin', f"{self.filename}-env")
 
         # Convert rpm to cpio so we can extract the content
         logging.info(f"Convert rpm to cpio for extraction: {self.package_path} to {cpio_path}")
@@ -63,35 +62,17 @@ class BundleRpm:
         # https://github.com/opensearch-project/OpenSearch/issues/2092
         os.environ[f"{self.filename.upper()}_PATH_CONF"] = min_config_path
 
-        if os.path.exists(min_bin_env_path):
-            # Backup original file
-            shutil.copy2(min_bin_env_path, f"{min_bin_env_path}.backup")
-            # Prevent sourcing as file is only in place after rpm installation
-            # So that min can install plugin zips
-            # Have to decode then encode back to ascii due to mypy complains TextIO not equals to BinaryIO
-            with open(min_bin_env_path, 'rb') as fp:
-                min_bin_env_lines = fp.read().decode('ascii')
-
-            with open(min_bin_env_path, 'wb') as fp:
-                fp.write(min_bin_env_lines.replace('source', '#source').encode('ascii'))
-
     def build(self, name: str, dest: str, archive_path: str, build_cls: BuildManifest.Build) -> None:
         # extract dest and build dest are not the same, this is restoring the extract dest
         # mainly due to rpm requires several different setups compares to tarball and zip
         ext_dest = os.path.dirname(archive_path)
         min_source_path = os.path.join(ext_dest, 'usr', 'share', self.filename)
         min_dest_path = os.path.join(ext_dest, self.min_path)
-        min_bin_env_path = os.path.join(min_dest_path, 'bin', f"{self.filename}-env")
         bundle_artifact_path: str = None
 
         # Remove env var
         logging.info('Organize folder structure before generating rpm')
         os.environ.pop('OPENSEARCH_PATH_CONF', None)
-
-        # Restore config file and core folder to original location
-        if os.path.exists(f"{min_bin_env_path}.backup"):
-            logging.info(f"Restore {min_bin_env_path}.backup to {min_bin_env_path}")
-            shutil.move(f"{min_bin_env_path}.backup", min_bin_env_path)
 
         shutil.move(min_dest_path, min_source_path)
 
