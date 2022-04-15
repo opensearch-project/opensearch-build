2fc1e8610e4d6f17ea5a4c89a2c6210aa1dc0b46
sruti1312 <srutiparthiban@gmail.com>
2022-04-15 14:13:19 -0700

    Update PA directory names
    Signed-off-by: sruti1312 <srutiparthiban@gmail.com>

--- scripts/legacy/tar/linux/opensearch-tar-install.sh
+++ scripts/legacy/tar/linux/opensearch-tar-install.sh
@@ -9,10 +9,10 @@ KNN_LIB_DIR=$OPENSEARCH_HOME/plugins/opensearch-knn/lib
 bash $OPENSEARCH_HOME/plugins/opensearch-security/tools/install_demo_configuration.sh -y -i -s
 
 ##Perf Plugin
-chmod 755 $OPENSEARCH_HOME/plugins/opensearch-performance-analyzer/pa_bin/performance-analyzer-agent
+chmod 755 $OPENSEARCH_HOME/plugins/opensearch-performance-analyzer/bin/performance-analyzer-agent
 chmod 755 $OPENSEARCH_HOME/bin/performance-analyzer-agent-cli
 echo "done security"
-PA_AGENT_JAVA_OPTS="-Dlog4j.configurationFile=$OPENSEARCH_HOME/plugins/opensearch-performance-analyzer/pa_config/log4j2.xml \
+PA_AGENT_JAVA_OPTS="-Dlog4j.configurationFile=$OPENSEARCH_HOME/plugins/opensearch-performance-analyzer/config/log4j2.xml \
               -Xms64M -Xmx64M -XX:+UseSerialGC -XX:CICompilerCount=1 -XX:-TieredCompilation -XX:InitialCodeCacheSize=4096 \
               -XX:MaxRAM=400m"
 
@@ -26,7 +26,7 @@ if ! grep -q '## OpenSearch Performance Analyzer' $OPENSEARCH_HOME/config/jvm.op
    echo '## OpenSearch Performance Analyzer' >> $OPENSEARCH_HOME/config/jvm.options
    echo "-Dclk.tck=$CLK_TCK" >> $OPENSEARCH_HOME/config/jvm.options
    echo "-Djdk.attach.allowAttachSelf=true" >> $OPENSEARCH_HOME/config/jvm.options
-   echo "-Djava.security.policy=$OPENSEARCH_HOME/plugins/opensearch-performance-analyzer/pa_config/opensearch_security.policy" >> $OPENSEARCH_HOME/config/jvm.options
+   echo "-Djava.security.policy=$OPENSEARCH_HOME/plugins/opensearch-performance-analyzer/config/opensearch_security.policy" >> $OPENSEARCH_HOME/config/jvm.options
    echo "--add-opens=jdk.attach/sun.tools.attach=ALL-UNNAMED" >> $OPENSEARCH_HOME/config/jvm.options
 fi
 echo "done plugins"
