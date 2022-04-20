6c8817bd7241edd8dfb95f77d006ca6af1f3dff9
Abhinav Gupta <abhng@amazon.com>
2022-04-20 12:30:49 -0700

    tag image as latest (#2001)
    Signed-off-by: Abhinav Gupta <abhng@amazon.com>

--- tests/jenkins/TestDockerPromoteWithLatestTagDockerHubJob.groovy
+++ tests/jenkins/TestDockerPromoteWithLatestTagDockerHubJob.groovy
@@ -2,17 +2,22 @@ import jenkins.tests.BuildPipelineTest
 import org.junit.Before
 import org.junit.Test
 
-class TestDockerPromoteWithDockerhubOnlyJob extends BuildPipelineTest {
+class TestDockerPromoteWithLatestTagDockerHubJob extends BuildPipelineTest {
 
     @Before
     void setUp() {
         String imageRepository = 'ci-runner-staging'
-        String imageTag = 'latest'
+        String imageTag = '1.3.0'
         String accountName = 'aws_account_artifact'
         String sourceImagePath = "opensearchstaging/${imageRepository}:${imageTag}"
 
         this.registerLibTester(new CopyContainerLibTester(sourceImagePath,
                 "opensearchproject/${imageRepository}:${imageTag}",
+                'docker',
+                'jenkins-staging-docker-prod-token'))
+
+        this.registerLibTester(new CopyContainerLibTester(sourceImagePath,
+                "opensearchproject/${imageRepository}:latest",
                 'docker',
                 'jenkins-staging-docker-prod-token'))
 
@@ -22,14 +27,14 @@ class TestDockerPromoteWithDockerhubOnlyJob extends BuildPipelineTest {
         binding.setVariable('IMAGE_TAG', imageTag)
         binding.setVariable('AWS_ACCOUNT_ARTIFACT', accountName)
         binding.setVariable('PLATFORM', 'docker-hub')
-        binding.setVariable('TAG_LATEST', 'false')
+        binding.setVariable('TAG_LATEST', 'true')
 
     }
 
     @Test
     public void testDockerForEcrJobProductionWithDockerhubOnly(){
         super.testPipeline("jenkins/docker-ecr/docker-ecr-promote.jenkinsfile",
-                "tests/jenkins/jenkinsjob-regression-files/docker-ecr/docker-ecr-promote-with-dockerhub-only.jenkinsfile")
+                "tests/jenkins/jenkinsjob-regression-files/docker-ecr/docker-ecr-promote-with-latestTag-dockerhub-only.jenkinsfile")
     }
 
 }
--- tests/jenkins/TestDockerPromoteWithLatestTagEcrOnlyJob.groovy
+++ tests/jenkins/TestDockerPromoteWithLatestTagEcrOnlyJob.groovy
@@ -2,17 +2,23 @@ import jenkins.tests.BuildPipelineTest
 import org.junit.Before
 import org.junit.Test
 
-class TestDockerPromoteWithEcrOnlyJob extends BuildPipelineTest {
+class TestDockerPromoteWithLatestTagEcrOnlyJob extends BuildPipelineTest {
 
     @Before
     void setUp() {
         String imageRepository = 'ci-runner-staging'
-        String imageTag = 'latest'
+        String imageTag = '1.3.0'
         String accountName = 'aws_account_artifact'
         String sourceImagePath = "opensearchstaging/${imageRepository}:${imageTag}"
 
         this.registerLibTester(new CopyContainerLibTester(sourceImagePath,
                 "public.ecr.aws/p5f6l6i3/${imageRepository}:${imageTag}",
+                'ecr',
+                'public.ecr.aws/p5f6l6i3',
+                accountName))
+
+        this.registerLibTester(new CopyContainerLibTester(sourceImagePath,
+                "public.ecr.aws/p5f6l6i3/${imageRepository}:latest",
                 'ecr',
                 'public.ecr.aws/p5f6l6i3',
                 accountName))
@@ -23,14 +29,14 @@ class TestDockerPromoteWithEcrOnlyJob extends BuildPipelineTest {
         binding.setVariable('IMAGE_TAG', imageTag)
         binding.setVariable('AWS_ACCOUNT_ARTIFACT', accountName)
         binding.setVariable('PLATFORM', 'ECR')
-        binding.setVariable('TAG_LATEST', 'false')
+        binding.setVariable('TAG_LATEST', 'true')
 
     }
 
     @Test
     public void testDockerForEcrAndDockerhubJobProductionWithECR(){
         super.testPipeline("jenkins/docker-ecr/docker-ecr-promote.jenkinsfile",
-                "tests/jenkins/jenkinsjob-regression-files/docker-ecr/docker-ecr-promote-with-ECR-only.jenkinsfile")
+                "tests/jenkins/jenkinsjob-regression-files/docker-ecr/docker-ecr-promote-with-latestTag-ECR-only.jenkinsfile")
     }
 
 }
