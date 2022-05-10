fd028ed95f2db7c4ffeed1af8ee4775bd2c5b225
Prudhvi Godithi <pgodithi@amazon.com>
2022-05-10 09:55:22 -0400

    Add --component support for jenkins build pipelines (From PR [2095]) (#2100)
    * Added tests
    
    Signed-off-by: pgodithi <pgodithi@amazon.com>
    
    * Add tests without component parameter and for rpm distribution.
    
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>
    
    Co-authored-by: Zelin Hao <zelinhao@amazon.com>

---
+++ tests/jenkins/TestbuildManifestVar.groovy
@@ -0,0 +1,20 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package jenkins.tests
+
+import org.junit.*
+
+class TestbuildManifestVar extends BuildPipelineTest {
+    
+    @Test
+    void testbuildManifest() {
+        super.testPipeline("tests/jenkins/jobs/BuildShManifest_Jenkinsfile")
+    }
+}
+
