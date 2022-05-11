b038dee2a99818d76b2af7d86c10bd93929ffef4
Peter Zhu <zhujiaxi@amazon.com>
2022-05-11 20:42:52 +0000

    Make rpm build on centos7 and assemble on rocky8 to generate digest correctly for FIPS-enabled nodes
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- vars/archiveAssembleUpload.groovy
+++ vars/archiveAssembleUpload.groovy
@@ -2,16 +2,11 @@ def call(Map args = [:]) {
     lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
     def inputManifestObj = lib.jenkins.InputManifest.new(readYaml(file: args.inputManifest))
 
+    String stashName = "build-archive-${args.platform}-${args.architecture}-${args.distribution}-${JOB_NAME}-${BUILD_NUMBER}"
+    echo "Unstashing ${stashName} before starting the assemble process"
+    unstash "${stashName}"
+
     echo "Assembling ${args.inputManifest}"
-
-    copyArtifacts(
-        filter: "${args.distribution}/*.zip",
-        fingerprintArtifacts: true,
-        projectName: "${JOB_NAME}",
-        selector: specific("${BUILD_NUMBER}")
-    )
-
-    unzip(zipFile: "${args.distribution}/archived-builds.zip")
 
     String buildManifest = "${args.distribution}/builds/${inputManifestObj.build.getFilename()}/manifest.yml"
     def buildManifestObj = lib.jenkins.BuildManifest.new(readYaml(file: buildManifest))
--- vars/buildArchive.groovy
+++ vars/buildArchive.groovy
@@ -2,13 +2,9 @@ void call(Map args = [:]) {
     def lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
     buildManifest(args)
 
-    echo "Archiving into zip: ${args.distribution}/builds/**, ${args.inputManifest}"
-
-    zip(
-        zipFile: "${args.distribution}/archived-builds.zip",
-        archive: true,
-        glob: "${args.distribution}/builds/**"
-    )
+    String stashName = "build-archive-${args.platform}-${args.architecture}-${args.distribution}-${JOB_NAME}-${BUILD_NUMBER}"
+    echo "Stashing builds to assemble later with name: ${stashName}"
+    stash includes: "${args.distribution}/builds/**", name: "${stashName}"
 
     lib.jenkins.Messages.new(this).add("${STAGE_NAME}", "Built ${STAGE_NAME}.")
 }
