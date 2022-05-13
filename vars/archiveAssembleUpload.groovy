3d6e80b7b422a94cecd2059624edad37aef99903
Peter Zhu <zhujiaxi@amazon.com>
2022-05-13 09:54:05 -0400

    Make rpm build on centos7 and assemble on rockylinux8 to generate digest correctly for FIPS-enabled nodes (#2113)
    * Make rpm build on centos7 and assemble on rocky8 to generate digest correctly for FIPS-enabled nodes
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Add jenkins tests
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Remove unused params
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Change stashName to be called from args
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- vars/archiveAssembleUpload.groovy
+++ vars/archiveAssembleUpload.groovy
@@ -2,16 +2,11 @@ def call(Map args = [:]) {
     lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
     def inputManifestObj = lib.jenkins.InputManifest.new(readYaml(file: args.inputManifest))
 
+    String stashName = "${args.stashName}"
+    echo "Unstashing ${stashName} before starting the assemble process"
+    unstash "${stashName}"
+
     echo "Assembling ${args.inputManifest}"
-
-    copyArtifacts(
-        filter: "${args.distribution}/*.zip",
-        fingerprintArtifacts: true,
-        projectName: "${JOB_NAME}",
-        selector: specific("${BUILD_NUMBER}")
-    )
-
-    unzip(zipFile: "${args.distribution}/archived-builds.zip")
 
     String buildManifest = "${args.distribution}/builds/${inputManifestObj.build.getFilename()}/manifest.yml"
     def buildManifestObj = lib.jenkins.BuildManifest.new(readYaml(file: buildManifest))
