293aca9f1337de94d87450c78149b181a66eafaa
pgodithi <pgodithi@amazon.com>
2022-05-14 07:58:32 -0400

    Ecr public
    Signed-off-by: pgodithi <pgodithi@amazon.com>

--- vars/copyContainer.groovy
+++ vars/copyContainer.groovy
@@ -6,6 +6,7 @@
  * @param args.destinationImagePath Follows the format NAME[:TAG|@DIGEST], e.g. opensearchproject/opensearch:1.2.4
  * @param args.destinationType The docker registry, currently supports 'docker' or 'ecr'
  * @param args.destinationCredentialIdentifier The credential identifier registered in the jenkins system associated with the NAME
+ * @param args.ecrProd (true/false) to choose between production and staging environments
  */
 void call(Map args = [:]) {
 
@@ -22,18 +23,18 @@ void call(Map args = [:]) {
         if(args.ecrProd) {
             withAWS(role: "${ARTIFACT_PROMOTION_ROLE_NAME}", roleAccount: "${AWS_ACCOUNT_ARTIFACT}", duration: 900, roleSessionName: 'jenkins-session') {
             sh """
+                set +x
                 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${args.destinationCredentialIdentifier}
-                gcrane cp ${args.sourceImagePath} ${args.destinationImagePath}
-                docker logout ${args.destinationCredentialIdentifier}
             """
+            sh "gcrane cp ${args.sourceImagePath} ${args.destinationImagePath}"
             }
         }
         if(!args.ecrProd) {
             sh """
                 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${args.destinationCredentialIdentifier}
                 gcrane cp ${args.sourceImagePath} ${args.destinationImagePath}
-                docker logout ${args.destinationCredentialIdentifier}
             """
         }
     }
+    sh "docker logout ${args.destinationCredentialIdentifier}"
 }
