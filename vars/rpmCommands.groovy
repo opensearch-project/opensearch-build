c24ae0a2f91b4dfe858b4e6fa21e08b510817b25
Peter Zhu <zhujiaxi@amazon.com>
2022-06-15 07:54:40 +0000

    Use yum downloader from yum-utils for rpm validation to compatible with both yum and dnf
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- vars/rpmCommands.groovy
+++ vars/rpmCommands.groovy
@@ -9,15 +9,17 @@ def call(Map args = [:]) {
     def command = args.command
     def product = args.product
     def repoFileURL = args.repoFileURL
+    def subPubKeyURL = "https://artifacts.opensearch.org/publickeys/opensearch.pgp"
     switch (command) {
         case ("setup"):
             sh ("cd /etc/yum.repos.d/ && curl -sSLO $repoFileURL && cd -")
+            sh ("curl -sSLO $subPubKeyURL && rpm --import opensearch.pgp")
             break
         case ("clean"):
             sh ("yum clean all")
             break
         case ("download"):
-            sh ("yum install -y yum-plugin-downloadonly && yum install -y --downloadonly --downloaddir=./yum-download/ $product")
+            sh ("yum install -y yum-utils && yumdownloader $product --destdir=$WORKSPACE/yum-download/")
             break
         case ("install"):
             sh ("yum install -y $product")
