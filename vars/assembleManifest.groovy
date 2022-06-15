950d55c1ed3f82e98120541fa40ff506338c1059
Peter Zhu <zhujiaxi@amazon.com>
2022-06-15 01:52:02 -0400

    Add RPM Temporary Signing Through Shell Scripts (#2196)
    * Add RPM Temporary Signing Through Shell Scripts
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Fix typo
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * More typo
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * More comments
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Add jenkins unittest
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- vars/assembleManifest.groovy
+++ vars/assembleManifest.groovy
@@ -1,6 +1,7 @@
 void call(Map args = [:]) {
     lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
     def buildManifest = lib.jenkins.BuildManifest.new(readYaml(file: args.buildManifest))
+    def filename = buildManifest.build.getFilename()
     def baseUrl = buildManifest.getArtifactRootUrlWithoutDistribution("${PUBLIC_ARTIFACT_URL}", "${JOB_NAME}", "${BUILD_NUMBER}")
     sh([
         './assemble.sh',
@@ -9,6 +10,13 @@ void call(Map args = [:]) {
     ].join(' '))
 
     if (buildManifest.build.distribution == 'rpm') {
+
+        signArtifacts(
+            artifactPath: "rpm/dist/${filename}",
+            sigtype: '.rpm',
+            platform: 'linux'
+        )
+
         buildYumRepo(
             baseUrl: baseUrl,
             buildManifest: args.buildManifest
