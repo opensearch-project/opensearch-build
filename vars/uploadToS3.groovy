c7154af8d6d7525b2304d73798ac9807d22881b8
Sayali Gaikawad <gaiksaya@amazon.com>
2022-06-22 14:40:04 -0700

    format
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- vars/uploadToS3.groovy
+++ vars/uploadToS3.groovy
@@ -1,5 +1,4 @@
-void call(Map args = [:]){
-
+void call(Map args = [:]) {
     withCredentials([string(credentialsId: 'AWS_ACCOUNT_PUBLIC', variable: 'aws_account_public')]) {
         withAWS(role: 'opensearch-bundle', roleAccount: "${aws_account_public}", duration: 900, roleSessionName: 'jenkins-session') {
             s3Upload(file: args.sourcePath, bucket: args.bucket, path: args.path)
