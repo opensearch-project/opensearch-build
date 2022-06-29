b7c2402b5ed62d0b197bb32f6ca3d9b04c132957
Peter Zhu <zhujiaxi@amazon.com>
2022-06-28 21:43:18 -0400

    Add test coverage xml and support 1.x gradle check (#2279)
    * Add test coverage xml and support 1.x gradle check
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Remove comments
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- vars/runGradleCheck.groovy
+++ vars/runGradleCheck.groovy
@@ -20,14 +20,27 @@ void call(Map args = [:]) {
                 set -e
                 set +x
 
-                env | grep JAVA | grep HOME
-
                 echo "Git clone: ${git_repo_url} with ref: ${git_reference}"
                 rm -rf search
                 git clone ${git_repo_url} search
                 cd search/
                 git checkout -f ${git_reference}
                 git rev-parse HEAD
+
+                echo "Get Major Version"
+                OS_VERSION=`cat buildSrc/version.properties | grep opensearch | cut -d= -f2 | grep -oE '[0-9.]+'`
+                OS_MAJOR_VERSION=`echo \$OS_VERSION | grep -oE '[0-9]+' | head -n 1`
+                echo "Version: \$OS_VERSION, Major Version: \$OS_MAJOR_VERSION"
+
+                if [ "\$OS_MAJOR_VERSION" -lt 2 ]; then
+                    echo "Using JAVA 11"
+                    export JAVA_HOME=\$JAVA11_HOME
+                else
+                    echo "Using JAVA 17"
+                    export JAVA_HOME=\$JAVA17_HOME
+                fi
+
+                env | grep JAVA | grep HOME
 
                 echo "Stop existing gradledaemon"
                 ./gradlew --stop
@@ -42,6 +55,12 @@ void call(Map args = [:]) {
 
                 echo "Check docker-compose version"
                 docker-compose version
+
+                echo "Check existing processes"
+                ps -ef | grep [o]pensearch | wc -l
+                echo "Cleanup existing processes"
+                kill -9 `ps -ef | grep [o]pensearch | awk '{print \$2}'` > /dev/null 2>&1 || echo
+                ps -ef | grep [o]pensearch | wc -l
 
                 echo "Start gradlecheck"
                 GRADLE_CHECK_STATUS=0
