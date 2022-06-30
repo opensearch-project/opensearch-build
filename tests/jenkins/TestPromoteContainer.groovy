303a2be235374b4a8128bdc8b20af7f77eb9fa87
Zelin Hao <zelinhao@amazon.com>
2022-06-29 17:04:39 -0700

    Create a Jenkins workflow for docker promotion job
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

---
+++ tests/jenkins/TestPromoteContainer.groovy
@@ -0,0 +1,118 @@
+import jenkins.tests.BuildPipelineTest
+import org.junit.Before
+import org.junit.Test
+
+class TestPromoteContainer extends BuildPipelineTest {
+
+    String PROMOTE_PRODUCT = 'alpine'
+    String RELEASE_VERSION = '3.15.4'
+    String SOURCE_TAG = '3.15.4-1234'
+
+    @Before
+    void setUp() {
+        binding.setVariable('PROMOTE_PRODUCT', PROMOTE_PRODUCT)
+        binding.setVariable('RELEASE_VERSION', RELEASE_VERSION)
+        binding.setVariable('SOURCE_TAG', SOURCE_TAG)
+        binding.setVariable('DOCKER_USERNAME', 'dummy_docker_username')
+        binding.setVariable('DOCKER_PASSWORD', 'dummy_docker_password')
+        binding.setVariable('ARTIFACT_PROMOTION_ROLE_NAME', 'dummy-agent-AssumeRole')
+        binding.setVariable('AWS_ACCOUNT_ARTIFACT', '1234567890')
+        binding.setVariable('DATA_PREPPER_STAGING_CONTAINER_REPOSITORY', 'dummy_dataprepper_ecr_url')
+
+
+        helper.registerAllowedMethod('withAWS', [Map, Closure], null)
+        super.setUp()
+
+    }
+
+    @Test
+    public void testPromoteContainerToDocker() {
+        String dockerPromote = true
+        String ecrPromote = false
+        String latestBoolean = false
+        String majorVersionBoolean = false
+        binding.setVariable('DOCKER_HUB_PROMOTE', dockerPromote)
+        binding.setVariable('ECR_PROMOTE', ecrPromote)
+        binding.setVariable('TAG_LATEST', latestBoolean)
+        binding.setVariable('TAG_MAJOR_VERSION', majorVersionBoolean)
+
+        super.testPipeline("jenkins/promotion/promote-docker-ecr.jenkinsfile",
+                "tests/jenkins/jenkinsjob-regression-files/promotion/promote-container/promote-container-testPromoteContainerToDocker.jenkinsfile")
+    }
+
+    @Test
+    public void testPromoteContainerToDockerLatest() {
+        String dockerPromote = true
+        String ecrPromote = false
+        String latestBoolean = true
+        String majorVersionBoolean = false
+        binding.setVariable('DOCKER_HUB_PROMOTE', dockerPromote)
+        binding.setVariable('ECR_PROMOTE', ecrPromote)
+        binding.setVariable('TAG_LATEST', latestBoolean)
+        binding.setVariable('TAG_MAJOR_VERSION', majorVersionBoolean)
+
+        super.testPipeline("jenkins/promotion/promote-docker-ecr.jenkinsfile",
+                "tests/jenkins/jenkinsjob-regression-files/promotion/promote-container/promote-container-testPromoteContainerToDockerLatest.jenkinsfile")
+    }
+
+    @Test
+    public void testPromoteContainerToDockerMajor() {
+        String dockerPromote = true
+        String ecrPromote = false
+        String latestBoolean = false
+        String majorVersionBoolean = true
+        binding.setVariable('DOCKER_HUB_PROMOTE', dockerPromote)
+        binding.setVariable('ECR_PROMOTE', ecrPromote)
+        binding.setVariable('TAG_LATEST', latestBoolean)
+        binding.setVariable('TAG_MAJOR_VERSION', majorVersionBoolean)
+
+        super.testPipeline("jenkins/promotion/promote-docker-ecr.jenkinsfile",
+                "tests/jenkins/jenkinsjob-regression-files/promotion/promote-container/promote-container-testPromoteContainerToDockerMajor.jenkinsfile")
+    }
+
+    @Test
+    public void testPromoteContainerToDockerLatestMajor() {
+        String dockerPromote = true
+        String ecrPromote = false
+        String latestBoolean = true
+        String majorVersionBoolean = true
+        binding.setVariable('DOCKER_HUB_PROMOTE', dockerPromote)
+        binding.setVariable('ECR_PROMOTE', ecrPromote)
+        binding.setVariable('TAG_LATEST', latestBoolean)
+        binding.setVariable('TAG_MAJOR_VERSION', majorVersionBoolean)
+
+        super.testPipeline("jenkins/promotion/promote-docker-ecr.jenkinsfile",
+                "tests/jenkins/jenkinsjob-regression-files/promotion/promote-container/promote-container-testPromoteContainerToDockerLatestMajor.jenkinsfile")
+    }
+
+    @Test
+    public void testPromoteContainerToECRLatestMajor() {
+        String dockerPromote = false
+        String ecrPromote = true
+        String latestBoolean = true
+        String majorVersionBoolean = true
+        binding.setVariable('DOCKER_HUB_PROMOTE', dockerPromote)
+        binding.setVariable('ECR_PROMOTE', ecrPromote)
+        binding.setVariable('TAG_LATEST', latestBoolean)
+        binding.setVariable('TAG_MAJOR_VERSION', majorVersionBoolean)
+
+        super.testPipeline("jenkins/promotion/promote-docker-ecr.jenkinsfile",
+                "tests/jenkins/jenkinsjob-regression-files/promotion/promote-container/promote-container-testPromoteContainerToECRLatestMajor.jenkinsfile")
+    }
+
+    @Test
+    public void testPromoteContainerToDockerECRLatestMajor() {
+        String dockerPromote = true
+        String ecrPromote = true
+        String latestBoolean = true
+        String majorVersionBoolean = true
+        binding.setVariable('DOCKER_HUB_PROMOTE', dockerPromote)
+        binding.setVariable('ECR_PROMOTE', ecrPromote)
+        binding.setVariable('TAG_LATEST', latestBoolean)
+        binding.setVariable('TAG_MAJOR_VERSION', majorVersionBoolean)
+
+        super.testPipeline("jenkins/promotion/promote-docker-ecr.jenkinsfile",
+                "tests/jenkins/jenkinsjob-regression-files/promotion/promote-container/promote-container-testPromoteContainerToDockerECRLatestMajor.jenkinsfile")
+    }
+
+}
--- vars/promoteContainer.groovy
+++ vars/promoteContainer.groovy
@@ -1,14 +1,14 @@
 /**@
- * Copies a container from one docker registry to another
+ * Promote image from staging docker to production docker hub or ECR repository.
  *
  * @param args A map of the following parameters
  * @param args.imageRepository The repository of staging image. E.g.: opensearch, opensearch-dashboards, data-prepper
  * @param args.version The official version for release. E.g.: 2.0.1
  * @param args.sourceTag The image tag from the staging repo. E.g.: 2.0.1.3910
- * @param args.dockerPromote
- * @param args.ecrPromote
- * @param args.latestTag
- * @param args.majorVersionTag
+ * @param args.dockerPromote The boolean argument if promote containers from staging to production docker repo.
+ * @param args.ecrPromote The boolean argument if promote containers from staging to production ECR repo.
+ * @param args.latestTag The boolean argument if promote containers from staging to production with latest tag.
+ * @param args.majorVersionTag The boolean argument if promote containers from staging to production with its major version tag.
  */
 void call(Map args = [:]) {
 
@@ -21,60 +21,63 @@ void call(Map args = [:]) {
     def majorVersionBoolean = args.majorVersionTag
     def majorVersion = version.split("\\.").first()
 
+    def sourceReg = (imageRepo == 'data-prepper') ? ${DATA_PREPPER_STAGING_CONTAINER_REPOSITORY} : "opensearchstaging"
+    def dockerProduction = "opensearchproject"
+    def ecrProduction = "public.ecr.aws/opensearchproject"
+
     //Promoting docker images
-    if (dockerPromote) {
+    if (dockerPromote.toBoolean()) {
         println("Promoting to production docker hub with with $version tag.")
         copyContainer(
                 sourceImage: "$imageRepo:$sourceTag",
-                sourceRegistry: "opensearchstaging",
+                sourceRegistry: sourceReg,
                 destinationImage: "$imageRepo:$version",
-                destinationRegistry: "opensearchproject"
+                destinationRegistry: dockerProduction
         )
-        if (majorVersionBoolean) {
+        if (majorVersionBoolean.toBoolean()) {
             println("Promoting to production docker hub with with $majorVersion tag.")
             copyContainer(
                     sourceImage: "$imageRepo:$sourceTag",
-                    sourceRegistry: "opensearchstaging",
+                    sourceRegistry: sourceReg,
                     destinationImage: "$imageRepo:$majorVersion",
-                    destinationRegistry: "opensearchproject"
+                    destinationRegistry: dockerProduction
             )
         }
-        if (latestBoolean) {
+        if (latestBoolean.toBoolean()) {
             println("Promoting to production docker hub with with latest tag.")
             copyContainer(
                     sourceImage: "$imageRepo:$sourceTag",
-                    sourceRegistry: "opensearchstaging",
+                    sourceRegistry: sourceReg,
                     destinationImage: "$imageRepo:latest",
-                    destinationRegistry: "opensearchproject"
+                    destinationRegistry: dockerProduction
             )
         }
     }
-    if (ecrPromote) {
+    if (ecrPromote.toBoolean()) {
         println("Promoting to production ECR with with $version tag.")
         copyContainer(
                 sourceImage: "$imageRepo:$sourceTag",
-                sourceRegistry: "opensearchstaging",
+                sourceRegistry: sourceReg,
                 destinationImage: "$imageRepo:$version",
-                destinationRegistry: "public.ecr.aws/opensearchproject"
+                destinationRegistry: ecrProduction
         )
-        if (majorVersionBoolean) {
+        if (majorVersionBoolean.toBoolean()) {
             println("Promoting to production ECR with with $majorVersion tag.")
             copyContainer(
                     sourceImage: "$imageRepo:$sourceTag",
-                    sourceRegistry: "opensearchstaging",
+                    sourceRegistry: sourceReg,
                     destinationImage: "$imageRepo:$majorVersion",
-                    destinationRegistry: "public.ecr.aws/opensearchproject"
+                    destinationRegistry: ecrProduction
             )
         }
-        if (latestBoolean) {
+        if (latestBoolean.toBoolean()) {
             println("Promoting to production ECR with with latest tag.")
             copyContainer(
                     sourceImage: "$imageRepo:$sourceTag",
-                    sourceRegistry: "opensearchstaging",
+                    sourceRegistry: sourceReg,
                     destinationImage: "$imageRepo:latest",
-                    destinationRegistry: "public.ecr.aws/opensearchproject"
+                    destinationRegistry: ecrProduction
             )
         }
     }
-
 }
