f41439c494152cf8355e0bad92bb9678ac091b85
Sayali Gaikawad <61760125+gaiksaya@users.noreply.github.com>
2022-07-11 13:23:19 -0700

    Rename webhooks for notifications (#2322)
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- tests/jenkins/TestOpenSearchBwcTest.groovy
+++ tests/jenkins/TestOpenSearchBwcTest.groovy
@@ -29,7 +29,7 @@ class TestOpenSearchBwcTest extends BuildPipelineTest {
                 'BWC Tests Successful',
                 '',
                 testManifest,
-                'INTEG_TEST_WEBHOOK'))
+                'jenkins-integ-test-webhook'))
         super.setUp()
 
         // Variables
