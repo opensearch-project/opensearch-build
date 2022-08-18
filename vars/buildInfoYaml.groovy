25c91aba0f653c023d82a947a27371a2fefc3e08
Zelin Hao <zelinhao@amazon.com>
2022-08-17 17:11:01 -0700

    Some simplify
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- vars/buildInfoYaml.groovy
+++ vars/buildInfoYaml.groovy
@@ -19,26 +19,19 @@ def call(Map args = [:]) {
         }
     }
     echo (componentsList.toString())
+
     if (args.stage == "START") {
         echo("Initiate the buildInfo yaml file.")
         inputManifest.build.status = "IN_PROGRESS"
         inputManifest.build.number = "${BUILD_NUMBER}"
         inputManifest.results = [:]
-        inputManifest.results.integ_test_x64 = status
-        inputManifest.results.bwc_test_x64 = status
-        inputManifest.results.integ_test_arm64 = status
-        inputManifest.results.bwc_test_arm64 = status
-        inputManifest.results.x64_tar = status
-        inputManifest.results.arm64_tar = status
-        inputManifest.results.x64_rpm = status
-        inputManifest.results.arm64_rpm = status
         inputManifest.components.each { component ->
             if (componentsList.contains(component.name)) {
                 // Convert ref from branch to commit
-                dir (component.name) {
-                    checkout([$class: 'GitSCM', branches: [[name: component.ref]],
+                dir(component.name) {
+                    checkout([$class           : 'GitSCM', branches: [[name: component.ref]],
                               userRemoteConfigs: [[url: component.repository]]])
-                    def commitID = sh (
+                    def commitID = sh(
                             script: "git rev-parse HEAD",
                             returnStdout: true
                     ).trim()
@@ -46,21 +39,13 @@ def call(Map args = [:]) {
                 }
             }
         }
-    }
-    else if (args.stage == "x64_tar" || args.stage == "x64_rpm" || args.stage == "arm64_tar" || args.stage == "arm64_rpm") {
-        stageField = args.stage
-        echo("stage is $stageField")
-        echo("status is $status")
-        inputManifest.results.("$stageField".toString()) = "$status"
-    }
-    else if (args.stage == "integ_test_x64" || args.stage == "integ_test_arm64" || args.stage == "bwc_test_x64" || args.stage == "bwc_test_arm64") {
-        stageField = args.stage
-        echo("stage is $stageField")
-        echo("status is $status")
-        inputManifest.results.("$stageField".toString()) = "$status"
-    }
-    else if (args.stage == "COMPLETE") {
+    } else if (args.stage == "COMPLETE") {
         inputManifest.build.status = status
+    } else {
+        stageField = args.stage
+        echo("stage is $stageField")
+        echo("status is $status")
+        inputManifest.results.("$stageField".toString()) = "$status"
         inputManifest.results.duration = currentBuild.duration
         inputManifest.results.startTimestamp = currentBuild.startTimeInMillis
     }
