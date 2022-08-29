dd524bf69c690906fbdf7cae1148bdfb4568964e
Sayali Gaikawad <gaiksaya@amazon.com>
2022-08-29 16:22:57 -0700

    Add license to missed jenkins libraries
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- vars/buildUploadManifestSHA.groovy
+++ vars/buildUploadManifestSHA.groovy
@@ -1,3 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
 void call(Map args = [:]) {
     lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
 
