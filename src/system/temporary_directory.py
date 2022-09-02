e1c58834c36a834db8abfef06138fc30473bd9d9
Peter Zhu <zhujiaxi@amazon.com>
2022-09-02 19:03:13 -0400

    Re-adding the windows snapshot with new changes (#2550)
    * Re-adding the windows snapshot with new changes
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Update manifests
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Add more tests
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Restore Pipefile.lock
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Tweak comments
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- src/system/temporary_directory.py
+++ src/system/temporary_directory.py
@@ -19,17 +19,17 @@ def g__handleRemoveReadonly(func: FunctionType, path: str, exc: Any) -> Any:
     logging.debug(f"excvalue {excvalue}")
     logging.debug(f"func {func.__name__}")
     if func in (os.rmdir, os.remove, os.unlink):
-        os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO | stat.S_IWRITE | stat.S_IREAD)  # 0777 nix* / +wr windows
+        os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO | stat.S_IREAD | stat.S_IWRITE)  # 0777 nix* / +rw windows
         retry_total = 3
         for retry_count in range(retry_total):  # Re-run func to force deletion especially on windows
             try:
-                logging.debug(f'Try count: {retry_count + 1}/{retry_total}')
+                logging.warn(f'Removing try count: {retry_count + 1}/{retry_total} for {path}')
                 func_result = func(path)
+                logging.debug(f'func_result: {func_result}')
                 if func_result is None:
                     break
             except Exception as ex:
-                logging.warn(func_result)
-                logging.warn(ex)
+                logging.warn(f'Exception: {ex}')
     else:
         raise
 
@@ -58,5 +58,5 @@ class TemporaryDirectory:
         if self.keep:
             logging.info(f"Keeping {self.name}")
         else:
-            logging.debug(f"Removing {self.name}")
+            logging.info(f"Removing {self.name}")
             shutil.rmtree(self.name, ignore_errors=False, onerror=g__handleRemoveReadonly)
