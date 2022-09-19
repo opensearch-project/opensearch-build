37706509c0ab614019a5c8dcbed41043fcc5420a
Rishabh Singh <sngri@amazon.com>
2022-09-19 13:28:41 -0700

    Enable github issue creation on build failure
    Signed-off-by: Rishabh Singh <sngri@amazon.com>

--- vars/createGithubIssue.groovy
+++ vars/createGithubIssue.groovy
@@ -27,10 +27,7 @@ def call(Map args = [:]){
             if (failedComponents.contains(component.name)) {
                 println("Component ${component.name} failed, creating github issue")
                 compIndex = failedComponents.indexOf(component.name)
-                if (component.name == "OpenSearch"){
-                    create_issue(component.name, component.repository, currentVersion, failureMessages[compIndex])
-                }
-                // create_issue(component.name, component.repository, currentVersion, failureMessages[compIndex])
+                create_issue(component.name, component.repository, currentVersion, failureMessages[compIndex])
                 sleep(time:3,unit:"SECONDS")
             }
         }
