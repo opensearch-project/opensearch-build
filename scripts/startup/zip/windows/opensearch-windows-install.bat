aa54e0727e7e9aa6a21cb0ae2081547e4a6be36e
Peter Zhu <zhujiaxi@amazon.com>
2022-10-06 23:15:31 +0000

    Add more comments
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- scripts/startup/zip/windows/opensearch-windows-install.bat
+++ scripts/startup/zip/windows/opensearch-windows-install.bat
@@ -7,18 +7,16 @@
 PUSHD "%~dp0"
 SET "OPENSEARCH_HOME=%CD%"
 SET "OPENSEARCH_PATH_CONF=%OPENSEARCH_HOME%\config"
-SET "PARAMS=%*"
 
 :: Echo User Inputs
 ECHO "OPENSEARCH_HOME: %OPENSEARCH_HOME%"
 ECHO "OPENSEARCH_PATH_CONF: %OPENSEARCH_PATH_CONF%"
-ECHO "PARAMS: %PARAMS%"
 
 :: Start OpenSearch
 ECHO Start OpenSearch
 IF "%~1" == "" (
     CALL "%OPENSEARCH_HOME%\bin\opensearch.bat"
     ) ELSE (
-    CALL "%OPENSEARCH_HOME%\bin\opensearch.bat" "%PARAMS%"
+    CALL "%OPENSEARCH_HOME%\bin\opensearch.bat" "%*"
     )
 
