fc51853d869b915a1542f13c40e7533e9bdc9443
Martin Wilhelmi <mnin@mnin.de>
2022-10-18 11:16:10 +0200

    Fix test bundle linux distro for mypy
    Signed-off-by: Martin Wilhelmi <mnin@mnin.de>

--- tests/tests_assemble_workflow/test_bundle_linux_distro.py
+++ tests/tests_assemble_workflow/test_bundle_linux_distro.py
@@ -5,6 +5,7 @@
 # this file be licensed under the Apache-2.0 license or a
 # compatible open source license.
 
+import os
 import unittest
 from unittest.mock import MagicMock, Mock, call, patch
 
@@ -44,7 +45,8 @@ class TestBundleLinuxDistro(unittest.TestCase):
     @patch("assemble_workflow.bundle_linux_distro.BundleLinuxDeb", return_value=MagicMock())
     def test_build_with_deb_extension(self, bundle_linux_deb_mock: Mock) -> None:
 
-        self.bundle_linux_distro.build("deb", "name", "dest", "archive_path", BuildManifest)
+        manifest_deb = BuildManifest.from_path(os.path.join(os.path.dirname(__file__), "data/opensearch-build-deb-1.3.0.yml"))
+        self.bundle_linux_distro.build("deb", "name", "dest", "archive_path", manifest_deb.build)
 
         bundle_linux_deb_mock.assert_has_calls(
             [
@@ -54,7 +56,7 @@ class TestBundleLinuxDistro(unittest.TestCase):
 
         bundle_linux_deb_mock.return_value.build.assert_has_calls(
             [
-                call("name", "dest", "archive_path", BuildManifest)
+                call("name", "dest", "archive_path", manifest_deb.build)
             ]
         )
 
@@ -78,7 +80,8 @@ class TestBundleLinuxDistro(unittest.TestCase):
     @patch("assemble_workflow.bundle_linux_distro.BundleLinuxRpm", return_value=MagicMock())
     def test_build_with_rpm_extension(self, bundle_linux_rpm_mock: Mock) -> None:
 
-        self.bundle_linux_distro.build("rpm", "name", "dest", "archive_path", BuildManifest)
+        manifest_rpm = BuildManifest.from_path(os.path.join(os.path.dirname(__file__), "data/opensearch-build-rpm-1.3.0.yml"))
+        self.bundle_linux_distro.build("rpm", "name", "dest", "archive_path", manifest_rpm.build)
 
         bundle_linux_rpm_mock.assert_has_calls(
             [
@@ -88,6 +91,6 @@ class TestBundleLinuxDistro(unittest.TestCase):
 
         bundle_linux_rpm_mock.return_value.build.assert_has_calls(
             [
-                call("name", "dest", "archive_path", BuildManifest)
+                call("name", "dest", "archive_path", manifest_rpm.build)
             ]
         )
