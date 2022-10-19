956628ed6c574499a42fada0fab176a841f50386
Peter Zhu <zhujiaxi@amazon.com>
2022-10-19 19:38:49 +0000

    Restructure zip tests
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- tests/tests_test_workflow/test_integ_workflow/integ_test/test_distribution_zip.py
+++ tests/tests_test_workflow/test_integ_workflow/integ_test/test_distribution_zip.py
@@ -12,38 +12,80 @@ from unittest.mock import MagicMock, Mock, patch
 from test_workflow.integ_test.distribution_zip import DistributionZip
 
 
-class TestDistributionZip(unittest.TestCase):
+class TestDistributionZipOpenSearch(unittest.TestCase):
 
     def setUp(self) -> None:
 
         self.work_dir = os.path.join(os.path.dirname(__file__), "data")
-        self.distribution_zip = DistributionZip("opensearch", "2.4.0", self.work_dir)
-        self.distribution_zip_dashboards = DistributionZip("opensearch-dashboards", "2.4.0", self.work_dir)
+        self.product = "opensearch"
+        self.version = "2.4.0"
+        self.distribution_zip = DistributionZip(self.product, self.version, self.work_dir)
 
     def test_distribution_zip_vars(self) -> None:
-        self.assertEqual(self.distribution_zip.filename, 'opensearch')
-        self.assertEqual(self.distribution_zip.version, '2.4.0')
+        self.assertEqual(self.distribution_zip.filename, self.product)
+        self.assertEqual(self.distribution_zip.version, self.version)
         self.assertEqual(self.distribution_zip.work_dir, self.work_dir)
 
     def test_install_dir(self) -> None:
-        self.assertEqual(self.distribution_zip.install_dir, os.path.join(self.work_dir, "opensearch-2.4.0"))
+        self.assertEqual(self.distribution_zip.install_dir, os.path.join(self.work_dir, f"{self.product}-{self.version}"))
 
     def test_config_dir(self) -> None:
-        self.assertEqual(self.distribution_zip.config_dir, os.path.join(self.work_dir, "opensearch-2.4.0", "config"))
+        self.assertEqual(self.distribution_zip.config_dir, os.path.join(self.work_dir, f"{self.product}-{self.version}", "config"))
 
     def test_install(self) -> None:
         with patch("test_workflow.integ_test.distribution_zip.ZipFile") as mock_zipfile_open:
             mock_zipfile_extractall = MagicMock()
             mock_zipfile_open.return_value.__enter__.return_value.extractall = mock_zipfile_extractall
 
-            self.distribution_zip.install(os.path.join(self.work_dir, "artifacts", "dist", "opensearch-min-2.4.0-windows-x64.zip"))
+            self.distribution_zip.install(os.path.join(self.work_dir, "artifacts", "dist", f"{self.product}-min-{self.version}-windows-x64.zip"))
 
-            mock_zipfile_open.assert_called_with(os.path.join(self.work_dir, "artifacts", "dist", "opensearch-min-2.4.0-windows-x64.zip"), "r")
+            mock_zipfile_open.assert_called_with(os.path.join(self.work_dir, "artifacts", "dist", f"{self.product}-min-{self.version}-windows-x64.zip"), "r")
             mock_zipfile_extractall.assert_called_with(self.work_dir)
 
     def test_start_cmd(self) -> None:
         self.assertEqual(self.distribution_zip.start_cmd, "./opensearch-windows-install.bat")
-        self.assertEqual(self.distribution_zip_dashboards.start_cmd, "./opensearch-dashboards.bat")
+
+    @patch("subprocess.check_call")
+    def test_uninstall(self, check_call_mock: Mock) -> None:
+        self.distribution_zip.uninstall()
+        args_list = check_call_mock.call_args_list
+
+        self.assertEqual(check_call_mock.call_count, 1)
+        self.assertEqual(f"rm -rf {self.work_dir}/*", args_list[0][0][0])
+
+
+class TestDistributionZipOpenSearchDashboards(unittest.TestCase):
+
+    def setUp(self) -> None:
+
+        self.work_dir = os.path.join(os.path.dirname(__file__), "data")
+        self.product = "opensearch-dashboards"
+        self.version = "2.4.0"
+        self.distribution_zip = DistributionZip(self.product, self.version, self.work_dir)
+
+    def test_distribution_zip_vars(self) -> None:
+        self.assertEqual(self.distribution_zip.filename, self.product)
+        self.assertEqual(self.distribution_zip.version, self.version)
+        self.assertEqual(self.distribution_zip.work_dir, self.work_dir)
+
+    def test_install_dir(self) -> None:
+        self.assertEqual(self.distribution_zip.install_dir, os.path.join(self.work_dir, f"{self.product}-{self.version}"))
+
+    def test_config_dir(self) -> None:
+        self.assertEqual(self.distribution_zip.config_dir, os.path.join(self.work_dir, f"{self.product}-{self.version}", "config"))
+
+    def test_install(self) -> None:
+        with patch("test_workflow.integ_test.distribution_zip.ZipFile") as mock_zipfile_open:
+            mock_zipfile_extractall = MagicMock()
+            mock_zipfile_open.return_value.__enter__.return_value.extractall = mock_zipfile_extractall
+
+            self.distribution_zip.install(os.path.join(self.work_dir, "artifacts", "dist", f"{self.product}-min-{self.version}-windows-x64.zip"))
+
+            mock_zipfile_open.assert_called_with(os.path.join(self.work_dir, "artifacts", "dist", f"{self.product}-min-{self.version}-windows-x64.zip"), "r")
+            mock_zipfile_extractall.assert_called_with(self.work_dir)
+
+    def test_start_cmd(self) -> None:
+        self.assertEqual(self.distribution_zip.start_cmd, "./opensearch-dashboards.bat")
 
     @patch("subprocess.check_call")
     def test_uninstall(self, check_call_mock: Mock) -> None:
