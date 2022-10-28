bafcf0d1ada562b0a841b59d60fb0ae76e8cfa35
Peter Zhu <zhujiaxi@amazon.com>
2022-10-28 14:43:38 -0400

    Remove quotes from parameters of windows script (#2818)
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- scripts/startup/zip/windows/opensearch-windows-install.bat
+++ scripts/startup/zip/windows/opensearch-windows-install.bat
@@ -14,7 +14,7 @@ ECHO "OPENSEARCH_PATH_CONF: %OPENSEARCH_PATH_CONF%"
 
 :: Security Plugin Setups
 ECHO "Running Security Plugin Install Demo Configuration"
-CALL "%OPENSEARCH_HOME%/plugins/opensearch-security/tools/install_demo_configuration.bat" "-y" "-i" "-s"
+CALL "%OPENSEARCH_HOME%/plugins/opensearch-security/tools/install_demo_configuration.bat" -y -i -s
 
 :: k-NN Plugin Setups
 ECHO "Set KNN Dylib Path for Windows systems"
