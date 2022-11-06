a29148db89d7dbcd4a3fc7fbdf8ac4d52330632e
Peter Zhu <zhujiaxi@amazon.com>
2022-11-06 18:12:22 -0500

    Use try catch to resolve issues with component without refs
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- src/ci_workflow/ci_check_list.py
+++ src/ci_workflow/ci_check_list.py
@@ -5,6 +5,8 @@
 # this file be licensed under the Apache-2.0 license or a
 # compatible open source license.
 
+import logging
+
 from abc import ABC, abstractmethod
 from typing import Any
 
@@ -14,7 +16,11 @@ from ci_workflow.ci_target import CiTarget
 class CiCheckList(ABC):
     def __init__(self, component: Any, target: CiTarget) -> None:
         self.component = component
-        self.component_ref_is_commit = True if len(self.component.ref) == 40 and int(self.component.ref, 16) else False
+        try:
+            self.component_ref_is_commit = True if len(self.component.ref) == 40 and int(self.component.ref, 16) else False
+            logging.debug("ref exists in component")
+        except AttributeError:
+            logging.debug("ref does not exist in component")
         self.target = target
 
     @abstractmethod
