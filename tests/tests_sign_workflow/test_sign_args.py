3176affa8da2149392a53a93238e29b3ead3f580
Sayali Gaikawad <61760125+gaiksaya@users.noreply.github.com>
2022-11-08 16:43:45 -0800

    Fix signing the artifacts for .asc signatures (#2850)
    * Add asc support
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- tests/tests_sign_workflow/test_sign_args.py
+++ tests/tests_sign_workflow/test_sign_args.py
@@ -34,7 +34,7 @@ class TestSignArgs(unittest.TestCase):
 
     @patch("argparse._sys.argv", [SIGN_PY, OPENSEARCH_MANIFEST])
     def test_sigtype_default(self) -> None:
-        self.assertEqual(SignArgs().sigtype, ".asc")
+        self.assertEqual(SignArgs().sigtype, ".sig")
 
     @patch("argparse._sys.argv", [SIGN_PY, OPENSEARCH_MANIFEST, "--verbose"])
     def test_verbose_true(self) -> None:
