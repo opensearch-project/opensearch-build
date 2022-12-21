5d3936c2a4ee5a8983b3d919521f2c00febf64f3
Sayali Gaikawad <61760125+gaiksaya@users.noreply.github.com>
2022-12-21 11:34:29 -0800

    Remove logging status code for test results (#3021)
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>
    
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- src/test_workflow/test_result/test_result.py
+++ src/test_workflow/test_result/test_result.py
@@ -25,7 +25,7 @@ class TestResult:
         return "PASS" if self.status == 0 else "FAIL"
 
     def __str__(self) -> str:
-        return "| {:20s} | {:20s} | {:5s} | {:4d} |".format(self.component, self.config, self.__test_result, self.status)
+        return "| {:20s} | {:20s} | {:5s} |".format(self.component, self.config, self.__test_result)
 
     def __logger(self) -> Callable:
         return logging.info if self.status == 0 else logging.error
