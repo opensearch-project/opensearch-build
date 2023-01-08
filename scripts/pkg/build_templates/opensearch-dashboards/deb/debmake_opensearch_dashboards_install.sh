5472b088195e92c930122d17f83aa96e57e93b9b
Peter Zhu <zhujiaxi@amazon.com>
2023-01-08 16:04:08 -0500

    OSD maintainers scripts
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- scripts/pkg/build_templates/opensearch-dashboards/deb/debmake_opensearch_dashboards_install.sh
+++ scripts/pkg/build_templates/opensearch-dashboards/deb/debmake_opensearch_dashboards_install.sh
@@ -7,7 +7,7 @@
 # this file be licensed under the Apache-2.0 license or a
 # compatible open source license.
 
-# debmake opensearch install script
+# debmake opensearch-dashboards install script
 
 set -ex
 
@@ -17,33 +17,30 @@ if [ -z "$1" ]; then
 fi
 
 curdir=$1
-product_dir=/usr/share/opensearch
-config_dir=/etc/opensearch
-data_dir=/var/lib/opensearch
-log_dir=/var/log/opensearch
-pid_dir=/var/run/opensearch
-buildroot=${curdir}/debian/opensearch
+product_dir=/usr/share/opensearch-dashboards
+config_dir=/etc/opensearch-dashboards
+data_dir=/var/lib/opensearch-dashboards
+log_dir=/var/log/opensearch-dashboards
+pid_dir=/var/run/opensearch-dashboards
+buildroot=${curdir}/debian/opensearch-dashboards
 
 # Create necessary directories
 mkdir -p ${buildroot}
 mkdir -p ${buildroot}${pid_dir}
+mkdir -p ${buildroot}${product_dir}/assets
 mkdir -p ${buildroot}${product_dir}/plugins
+mkdir -p ${buildroot}${log_dir}
 
 # Install directories/files
+# Service files stored in /usr/lib/systemd for pkg installation, /etc/systemd is meant for manual changes by sysadmin
+rm -rvf ${curdir}/etc/systemd
 cp -va ${curdir}/etc ${curdir}/usr ${curdir}/var ${buildroot}/
 chmod -c 0755 ${buildroot}${product_dir}/bin/*
-if [ -d ${buildroot}${product_dir}/plugins/opensearch-security ]; then
-    chmod -c 0755 ${buildroot}${product_dir}/plugins/opensearch-security/tools/*
-fi
 
 # Symlinks (do not symlink config dir as security demo installer has dependency, if no presense it will switch to rpm/deb mode)
 ln -s ${data_dir} ${buildroot}${product_dir}/data
 ln -s ${log_dir}  ${buildroot}${product_dir}/logs
 
-# Performance Analyzer Settings
-echo 'true' > ${buildroot}${data_dir}/rca_enabled.conf
-echo 'true' > ${buildroot}${config_dir}/performance_analyzer_enabled.conf
-echo 'true' > ${buildroot}${config_dir}/rca_enabled.conf
 # Change Permissions
 chmod -Rf a+rX,u+w,g-w,o-w ${buildroot}/*
 
