a35ea924c5691c0e2ce0d12de5a6884a0baacecf
Peter Zhu <zhujiaxi@amazon.com>
2023-03-13 15:28:44 -0400

    Temporarily resolve windows issues to process && in shell
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- scripts/components/ganttChartDashboards/build.sh
+++ scripts/components/ganttChartDashboards/build.sh
@@ -80,9 +80,9 @@ PLUGIN_NAME=$(basename $(dirname "$PWD"))
 # This makes it so there is a dependency on having Dashboards pulled already.
 cp -r ../$PLUGIN_FOLDER/ ../../OpenSearch-Dashboards/plugins
 echo "BUILD MODULES FOR $PLUGIN_NAME"
-(cd ../../OpenSearch-Dashboards && eval $NVM_CMD && yarn osd bootstrap)
+(cd ../../OpenSearch-Dashboards; eval $NVM_CMD; yarn osd bootstrap)
 echo "BUILD RELEASE ZIP FOR $PLUGIN_NAME"
-(cd ../../OpenSearch-Dashboards && eval $NVM_CMD && cd plugins/$PLUGIN_FOLDER && yarn plugin-helpers build --opensearch-dashboards-version=$VERSION$QUALIFIER_IDENTIFIER)
+(cd ../../OpenSearch-Dashboards; eval $NVM_CMD; cd plugins/$PLUGIN_FOLDER; yarn plugin-helpers build --opensearch-dashboards-version=$VERSION$QUALIFIER_IDENTIFIER)
 echo "COPY $PLUGIN_NAME.zip"
 cp -r ../../OpenSearch-Dashboards/plugins/$PLUGIN_FOLDER/build/$PLUGIN_NAME-$VERSION$QUALIFIER_IDENTIFIER.zip $OUTPUT/plugins/
 rm -rf ../../OpenSearch-Dashboards/plugins/$PLUGIN_FOLDER
