983f981b9adbd880697ec3f66fa80072f78e9eb7
Sayali Gaikawad <gaiksaya@amazon.com>
2023-04-13 12:03:25 -0700

    Enable integration test failure notification via GitHub issues for OpenSearch plugins
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- tests/jenkins/TestOpenSearchIntegTest.groovy
+++ tests/jenkins/TestOpenSearchIntegTest.groovy
@@ -10,6 +10,10 @@ import org.junit.Test
 import org.yaml.snakeyaml.Yaml
 import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
 import static com.lesfurets.jenkins.unit.global.lib.GitSource.gitSource
+import static com.lesfurets.jenkins.unit.MethodCall.callArgsToString
+import static org.hamcrest.CoreMatchers.hasItem
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.junit.jupiter.api.Assertions.assertThrows
 
 class TestOpenSearchIntegTest extends BuildPipelineTest {
 
@@ -19,7 +23,7 @@ class TestOpenSearchIntegTest extends BuildPipelineTest {
 
         helper.registerSharedLibrary(
             library().name('jenkins')
-                .defaultVersion('2.1.0')
+                .defaultVersion('4.0.0')
                 .allowOverride(true)
                 .implicit(true)
                 .targetPath('vars')
@@ -35,69 +39,101 @@ class TestOpenSearchIntegTest extends BuildPipelineTest {
         def buildManifest = "tests/jenkins/data/opensearch-1.3.0-build.yml"
         def buildManifestUrl = "https://ci.opensearch.org/ci/dbc/distribution-build-opensearch/1.3.0/${buildId}/linux/x64/dist/opensearch/opensearch-1.3.0-linux-x64.tar.gz"
         def agentLabel = "Jenkins-Agent-AL2-X64-C54xlarge-Docker-Host"
-        def destPath = "${this.workspace}/artifacts"
-        def artifactsPath = "${jobName}/1.3.0/${buildId}/linux/x64/tar/"
         def bucketName = 'job-s3-bucket-name'
 
         binding.setVariable('ARTIFACT_DOWNLOAD_ROLE_NAME', 'Dummy_Download_Role')
         binding.setVariable('AWS_ACCOUNT_PUBLIC', 'dummy_account')
-        binding.setVariable('env', ['BUILD_NUMBER': "${buildId}"])
+        binding.setVariable('env', ['BUILD_NUMBER': '234', 'DOCKER_AGENT':[image:'opensearchstaging/ci-runner:ci-runner-centos7-v1', args:'-e JAVA_HOME=/opt/java/openjdk-11'] ])
         binding.setVariable('ARTIFACT_BUCKET_NAME', 'DUMMY_BUCKET_NAME')
         binding.setVariable('AWS_ACCOUNT_PUBLIC', 'DUMMY_AWS_ACCOUNT_PUBLIC')
         binding.setVariable('ARTIFACT_BUCKET_NAME', 'DUMMY_ARTIFACT_BUCKET_NAME')
         binding.setVariable('PUBLIC_ARTIFACT_URL', 'DUMMY_PUBLIC_ARTIFACT_URL')
-        binding.setVariable('env', ['BUILD_NUMBER': "${buildId}"])
         binding.setVariable('STAGE_NAME', 'DUMMY_STAGE_NAME')
         binding.setVariable('JOB_NAME', 'dummy_job')
-        binding.setVariable('BUILD_NUMBER', "${buildId}")
         binding.setVariable('BUILD_URL', 'htth://BUILD_URL_dummy.com')
         binding.setVariable('WEBHOOK_URL', 'htth://WEBHOOK_URL_dummy.com')
         binding.setVariable('TEST_MANIFEST', testManifest)
         binding.setVariable('BUILD_MANIFEST_URL', buildManifestUrl)
         binding.setVariable('AGENT_LABEL', agentLabel)
+        binding.setVariable('BUILD_NUMBER', '234')
         binding.setVariable('BUILD_MANIFEST', buildManifest)
-        binding.setVariable('BUILD_ID', "${buildId}")
         binding.setVariable('ARTIFACT_BUCKET_NAME', bucketName)
-        def env = binding.getVariable('env')
-        env['DOCKER_AGENT'] = [image:'opensearchstaging/ci-runner:ci-runner-centos7-v1', args:'-e JAVA_HOME=/opt/java/openjdk-11']
+        binding.setVariable('RUN_DISPLAY_URL', 'https://some/url/redirect')
         binding.getVariable('currentBuild').upstreamBuilds = [[fullProjectName: jobName]]
-        
         helper.registerAllowedMethod("s3Download", [Map])
         helper.registerAllowedMethod("withAWS", [Map, Closure], { args, closure ->
             closure.delegate = delegate
             return helper.callClosure(closure)
         })
         helper.registerAllowedMethod("withCredentials", [Map])
-        helper.registerAllowedMethod('parameterizedCron', [String], null)
         helper.registerAllowedMethod('readYaml', [Map.class], { args ->
             return new Yaml().load((this.testManifest ?: binding.getVariable('TEST_MANIFEST') as File).text)
         })
+        helper.registerAllowedMethod('parameterizedCron', [String], null)
         helper.registerAllowedMethod('readYaml', [Map.class], { args ->
             return new Yaml().load((buildManifest as File).text)
         })
-        helper.registerAllowedMethod("withCredentials", [Map, Closure], { args, closure ->
-            closure.delegate = delegate
-            return helper.callClosure(closure)
-        })
-        helper.registerAllowedMethod("withAWS", [Map, Closure], { args, closure ->
-            closure.delegate = delegate
-            return helper.callClosure(closure)
-        })
+        helper.addFileExistsMock("manifests/${testManifest}", true)
         helper.registerAllowedMethod("s3Upload", [Map])
-        helper.registerAllowedMethod('fileExists', [String.class], { args ->
-            return true;
-        })
-
         helper.registerAllowedMethod('findFiles', [Map.class], null)
         helper.registerAllowedMethod('unstash', [String.class], null)
-        helper.registerAllowedMethod('fileExists', [String.class], { args ->
-            return true;
-        })
     }
 
     @Test
     void integTests_runs_consistently() {
         super.testPipeline('jenkins/opensearch/integ-test.jenkinsfile',
                 'tests/jenkins/jenkinsjob-regression-files/opensearch/integ-test.jenkinsfile')
+        assertThat(getCommandExecutions('sh', 'test.sh'), hasItem(' env JAVA_HOME=/opt/java/openjdk-17 ./test.sh integ-test manifests/tests/jenkins/data/opensearch-1.3.0-test.yml --component OpenSearch --test-run-id 234 --paths opensearch=/tmp/workspace/tar '))
+    }
+
+    @Test
+    void checkUploadResults() {
+        runScript('jenkins/opensearch/integ-test.jenkinsfile')
+        assertThat(getCommandExecutions('s3Upload', ''), hasItem('{file=test-results, bucket=ARTIFACT_BUCKET_NAME, path=dummy_job/1.3.0/717/linux/x64/tar/test-results}'))
+    }
+
+    @Test
+    void checkError() {
+        helper.addFileExistsMock('manifests/tests/jenkins/data/opensearch-1.3.0-test.yml', false)
+        runScript('jenkins/opensearch/integ-test.jenkinsfile')
+        assertThat(getCommandExecutions('error', ''), hasItem('Integration Tests failed to start. Test manifest was not provided or not found in manifests/tests/jenkins/data/opensearch-1.3.0-test.yml.'))
+        assertJobStatusFailure()
+    }
+
+    @Test
+    void checkGHissueCreation() {
+        helper.addShMock(' env JAVA_HOME=/opt/java/openjdk-17 ./test.sh integ-test manifests/tests/jenkins/data/opensearch-1.3.0-test.yml --component OpenSearch --test-run-id 234 --paths opensearch=/tmp/workspace/tar ', '', 1)
+        helper.addShMock('gh issue list --repo https://github.com/opensearch-project/OpenSearch.git -S "Integration Test failed for component OpenSearch: 1.3.0 in:title" --label autocut,v1.3.0,test-failure', '', 0)
+        assertThrows(Exception) {
+            runScript('jenkins/opensearch/integ-test.jenkinsfile')
+            }
+        assertJobStatusFailure()
+        assertThat(getCommandExecutions('sh', 'create'), hasItem('{script=gh issue create --title \"Integration Test failed for component OpenSearch: 1.3.0\" --body \"The integration test failed at distribution level for component OpenSearch for version: 1.3.0<br><br>Please check the logs: https://some/url/redirect<br><br> * Steps to reproduce: See https://github.com/opensearch-project/opensearch-build/tree/main/src/test_workflow#integration-tests<br>* Access cluster logs:<br> - [With security](https://ci.opensearch.org/ci/dbc/dummy_job/1.3.0/717/linux/x64/tar/test-results/234/integ-test/OpenSearch/with-security/local-cluster-logs/stdout.txt) (if applicable)<br> - [Without security](https://ci.opensearch.org/ci/dbc/dummy_job/1.3.0/717/linux/x64/tar/test-results/234/integ-test/OpenSearch/without-security/local-cluster-logs/stdout.txt) (if applicable)<br><br> _Note: All in one test report manifest with all the details coming soon. See https://github.com/opensearch-project/opensearch-build/issues/1274_\" --label autocut,v1.3.0,test-failure --label \"untriaged\" --repo https://github.com/opensearch-project/OpenSearch.git, returnStdout=true}'))
+    }
+
+    @Test
+    void checkGHexistingIssue() {
+        helper.addShMock(' env JAVA_HOME=/opt/java/openjdk-17 ./test.sh integ-test manifests/tests/jenkins/data/opensearch-1.3.0-test.yml --component OpenSearch --test-run-id 234 --paths opensearch=/tmp/workspace/tar ', '', 1)
+        assertThrows(Exception) {
+            runScript('jenkins/opensearch/integ-test.jenkinsfile')
+            }
+        assertJobStatusFailure()
+        assertThat(getCommandExecutions('println', 'Issue'), hasItem('Issue already exists in the repository, skipping.'))
+    }
+
+    def getCommandExecutions(methodName, command) {
+        def shCommands = helper.callStack.findAll {
+            call ->
+                call.methodName == methodName
+        }.
+        collect {
+            call ->
+                callArgsToString(call)
+        }.findAll {
+            shCommand ->
+                shCommand.contains(command)
+        }
+
+        return shCommands
     }
 }
