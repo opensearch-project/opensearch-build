8be12ac0608157b80dcdaa1e8a0e5f01ad18f92c
Divya Madala <divyaasm@amazon.com>
2023-05-25 11:17:44 -0700

    Add changes
    Signed-off-by: Divya Madala <divyaasm@amazon.com>

--- tests/jenkins/TestOpenSearchDashboardsIntegTest.groovy
+++ tests/jenkins/TestOpenSearchDashboardsIntegTest.groovy
@@ -142,8 +142,6 @@ class TestOpenSearchDashboardsIntegTest extends BuildPipelineTest {
         assertJobStatusFailure()
     }
 
-
-
     def getCommandExecutions(methodName, command) {
         def shCommands = helper.callStack.findAll {
             call ->
