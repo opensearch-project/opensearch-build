0470db311a1b03f2e01f07a507dcdb34c0ac1242
Zelin Hao <zelinhao@amazon.com>
2023-07-24 15:12:13 -0700

    Fix integ tests for windows platform
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- tests/tests_test_workflow/test_recorder/test_test_recorder.py
+++ tests/tests_test_workflow/test_recorder/test_test_recorder.py
@@ -80,7 +80,7 @@ class TestTestRecorder(unittest.TestCase):
 
         component_yml = test_recorder._generate_yml(mock_test_result_data, "working-directory")
 
-        mock_open.assert_has_calls([call(os.path.join("working-directory", "sql.yml"), 'w')])
+        mock_open.assert_has_calls([call(os.path.join("working-directory", "sql.yml"), 'w', encoding='utf-8')])
         mock_file_path.assert_called_once_with(None, "sql", "with-security")
         mock_list_files.assert_called()
         mock_update_path.assert_called()
