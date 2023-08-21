2b2bf8adb19faa619dd01ec1213ae2a0d01bc866
Rishabh Singh <sngri@amazon.com>
2023-08-21 14:22:23 -0700

    Add support to accept telemetry parameters
    Signed-off-by: Rishabh Singh <sngri@amazon.com>

--- tests/tests_test_workflow/test_benchmark_workflow/benchmark_test/test_benchmark_test_suite.py
+++ tests/tests_test_workflow/test_benchmark_workflow/benchmark_test/test_benchmark_test_suite.py
@@ -14,13 +14,14 @@ from test_workflow.benchmark_test.benchmark_test_suite import BenchmarkTestSuite
 
 class TestBenchmarkTestSuite(unittest.TestCase):
     def setUp(self, config: Optional[str] = None, tag: Optional[str] = None,
-              workload_params: Optional[str] = None, telemetry: Optional[list] = None) -> None:
+              workload_params: Optional[str] = None, telemetry: Optional[list] = None, telemetry_params: Optional[str] = None) -> None:
         self.args = Mock()
         self.args.workload = "nyc_taxis"
         self.args.benchmark_config = config
         self.args.user_tag = tag
         self.args.workload_params = workload_params
         self.args.telemetry = telemetry
+        self.args.telemetry_params = telemetry_params
         self.endpoint = "abc.com"
         self.benchmark_test_suite = BenchmarkTestSuite(endpoint=self.endpoint, security=False, args=self.args)
 
@@ -44,6 +45,20 @@ class TestBenchmarkTestSuite(unittest.TestCase):
                              'verify_certs:false,basic_auth_user:\'admin\',basic_auth_password:\'admin\'"')
 
     def test_execute_default_with_optional_args(self) -> None:
+        TestBenchmarkTestSuite.setUp(self, "/home/test/benchmark.ini", "key1:value1,key2:value2", "{\"number_of_replicas\":\"1\"}", ['node-stats', 'test'], "{\"example_key\":\"example_value\"}")
+        with patch("subprocess.check_call") as mock_check_call:
+            self.benchmark_test_suite.execute()
+            self.assertEqual(mock_check_call.call_count, 1)
+            self.assertEqual(self.benchmark_test_suite.command, 'docker run --rm -v /home/test/benchmark.ini:'
+                                                                '/opensearch-benchmark/.benchmark/benchmark.ini '
+                                                                'opensearchproject/opensearch-benchmark:latest execute-test '
+                                                                '--workload=nyc_taxis '
+                                                                '--pipeline=benchmark-only --target-hosts=abc.com '
+                                                                '--workload-params \'{"number_of_replicas":"1"}\' '
+                                                                '--user-tag="key1:value1,key2:value2" --telemetry node-stats,test, --telemetry-params \'{"example_key":"example_value"}\' '
+                                                                '--client-options="timeout:300"')
+
+    def test_execute_default_with_no_telemetry_params(self) -> None:
         TestBenchmarkTestSuite.setUp(self, "/home/test/benchmark.ini", "key1:value1,key2:value2", "{\"number_of_replicas\":\"1\"}", ['node-stats', 'test'])
         with patch("subprocess.check_call") as mock_check_call:
             self.benchmark_test_suite.execute()
@@ -54,4 +69,5 @@ class TestBenchmarkTestSuite(unittest.TestCase):
                                                                 '--workload=nyc_taxis '
                                                                 '--pipeline=benchmark-only --target-hosts=abc.com '
                                                                 '--workload-params \'{"number_of_replicas":"1"}\' '
-                                                                '--user-tag="key1:value1,key2:value2" --telemetry node-stats,test, --client-options="timeout:300"')
+                                                                '--user-tag="key1:value1,key2:value2" --telemetry node-stats,test, '
+                                                                '--client-options="timeout:300"')
