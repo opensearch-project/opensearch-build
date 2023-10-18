93e6a25d41c79353dd64a6ecefe4ebda6f5762b9
Peter Zhu <zhujiaxi@amazon.com>
2023-10-18 18:56:35 -0400

    Onboard 5.10.1 lib to fix the scoop issue on Windows Java verion switch (#4147)
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- tests/jenkins/TestOpenSearchDashboardsIntegTest.groovy
+++ tests/jenkins/TestOpenSearchDashboardsIntegTest.groovy
@@ -24,7 +24,7 @@ class TestOpenSearchDashboardsIntegTest extends BuildPipelineTest {
 
         helper.registerSharedLibrary(
             library().name('jenkins')
-                .defaultVersion('5.9.1')
+                .defaultVersion('5.10.1')
                 .allowOverride(true)
                 .implicit(true)
                 .targetPath('vars')
