128b59bd32cf4a44a516df793c55958c16a31db1
Rupa Lahiri <rupacoach@gmail.com>
2023-10-24 22:28:34 +0530

    Throw error for duplicate component names in manifests YML (#4090)
    Signed-off-by: Rupa Lahiri <rupacoach@gmail.com>
    Co-authored-by: Sayali Gaikawad <61760125+gaiksaya@users.noreply.github.com>

--- src/ci_workflow/ci_manifests.py
+++ src/ci_workflow/ci_manifests.py
@@ -7,8 +7,11 @@
 
 
 import re
+from collections import Counter
 from io import TextIOWrapper
 from typing import Type, Union
+
+import yaml
 
 from ci_workflow.ci_args import CiArgs
 from ci_workflow.ci_input_manifest import CiInputManifest
@@ -23,6 +26,29 @@ class CiManifests:
         else:
             return CiInputManifest
 
+    @staticmethod
+    def __get_duplicate_component_names(count_component_names: Counter) -> list:
+        duplicate_component_names = []
+        for component_name, count in count_component_names.items():
+            if count > 1:
+                duplicate_component_names.append(component_name)
+        return duplicate_component_names
+
+    @staticmethod
+    def __check_duplicate_component_names(file: TextIOWrapper) -> None:
+        yaml_dict = yaml.safe_load(file)
+        component_names = []
+        for component in yaml_dict['components']:
+            component_names.append(component['name'])
+        count_component_names = Counter(component_names)
+
+        if set(count_component_names.values()) != set([1]):
+            duplicate_component_names = CiManifests.__get_duplicate_component_names(count_component_names)
+            duplicate_component_names_string = ', '.join(duplicate_component_names)
+            raise ValueError(f"Found {duplicate_component_names_string} as a duplicate component(s) in manifest {file.name}. ")
+        file.seek(0)
+
     @classmethod
     def from_file(cls, file: TextIOWrapper, args: CiArgs) -> Union[CiTestManifest, CiInputManifest]:
+        cls.__check_duplicate_component_names(file)
         return cls.__klass(file.name)(file, args)
