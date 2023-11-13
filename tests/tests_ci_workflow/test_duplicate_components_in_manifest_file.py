2be8085b960c6cfac8be72f3204bc07eec7f442d
Rupa Lahiri <rupacoach@gmail.com>
2023-11-14 02:28:34 +0530

    Use single line to extract duplicate component names (#4194)
    Signed-off-by: Rupa Lahiri <rupacoach@gmail.com>

--- tests/tests_ci_workflow/test_duplicate_components_in_manifest_file.py
+++ tests/tests_ci_workflow/test_duplicate_components_in_manifest_file.py
@@ -14,7 +14,12 @@ from ci_workflow.ci_manifests import CiManifests
 class TestDuplicateComponentsInManifestFile(unittest.TestCase):
     def test_duplicate_components_in_manifest_file(self) -> None:
         data_path = os.path.join(os.path.dirname(__file__), "data")
-        manifest_filename = os.path.join(data_path, 'opensearch-1.3.11.yml')
-        with self.assertRaises(ValueError):
+        manifest_filename = os.path.join(data_path, 'opensearch-1.3.11-with-duplicates.yml')
+        with self.assertRaises(ValueError) as valueErrorException:
             with open(manifest_filename, 'r') as manifest_file:
                 CiManifests.from_file(manifest_file, None)
+        self.assertTrue(
+            "Found sql, k-NN, performance-analyzer, opensearch-observability, anomaly-detection, "
+            "index-management as a duplicate component(s) in manifest "
+            + manifest_filename
+            in str(valueErrorException.exception))
