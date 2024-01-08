00fa11f15bb1ceb8c016c1a3de04b91f52bb0c68
Derek Ho <dxho@amazon.com>
2024-01-08 18:15:41 -0500

    Revert rpm/deb changes pending decision
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- src/test_workflow/integ_test/distribution_rpm.py
+++ src/test_workflow/integ_test/distribution_rpm.py
@@ -30,9 +30,6 @@ class DistributionRpm(Distribution):
         logging.info("rpm installation requires sudo, script will exit if current user does not have sudo access")
         rpm_install_cmd = " ".join(
             [
-                'export',
-                'OPENSEARCH_INITIAL_ADMIN_PASSWORD=myStrongPassword123!',
-                '&&',
                 'sudo',
                 'yum',
                 'remove',
