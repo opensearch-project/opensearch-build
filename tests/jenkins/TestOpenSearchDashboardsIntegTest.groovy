03b8f49e8b9b40558c27871029d90fba7ff361b4
Sayali Gaikawad <61760125+gaiksaya@users.noreply.github.com>
2024-01-30 11:11:23 -0800

    Reactor and enable integ-tests for all distributions in OSD (#4374)
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- tests/jenkins/TestOpenSearchDashboardsIntegTest.groovy
+++ tests/jenkins/TestOpenSearchDashboardsIntegTest.groovy
@@ -12,6 +12,7 @@ import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
 import static com.lesfurets.jenkins.unit.MethodCall.callArgsToString
 import static com.lesfurets.jenkins.unit.global.lib.GitSource.gitSource
 import static org.hamcrest.CoreMatchers.hasItem
+import static org.hamcrest.CoreMatchers.equalTo
 import static org.hamcrest.CoreMatchers.hasItems
 import static org.hamcrest.MatcherAssert.assertThat
 import static org.junit.jupiter.api.Assertions.assertThrows
@@ -68,7 +69,6 @@ class TestOpenSearchDashboardsIntegTest extends BuildPipelineTest {
         binding.setVariable('distribution', 'tar' )
         binding.setVariable('COMPONENT_NAME', '' )
         binding.getVariable('currentBuild').upstreamBuilds = [[fullProjectName: jobName]]
-        binding.setVariable('UPDATE_GITHUB_ISSUES', true)
         def env = binding.getVariable('env')
         env['DOCKER_AGENT'] = [image:'opensearchstaging/opensearchstaging/ci-runner:ci-runner-rockylinux8-opensearch-dashboards-integtest-v2', args:'-e JAVA_HOME=/opt/java/openjdk-11']
         env['PUBLIC_ARTIFACT_URL'] = 'DUMMY_PUBLIC_ARTIFACT_URL'
@@ -135,6 +135,7 @@ class TestOpenSearchDashboardsIntegTest extends BuildPipelineTest {
 
     @Test
     void checkGHissueCreation() {
+        addParam('UPDATE_GITHUB_ISSUES', true)
         helper.addShMock("date -d \"3 days ago\" +'%Y-%m-%d'") { script ->
             return [stdout: "2023-10-24", exitValue: 0]
         }
@@ -154,6 +155,7 @@ class TestOpenSearchDashboardsIntegTest extends BuildPipelineTest {
 
     @Test
     void CheckCloseGHissue() {
+        addParam('UPDATE_GITHUB_ISSUES', true)
         helper.addShMock("date -d \"3 days ago\" +'%Y-%m-%d'") { script ->
             return [stdout: "2023-10-24", exitValue: 0]
         }
@@ -170,6 +172,7 @@ class TestOpenSearchDashboardsIntegTest extends BuildPipelineTest {
 
     @Test
     void checkGHexistingIssue() {
+        addParam('UPDATE_GITHUB_ISSUES', true)
         helper.addShMock("date -d \"3 days ago\" +'%Y-%m-%d'") { script ->
             return [stdout: "2023-10-24", exitValue: 0]
         }
@@ -186,6 +189,17 @@ class TestOpenSearchDashboardsIntegTest extends BuildPipelineTest {
         assertJobStatusFailure()
         assertThat(getCommandExecutions('println', 'Issue'), hasItem('Issue already exists, adding a comment'))
         assertThat(getCommandExecutions('sh', 'script'), hasItem("{script=gh issue comment bbb\nccc --repo https://github.com/opensearch-project/dashboards-observability.git --body \"The integration test failed at distribution level for component observabilityDashboards<br>Version: 3.0.0<br>Distribution: tar<br>Architecture: x64<br>Platform: linux<br><br>Please check the logs: https://some/url/redirect<br><br> * Test-report manifest:*<br> - https://ci.opensearch.org/ci/dbc/dummy_job/3.0.0/215/linux/x64/tar/test-results/215/integ-test/test-report.yml <br><br> _Note: Steps to reproduce, additional logs and other files can be found within the above test-report manifest. <br>Instructions of this test-report manifest can be found [here](https://github.com/opensearch-project/opensearch-build/tree/main/src/report_workflow#guide-on-test-report-manifest-from-ci)._\", returnStdout=true}"))
+    }
+
+    @Test
+    void checkGHIssueDisable() {
+        addParam('COMPONENT_NAME', 'ganttChartDashboards')
+        helper.addShMock('env PATH=$PATH  ./test.sh integ-test manifests/tests/jenkins/data/opensearch-dashboards-3.0.0-test.yml --component ganttChartDashboards --test-run-id 215 --paths opensearch=/tmp/workspace/tar opensearch-dashboards=/tmp/workspace/tar --base-path DUMMY_PUBLIC_ARTIFACT_URL/dummy_job/3.0.0/215/linux/x64/tar ', '', 1)
+        assertThrows(Exception) {
+            runScript('jenkins/opensearch-dashboards/integ-test.jenkinsfile')
+        }
+        assertJobStatusFailure()
+        assertThat(getCommandExecutions('sh', 'gh issue').size(), equalTo(0))
     }
 
     def getCommandExecutions(methodName, command) {
