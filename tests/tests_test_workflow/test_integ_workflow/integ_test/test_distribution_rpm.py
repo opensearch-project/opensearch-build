86b3470eb7a5ea90543b82ce74c562ab069ad71a
Zelin Hao <zelinhao@amazon.com>
2024-06-12 14:31:48 -0700

    Add condition for OSD integ tests on deb and rpm (#4770)
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- tests/tests_test_workflow/test_integ_workflow/integ_test/test_distribution_rpm.py
+++ tests/tests_test_workflow/test_integ_workflow/integ_test/test_distribution_rpm.py
@@ -55,6 +55,25 @@ class TestDistributionRpm(unittest.TestCase):
             args_list[0][0][0],
         )
 
+    @patch("subprocess.check_call")
+    def test_install_opensearch_dashboards(self, check_call_mock: Mock) -> None:
+        self.distribution_rpm_dashboards.install("opensearch-dashboards.rpm")
+        args_list = check_call_mock.call_args_list
+
+        self.assertEqual(check_call_mock.call_count, 1)
+        self.assertEqual(
+            (
+                "sudo yum remove -y opensearch-dashboards && "
+                "sudo env OPENSEARCH_INITIAL_ADMIN_PASSWORD=myStrongPassword123! "
+                "yum install -y opensearch-dashboards.rpm && "
+                f"sudo chmod 0666 {self.distribution_rpm_dashboards.config_path} && "
+                f"sudo chmod 0755 {os.path.dirname(self.distribution_rpm_dashboards.config_path)} {self.distribution_rpm_dashboards.log_dir} && "
+                f"sudo usermod -a -G opensearch-dashboards `whoami` && "
+                f"sudo usermod -a -G adm `whoami`"
+            ),
+            args_list[0][0][0],
+        )
+
     def test_start_cmd(self) -> None:
         self.assertEqual(self.distribution_rpm.start_cmd, "sudo systemctl start opensearch")
         self.assertEqual(self.distribution_rpm_dashboards.start_cmd, "sudo systemctl start opensearch-dashboards")
