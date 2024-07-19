049851d1759b2cf4ee92d276eb78ce0bb34d6300
Peter Zhu <zhujiaxi@amazon.com>
2024-07-19 17:53:49 -0400

    Avoid checking outdated branches during manifest update (#4867)
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- tests/tests_manifests_workflow/test_input_manifests_opensearch.py
+++ tests/tests_manifests_workflow/test_input_manifests_opensearch.py
@@ -59,3 +59,18 @@ class TestInputManifestsOpenSearch(unittest.TestCase):
         mock_add_to_versionincrement_workflow.assert_has_calls([
             call('2.12.1000'),
         ])
+
+    @patch("manifests_workflow.input_manifests.InputManifests.add_to_versionincrement_workflow")
+    @patch("manifests_workflow.input_manifests.InputManifests.add_to_cron")
+    @patch("manifests.manifest.Manifest.to_file")
+    @patch("manifests_workflow.input_manifests_opensearch.ComponentOpenSearchMin")
+    def test_update_outdated_branch(self, mock_component_opensearch_min: MagicMock, mock_manifest_to_file: MagicMock,
+                                    mock_add_to_cron: MagicMock, mock_add_to_versionincrement_workflow: MagicMock) -> None:
+        mock_component_opensearch_min.return_value = MagicMock(name="OpenSearch")
+        mock_component_opensearch_min.branches.return_value = ["1.2"]
+        mock_component_opensearch_min.checkout.return_value = MagicMock(version="1.2.1000")
+        manifests = InputManifestsOpenSearch()
+        manifests.update()
+        self.assertEqual(mock_manifest_to_file.call_count, 0)
+        self.assertEqual(mock_add_to_cron.call_count, 0)
+        self.assertEqual(mock_add_to_versionincrement_workflow.call_count, 0)
--- tests/tests_manifests_workflow/test_input_manifests_opensearch_dashboards.py
+++ tests/tests_manifests_workflow/test_input_manifests_opensearch_dashboards.py
@@ -60,3 +60,19 @@ class TestInputManifestsOpenSearchDashboards(unittest.TestCase):
         mock_add_to_versionincrement_workflow.assert_has_calls([
             call('2.12.1000')
         ])
+
+    @patch("manifests_workflow.input_manifests.InputManifests.add_to_versionincrement_workflow")
+    @patch("manifests_workflow.input_manifests.InputManifests.add_to_cron")
+    @patch("manifests.manifest.Manifest.to_file")
+    @patch("manifests_workflow.input_manifests_opensearch_dashboards.ComponentOpenSearchDashboardsMin")
+    def test_update_outdated_branch(self, mock_component_opensearch_dashboards_min: MagicMock, mock_manifest_to_file: MagicMock,
+                                    mock_add_to_cron: MagicMock, mock_add_to_versionincrement_workflow: MagicMock) -> None:
+        mock_component_opensearch_dashboards_min.return_value = MagicMock(name="OpenSearch-Dashboards")
+        mock_component_opensearch_dashboards_min.branches.return_value = ["1.2"]
+        mock_component_opensearch_dashboards_min.checkout.return_value = MagicMock(version="1.2.1000")
+
+        manifests = InputManifestsOpenSearchDashboards()
+        manifests.update()
+        self.assertEqual(mock_manifest_to_file.call_count, 0)
+        self.assertEqual(mock_add_to_cron.call_count, 0)
+        self.assertEqual(mock_add_to_versionincrement_workflow.call_count, 0)
