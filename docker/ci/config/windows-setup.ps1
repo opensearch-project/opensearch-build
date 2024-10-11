6735b6c1c00e957712d9e9dfb665e1c2504ece96
Peter Zhu <zhujiaxi@amazon.com>
2024-10-10 20:06:57 -0400

    Resolve scoop Select-CurrentVersion error by locking to 0.4.2
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- docker/ci/config/windows-setup.ps1
+++ docker/ci/config/windows-setup.ps1
@@ -11,7 +11,12 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Install Scoop as Administrator User here
-iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
+# Scoop version >= 0.5.0 has error with Select-CurrentVersion function
+# They have not fix the issue and will error out on Windows ltsc2019
+# https://github.com/ScoopInstaller/Scoop/issues/6180
+# A temp solution is to hardcode the scoop version to 0.4.2
+# iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
+iex "& {$(irm https://raw.githubusercontent.com/peterzhuamazon/scoop-Install/refs/heads/stable/install.ps1)} -RunAsAdmin"
 
 # Disable "current" alias directory as it is not preserved after AMI creation
 # Use static path in environment variable
