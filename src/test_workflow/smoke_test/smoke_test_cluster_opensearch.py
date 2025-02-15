ca41b1bd73b84c3bb8767500cac4974b1f0cf997
Zelin Hao <zelinhao@amazon.com>
2025-02-14 16:01:42 -0800

    Add local cluster logs recording to smoke tests framework (#5321)
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- src/test_workflow/smoke_test/smoke_test_cluster_opensearch.py
+++ src/test_workflow/smoke_test/smoke_test_cluster_opensearch.py
@@ -97,6 +97,20 @@ class SmokeTestClusterOpenSearch():
             logging.info(f"Cluster check fails: {e}")
             return False
 
+    def __save_local_cluster_logs__(self) -> None:
+        dest_directory = os.path.join(self.test_recorder.location, "local-cluster-logs")
+        os.makedirs(dest_directory, exist_ok=True)
+        logging.info(
+            f"Recording local cluster logs for at {dest_directory}"
+        )
+
+        self.test_recorder._generate_std_files(
+            self.process_handler.stdout_data,
+            self.process_handler.stderr_data,
+            dest_directory,
+        )
+
     def __uninstall__(self) -> None:
         self.process_handler.terminate()
+        self.__save_local_cluster_logs__()
         logging.info("Cluster is terminated.")
