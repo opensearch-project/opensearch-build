62b8a90bd9a4c8a544873b69a335f6d28ed32868
Peter Zhu <zhujiaxi@amazon.com>
2025-03-04 16:55:30 +0000

    Update systemd test
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- scripts/components/OpenSearch/integtest.sh
+++ scripts/components/OpenSearch/integtest.sh
@@ -7,5 +7,18 @@
 # this file be licensed under the Apache-2.0 license or a
 # compatible open source license.
 
-echo "Run systemd integTest for OpenSearch core engine"
-./gradlew qa:systemd-test:integTest --tests org.opensearch.systemdinteg.SystemdIT --console=plain
+set -e
+
+echo "Check if distribution is deb or rpm on linux"
+if [ "$OSTYPE" = "linux-gnu" ]; then
+    if (dpkg -s opensearch > /dev/null 2>&1) || (rpm -q opensearch > /dev/null 2>&1); then
+        echo "Run systemd integTest for OpenSearch core engine"
+        ./gradlew :qa:systemd-test:test --tests org.opensearch.systemdinteg.SystemdIntegTests --console=plain
+    else
+        echo "No deb or rpm installed detected, skip test"
+    fi
+else
+    echo "Not on linux host, skip test"
+fi
+
+exit 0
