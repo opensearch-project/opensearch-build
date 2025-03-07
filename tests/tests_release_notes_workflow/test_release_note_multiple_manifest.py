a61b63bbfb3e87ca088e4136691559919a3bb83b
Peter Zhu <zhujiaxi@amazon.com>
2025-03-06 22:34:10 -0500

    Update release notes workflows to support qualifier and update gradle check lib 8.2.3 (#5374)
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- tests/tests_release_notes_workflow/test_release_note_multiple_manifest.py
+++ tests/tests_release_notes_workflow/test_release_note_multiple_manifest.py
@@ -23,12 +23,18 @@ class TestReleaseNotes(unittest.TestCase):
 
     def setUp(self) -> None:
         OPENSEARCH_MANIFEST = os.path.realpath(os.path.join(TestReleaseNotes.MANIFESTS, "opensearch-test-main.yml"))
+        OPENSEARCH_MANIFEST_QUALIFIER = os.path.realpath(os.path.join(TestReleaseNotes.MANIFESTS, "opensearch-test-main-qualifier.yml"))
         DASHBOARDS_MANIFEST = os.path.realpath(
             os.path.join(TestReleaseNotes.MANIFESTS, "opensearch-dashboards-test-main.yml"))
         self.opensearch_manifest = InputManifest.from_file(open(OPENSEARCH_MANIFEST))
+        self.opensearch_manifest_qualifier = InputManifest.from_file(open(OPENSEARCH_MANIFEST_QUALIFIER))
         self.dashboards_manifest = InputManifest.from_file(open(DASHBOARDS_MANIFEST))
         self.build_version = self.opensearch_manifest.build.version
+        self.build_qualifier = self.opensearch_manifest.build.qualifier
+        self.build_with_qualifier_version = self.opensearch_manifest_qualifier.build.version
+        self.build_with_qualifier_qualifier = self.opensearch_manifest_qualifier.build.qualifier
         self.release_notes = ReleaseNotes([self.opensearch_manifest, self.dashboards_manifest], "2022-07-26", "compile")
+        self.release_notes_qualifier = ReleaseNotes([self.opensearch_manifest_qualifier, self.dashboards_manifest], "2022-07-26", "compile")
         self.opensearch_component = InputComponentFromSource(
             {"name": "OpenSearch-test", "repository": "url", "ref": "ref"})
         self.dashboards_component = InputComponentFromSource(
@@ -37,16 +43,24 @@ class TestReleaseNotes(unittest.TestCase):
     @patch("subprocess.check_output", return_value=''.encode())
     @patch("subprocess.check_call")
     def test_check(self, *mocks: Any) -> None:
-        self.assertEqual(self.release_notes.check(self.opensearch_component, self.build_version),
+        self.assertEqual(self.release_notes.check(self.opensearch_component, self.build_version, self.build_qualifier),
                          ['OpenSearch-test', '[ref]', None, None, False, None])
-        self.assertEqual(self.release_notes.check(self.dashboards_component, self.build_version),
+        self.assertEqual(self.release_notes.check(self.dashboards_component, self.build_version, self.build_qualifier),
+                         ['OpenSearch-Dashboards-test', '[ref]', None, None, False, None])
+
+    @patch("subprocess.check_output", return_value=''.encode())
+    @patch("subprocess.check_call")
+    def test_check_qualifier(self, *mocks: Any) -> None:
+        self.assertEqual(self.release_notes_qualifier.check(self.opensearch_component, self.build_version, self.build_with_qualifier_qualifier),
+                         ['OpenSearch-test', '[ref]', None, None, False, None])
+        self.assertEqual(self.release_notes_qualifier.check(self.dashboards_component, self.build_version, self.build_with_qualifier_qualifier),
                          ['OpenSearch-Dashboards-test', '[ref]', None, None, False, None])
 
     @patch("subprocess.check_output", return_value=''.encode())
     @patch("subprocess.check_call")
     def test_table(self, *mocks: Any) -> None:
-        self.release_notes.check(self.opensearch_component, self.build_version)
-        self.release_notes.check(self.dashboards_component, self.build_version)
+        self.release_notes.check(self.opensearch_component, self.build_version, self.build_qualifier)
+        self.release_notes.check(self.dashboards_component, self.build_version, self.build_qualifier)
         self.release_notes.table()
 
         self.assertEqual(self.release_notes.table()._table_name.strip(),
@@ -54,5 +68,20 @@ class TestReleaseNotes(unittest.TestCase):
         self.assertEqual(self.release_notes.table().headers,
                          ['Repo', 'Branch', 'CommitID', 'Commit Date', 'Release Notes Exists', 'URL'])
         self.assertEqual(self.release_notes.table().value_matrix,
+                         [['OpenSearch-Dashboards-test', '[main]', None, None, False, None],
+                          ['OpenSearch-test', '[main]', None, None, False, None]])
+
+    @patch("subprocess.check_output", return_value=''.encode())
+    @patch("subprocess.check_call")
+    def test_table_qualifier(self, *mocks: Any) -> None:
+        self.release_notes_qualifier.check(self.opensearch_component, self.build_with_qualifier_version, self.build_with_qualifier_qualifier)
+        self.release_notes_qualifier.check(self.dashboards_component, self.build_with_qualifier_version, self.build_with_qualifier_qualifier)
+        self.release_notes_qualifier.table()
+
+        self.assertEqual(self.release_notes_qualifier.table()._table_name.strip(),
+                         'Core Components CommitID(after 2022-07-26) & Release Notes info')
+        self.assertEqual(self.release_notes_qualifier.table().headers,
+                         ['Repo', 'Branch', 'CommitID', 'Commit Date', 'Release Notes Exists', 'URL'])
+        self.assertEqual(self.release_notes_qualifier.table().value_matrix,
                          [['OpenSearch-Dashboards-test', '[main]', None, None, False, None],
                           ['OpenSearch-test', '[main]', None, None, False, None]])
