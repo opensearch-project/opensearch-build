9341bdf9a60366565a3183c93c3238fd459423f1
Zelin Hao <zelinhao@amazon.com>
2025-04-16 16:12:20 -0700

    Update publish to Maven
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- tests/jenkins/TestMavenSignReleaseJob.groovy
+++ tests/jenkins/TestMavenSignReleaseJob.groovy
@@ -18,7 +18,7 @@ class TestMavenSignReleaseJob extends BuildPipelineTest {
 
         helper.registerSharedLibrary(
             library().name('jenkins')
-                .defaultVersion('1.0.4')
+                .defaultVersion('9.0.0')
                 .allowOverride(true)
                 .implicit(true)
                 .targetPath('vars')
