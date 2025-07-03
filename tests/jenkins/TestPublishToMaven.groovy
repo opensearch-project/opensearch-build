230671cde873140dbd8a2cc59250550fdb3c796a
Sayali Gaikawad <gaiksaya@amazon.com>
2025-07-03 11:33:50 -0700

    Update maven publishing workflow to accommodate nexus EOL (#5618)
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- tests/jenkins/TestPublishToMaven.groovy
+++ tests/jenkins/TestPublishToMaven.groovy
@@ -22,7 +22,7 @@ class TestPublishToMaven extends BuildPipelineTest {
     void setUp() {
         helper.registerSharedLibrary(
             library().name('jenkins')
-                .defaultVersion('testMaven')
+                .defaultVersion('10.0.0')
                 .allowOverride(true)
                 .implicit(true)
                 .targetPath('vars')
