5c7e61594a1d64c7086c864c905cc7439b65eb6b
Sayali Gaikawad <gaiksaya@amazon.com>
2025-10-01 16:56:19 -0700

    Fix reference for release notes generation
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- tests/jenkins/TestReleaseNotesGenerate.groovy
+++ tests/jenkins/TestReleaseNotesGenerate.groovy
@@ -72,6 +72,29 @@ class TestReleaseNotesGenerate extends BuildPipelineTest {
         assertThat(helper.callStack.findAll { call ->
             call.methodName == 'sh'
         }.any { call ->
+            callArgsToString(call).contains('./release_notes.sh generate manifests/tests/jenkins/data/opensearch-2.2.0.yml --component sql --date 2025-06-24 --max-tokens 10000')
+        }).isTrue()
+
+        assertThat(helper.callStack.findAll { call ->
+            call.methodName == 'sh'
+        }.any { call ->
+            callArgsToString(call).contains('./release_notes.sh generate manifests/tests/jenkins/data/opensearch-2.2.0.yml --component OpenSearch --date 2025-06-24 --max-tokens 10000')
+        }).isTrue()
+    }
+
+    @Test
+    public void releaseNotesCheckOpenSearchAndSqlWithRef() {
+        addParam('COMPONENTS', 'OpenSearch sql')
+        addParam('REF', 'main')
+        runScript("jenkins/release-workflows/release-notes-generate.jenkinsfile")
+
+        def callStack = helper.getCallStack()
+        assertCallStack().contains('Release notes for OpenSearch=groovy.lang.Closure, Release notes for sql=groovy.lang.Closure')
+        assertCallStack().contains('OpenSearch with index 0 will sleep 0 seconds to reduce load && sleep 0')
+        assertCallStack().contains('sql with index 1 will sleep 30 seconds to reduce load && sleep 30')
+        assertThat(helper.callStack.findAll { call ->
+            call.methodName == 'sh'
+        }.any { call ->
             callArgsToString(call).contains('./release_notes.sh generate manifests/tests/jenkins/data/opensearch-2.2.0.yml --component sql --date 2025-06-24 --max-tokens 10000 --ref main')
         }).isTrue()
 
@@ -81,4 +104,5 @@ class TestReleaseNotesGenerate extends BuildPipelineTest {
             callArgsToString(call).contains('./release_notes.sh generate manifests/tests/jenkins/data/opensearch-2.2.0.yml --component OpenSearch --date 2025-06-24 --max-tokens 10000 --ref main')
         }).isTrue()
     }
+    
 }
