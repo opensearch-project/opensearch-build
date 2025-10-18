484b78a16fcad63fbff6b90f38aaa3789c5fc52b
Peter Zhu <zhujiaxi@amazon.com>
2025-10-18 15:34:13 -0400

    Update session token to be optional parameter
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- publish/publish-snapshot.sh
+++ publish/publish-snapshot.sh
@@ -26,14 +26,14 @@ usage() {
   echo "          example: dir = ~/.m2/repository/org/opensearch where dir contains artifacts of a path:"
   echo "                   /maven/reindex-client/1.0.0-SNAPSHOT/reindex-client-1.0.0-SNAPSHOT.jar"
   echo "  -h      display help"
-  echo "Required environment variables:"
-  echo "SONATYPE_USERNAME - username with publish rights to a sonatype repository"
-  echo "SONATYPE_PASSWORD - password for sonatype"
-  echo "AWS_ACCESS_KEY_ID - aws_access_key_id for AWS account with publish rights to a s3 bucket"
-  echo "AWS_SECRET_ACCESS_KEY - aws_secret_access_key for s3"
-  echo "AWS_SESSION_TOKEN - aws_session_token for s3 when assume role"
-  echo "SNAPSHOT_REPO_URL - repository URL ex. http://localhost:8081/nexus/content/repositories/snapshots/"
-  echo "                                   ex. s3://my-bucket-name/maven/snapshots/"
+  echo "Environment variables:"
+  echo "SONATYPE_USERNAME (Required)     - username with publish rights to a sonatype repository"
+  echo "SONATYPE_PASSWORD (Required)     - password for sonatype"
+  echo "AWS_ACCESS_KEY_ID (Required)     - aws_access_key_id for AWS account with publish rights to a s3 bucket"
+  echo "AWS_SECRET_ACCESS_KEY (Required) - aws_secret_access_key for s3"
+  echo "AWS_SESSION_TOKEN (Optional)     - aws_session_token for s3 when assume role"
+  echo "SNAPSHOT_REPO_URL (Required)     - repository URL nexus : http://localhost:8081/nexus/content/repositories/snapshots/"
+  echo "                                                  aws s3: s3://my-bucket-name/maven/snapshots/"
   exit 1
 }
 
@@ -68,8 +68,8 @@ else
 fi
 
 if [ "$SERVER_ID" = "s3" ]; then
-  if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$AWS_SESSION_TOKEN" ]; then
-    echo "AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_SESSION_TOKEN are required for s3"
+  if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+    echo "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are required for s3"
     exit 1
   fi
 else
