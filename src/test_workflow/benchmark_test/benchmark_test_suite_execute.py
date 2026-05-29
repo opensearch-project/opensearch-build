1dd0b4d4c7c2b22c11956d3e76205ee37d54a4a8
Rishabh Singh <sngri@amazon.com>
2026-05-29 16:30:22 -0700

    fix agent name
    Signed-off-by: Rishabh Singh <sngri@amazon.com>

--- src/test_workflow/benchmark_test/benchmark_test_suite_execute.py
+++ src/test_workflow/benchmark_test/benchmark_test_suite_execute.py
@@ -21,7 +21,7 @@ from test_workflow.benchmark_test.benchmark_test_suite import BenchmarkTestSuite
 
 class BenchmarkTestSuiteExecute(BenchmarkTestSuite):
 
-    DATA_WORKLOADS = ["clickbench", "textbench"]
+    DATA_WORKLOADS = ["clickbench", "textbench", "big5"]
 
     def __init__(self, endpoint: str, security: bool, args: BenchmarkArgs, password: str):
         super().__init__(args, endpoint, security, password)
